9f91dc79824e74cd754e5d3eb46fe13a8f825691
Jose Luis Rivero <jrivero@osrfoundation.org>
2014-04-14 22:51:03 +0000

    Define proper constant

--- include/ignition/math/Helpers.hh
+++ include/ignition/math/Helpers.hh
@@ -17,8 +17,9 @@
 #ifndef _IGNITION_MATH_FUNCTIONS_HH_
 #define _IGNITION_MATH_FUNCTIONS_HH_
 
-#include <algorithm>
+#define _USE_MATH_DEFINES
 #include <cmath>
+#include <algorithm>
 #include <limits>
 #include <string>
 #include <iostream>
