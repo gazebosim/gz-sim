57b9a3954fb0d5b0a9d0355c95eb8f8b33470c59
Nate Koenig <natekoenig@gmail.com>
2014-07-10 07:25:13 -0700

    Improved code coverage

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -100,19 +100,25 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 
 	# Setup target
 	ADD_CUSTOM_TARGET(${_targetname}
+
 		# Capturing lcov counters and generating report
-		COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
-    COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'test/*' '/usr/*' '*_TEST*' --output-file ${_outputname}.info.cleaned
-		COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
-		COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info ${_outputname}.info.cleaned
+    COMMAND ${LCOV_PATH} -q --no-checksum --directory ${PROJECT_BINARY_DIR}
+      --capture --output-file ${_outputname}.info 2>/dev/null
+    COMMAND ${LCOV_PATH} -q --remove ${_outputname}.info
+      'test/*' '/usr/*' '*_TEST*' --output-file ${_outputname}.info.cleaned
+		COMMAND ${GENHTML_PATH} -q --legend -o ${_outputname}
+      ${_outputname}.info.cleaned
+    COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info
+      ${_outputname}.info.cleaned
 		
 		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-		COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."
+		COMMENT "Resetting code coverage counters to zero.\n"
+      "Processing code coverage counters and generating report."
 	)
 	
 	# Show info where to find the report
 	ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
-		COMMAND ${LCOV_PATH} --directory . --zerocounters;
+		COMMAND COMMAND ${LCOV_PATH} -q --zerocounters --directory ${PROJECT_BINARY_DIR};
 		COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
 	)
 
