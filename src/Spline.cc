ab887d705b19c03dca6452adc9311dc639d7e769
nate koenig <natekoenig@gmail.com>
2015-04-17 08:14:24 -0700

    Final bits to make library compile cleanly on windows

--- src/Spline.cc
+++ src/Spline.cc
@@ -17,6 +17,7 @@
 // Note: Originally cribbed from Ogre3d. Modified to implement Cardinal
 // spline and catmull-rom spline
 
+#include "ignition/math/SplinePrivate.hh"
 #include "ignition/math/Helpers.hh"
 #include "ignition/math/Vector4.hh"
 #include "ignition/math/Spline.hh"
@@ -26,56 +27,59 @@ using namespace math;
 
 ///////////////////////////////////////////////////////////
 Spline::Spline()
+: dataPtr(new SplinePrivate)
 {
   // Set up matrix
   // Hermite polynomial
-  this->coeffs(0, 0) = 2;
-  this->coeffs(0, 1) = -2;
-  this->coeffs(0, 2) = 1;
-  this->coeffs(0, 3) = 1;
+  this->dataPtr->coeffs(0, 0) = 2;
+  this->dataPtr->coeffs(0, 1) = -2;
+  this->dataPtr->coeffs(0, 2) = 1;
+  this->dataPtr->coeffs(0, 3) = 1;
 
-  this->coeffs(1, 0) = -3;
-  this->coeffs(1, 1) = 3;
-  this->coeffs(1, 2) = -2;
-  this->coeffs(1, 3) = -1;
+  this->dataPtr->coeffs(1, 0) = -3;
+  this->dataPtr->coeffs(1, 1) = 3;
+  this->dataPtr->coeffs(1, 2) = -2;
+  this->dataPtr->coeffs(1, 3) = -1;
 
-  this->coeffs(2, 0) = 0;
-  this->coeffs(2, 1) = 0;
-  this->coeffs(2, 2) = 1;
-  this->coeffs(2, 3) = 0;
+  this->dataPtr->coeffs(2, 0) = 0;
+  this->dataPtr->coeffs(2, 1) = 0;
+  this->dataPtr->coeffs(2, 2) = 1;
+  this->dataPtr->coeffs(2, 3) = 0;
 
-  this->coeffs(3, 0) = 1;
-  this->coeffs(3, 1) = 0;
-  this->coeffs(3, 2) = 0;
-  this->coeffs(3, 3) = 0;
+  this->dataPtr->coeffs(3, 0) = 1;
+  this->dataPtr->coeffs(3, 1) = 0;
+  this->dataPtr->coeffs(3, 2) = 0;
+  this->dataPtr->coeffs(3, 3) = 0;
 
-  this->autoCalc = true;
-  this->tension = 0.0;
+  this->dataPtr->autoCalc = true;
+  this->dataPtr->tension = 0.0;
 }
 
 ///////////////////////////////////////////////////////////
 Spline::~Spline()
 {
+  delete this->dataPtr;
+  this->dataPtr = NULL;
 }
 
 ///////////////////////////////////////////////////////////
 void Spline::Tension(double _t)
 {
-  this->tension = _t;
+  this->dataPtr->tension = _t;
   this->RecalcTangents();
 }
 
 ///////////////////////////////////////////////////////////
 double Spline::Tension() const
 {
-  return this->tension;
+  return this->dataPtr->tension;
 }
 
 ///////////////////////////////////////////////////////////
 void Spline::AddPoint(const Vector3d &_p)
 {
-  this->points.push_back(_p);
-  if (this->autoCalc)
+  this->dataPtr->points.push_back(_p);
+  if (this->dataPtr->autoCalc)
     this->RecalcTangents();
 }
 
@@ -87,7 +91,7 @@ Vector3d Spline::Interpolate(double _t) const
   // TODO: base on arclength?
 
   // Work out which segment this is in
-  double fSeg = _t * (this->points.size() - 1);
+  double fSeg = _t * (this->dataPtr->points.size() - 1);
   unsigned int segIdx = (unsigned int)fSeg;
   // Apportion t
   _t = fSeg - segIdx;
@@ -99,21 +103,21 @@ Vector3d Spline::Interpolate(double _t) const
 Vector3d Spline::Interpolate(unsigned int _fromIndex, double _t) const
 {
   // Bounds check
-  if (_fromIndex >= this->points.size())
+  if (_fromIndex >= this->dataPtr->points.size())
     throw IndexException();
 
-  if ((_fromIndex + 1) == this->points.size())
+  if ((_fromIndex + 1) == this->dataPtr->points.size())
   {
     // Duff request, cannot blend to nothing
     // Just return source
-    return this->points[_fromIndex];
+    return this->dataPtr->points[_fromIndex];
   }
 
   // Fast special cases
   if (equal(_t, 0.0))
-    return this->points[_fromIndex];
+    return this->dataPtr->points[_fromIndex];
   else if (equal(_t, 1.0))
-    return this->points[_fromIndex + 1];
+    return this->dataPtr->points[_fromIndex + 1];
 
   // double interpolation
   // Form a vector of powers of t
@@ -123,12 +127,12 @@ Vector3d Spline::Interpolate(unsigned int _fromIndex, double _t) const
   Vector4d powers(t3, t2, _t, 1);
 
 
-  // Algorithm is ret = powers * this->coeffs * Matrix4(point1,
+  // Algorithm is ret = powers * this->dataPtr->coeffs * Matrix4(point1,
   // point2, tangent1, tangent2)
-  const Vector3d &point1 = this->points[_fromIndex];
-  const Vector3d &point2 = this->points[_fromIndex+1];
-  const Vector3d &tan1 = this->tangents[_fromIndex];
-  const Vector3d &tan2 = this->tangents[_fromIndex+1];
+  const Vector3d &point1 = this->dataPtr->points[_fromIndex];
+  const Vector3d &point2 = this->dataPtr->points[_fromIndex+1];
+  const Vector3d &tan1 = this->dataPtr->tangents[_fromIndex];
+  const Vector3d &tan2 = this->dataPtr->tangents[_fromIndex+1];
   Matrix4d pt;
 
   pt(0, 0) = point1.X();
@@ -148,7 +152,7 @@ Vector3d Spline::Interpolate(unsigned int _fromIndex, double _t) const
   pt(3, 2) = tan2.Z();
   pt(3, 3) = 1.0f;
 
-  Vector4d ret = powers * this->coeffs * pt;
+  Vector4d ret = powers * this->dataPtr->coeffs * pt;
 
   return Vector3d(ret.X(), ret.Y(), ret.Z());
 }
@@ -165,7 +169,7 @@ void Spline::RecalcTangents()
   size_t i, numPoints;
   bool isClosed;
 
-  numPoints = this->points.size();
+  numPoints = this->dataPtr->points.size();
   if (numPoints < 2)
   {
     // Can't do anything yet
@@ -173,13 +177,13 @@ void Spline::RecalcTangents()
   }
 
   // Closed or open?
-  if (this->points[0] == this->points[numPoints-1])
+  if (this->dataPtr->points[0] == this->dataPtr->points[numPoints-1])
     isClosed = true;
   else
     isClosed = false;
 
-  double t = 1.0 - this->tension;
-  this->tangents.resize(numPoints);
+  double t = 1.0 - this->dataPtr->tension;
+  this->dataPtr->tangents.resize(numPoints);
 
   for (i = 0; i < numPoints; ++i)
   {
@@ -188,15 +192,15 @@ void Spline::RecalcTangents()
       // Special case start
       if (isClosed)
       {
-        // Use nuthis->points-2 since nuthis->points-1 is the last
+        // Use nuthis->dataPtr->points-2 since nuthis->dataPtr->points-1 is the last
         // point and == [0]
-        this->tangents[i] =
-          ((this->points[1] - this->points[numPoints-2]) * 0.5) * t;
+        this->dataPtr->tangents[i] =
+          ((this->dataPtr->points[1] - this->dataPtr->points[numPoints-2]) * 0.5) * t;
       }
       else
       {
-        this->tangents[i] =
-          ((this->points[1] - this->points[0]) * 0.5) * t;
+        this->dataPtr->tangents[i] =
+          ((this->dataPtr->points[1] - this->dataPtr->points[0]) * 0.5) * t;
       }
     }
     else if (i == numPoints-1)
@@ -205,18 +209,18 @@ void Spline::RecalcTangents()
       if (isClosed)
       {
         // Use same tangent as already calculated for [0]
-        this->tangents[i] = this->tangents[0];
+        this->dataPtr->tangents[i] = this->dataPtr->tangents[0];
       }
       else
       {
-        this->tangents[i] =
-          ((this->points[i] - this->points[i-1]) * 0.5) * t;
+        this->dataPtr->tangents[i] =
+          ((this->dataPtr->points[i] - this->dataPtr->points[i-1]) * 0.5) * t;
       }
     }
     else
     {
-      this->tangents[i] =
-        ((this->points[i+1] - this->points[i-1]) * 0.5) * t;
+      this->dataPtr->tangents[i] =
+        ((this->dataPtr->points[i+1] - this->dataPtr->points[i-1]) * 0.5) * t;
     }
   }
 }
@@ -224,47 +228,47 @@ void Spline::RecalcTangents()
 ///////////////////////////////////////////////////////////
 Vector3d Spline::Point(unsigned int _index) const
 {
-  if (_index >= this->points.size())
+  if (_index >= this->dataPtr->points.size())
     throw IndexException();
 
-  return this->points[_index];
+  return this->dataPtr->points[_index];
 }
 
 ///////////////////////////////////////////////////////////
 Vector3d Spline::Tangent(unsigned int _index) const
 {
-  if (_index >= this->tangents.size())
+  if (_index >= this->dataPtr->tangents.size())
     throw IndexException();
 
-  return this->tangents[_index];
+  return this->dataPtr->tangents[_index];
 }
 
 ///////////////////////////////////////////////////////////
-unsigned int Spline::PointCount() const
+size_t Spline::PointCount() const
 {
-  return this->points.size();
+  return this->dataPtr->points.size();
 }
 
 ///////////////////////////////////////////////////////////
 void Spline::Clear()
 {
-  this->points.clear();
-  this->tangents.clear();
+  this->dataPtr->points.clear();
+  this->dataPtr->tangents.clear();
 }
 
 ///////////////////////////////////////////////////////////
 void Spline::UpdatePoint(unsigned int _index, const Vector3d &_value)
 {
-  if (_index >= this->points.size())
+  if (_index >= this->dataPtr->points.size())
     throw IndexException();
 
-  this->points[_index] = _value;
-  if (this->autoCalc)
+  this->dataPtr->points[_index] = _value;
+  if (this->dataPtr->autoCalc)
     this->RecalcTangents();
 }
 
 ///////////////////////////////////////////////////////////
 void Spline::AutoCalculate(bool _autoCalc)
 {
-  this->autoCalc = _autoCalc;
+  this->dataPtr->autoCalc = _autoCalc;
 }
