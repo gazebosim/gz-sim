22e80621c9f2595a45e2e2136fc07394a8a243f3
Steve Peters <scpeters@osrfoundation.org>
2015-06-01 01:49:18 -0700

    Convert EXPECT_TRUE(v == *) to EXPECT_EQ(v, *) in Vector4_TEST.cc

--- src/Vector4_TEST.cc
+++ src/Vector4_TEST.cc
@@ -41,7 +41,7 @@ TEST(Vector4dTest, Vector4d)
   EXPECT_TRUE(math::equal(v1.W(), 4.0));
 
   math::Vector4d v(v1);
-  EXPECT_TRUE(v == v1);
+  EXPECT_EQ(v, v1);
 
   EXPECT_TRUE(math::equal(v.Distance(
           math::Vector4d(0, 0, 0, 0)), 5.4772, 1e-3));
@@ -55,79 +55,79 @@ TEST(Vector4dTest, Vector4d)
 
   // ::Normalize
   v.Normalize();
-  EXPECT_TRUE(v == math::Vector4d(0.182574, 0.365148, 0.547723, 0.730297));
+  EXPECT_EQ(v, math::Vector4d(0.182574, 0.365148, 0.547723, 0.730297));
 
   // ::Set
   v.Set(2, 4, 6, 8);
-  EXPECT_TRUE(v == math::Vector4d(2, 4, 6, 8));
+  EXPECT_EQ(v, math::Vector4d(2, 4, 6, 8));
 
   // ::operator= vector4
   v = v1;
-  EXPECT_TRUE(v == v1);
+  EXPECT_EQ(v, v1);
 
   // ::operator= double
   v = 1.2;
-  EXPECT_TRUE(v == math::Vector4d(1.2, 1.2, 1.2, 1.2));
+  EXPECT_EQ(v, math::Vector4d(1.2, 1.2, 1.2, 1.2));
 
   // ::operator+ vector4
   v = v + math::Vector4d(1, 2, 3, 4);
-  EXPECT_TRUE(v == math::Vector4d(2.2, 3.2, 4.2, 5.2));
+  EXPECT_EQ(v, math::Vector4d(2.2, 3.2, 4.2, 5.2));
 
   // ::operator+=
   v += v;
-  EXPECT_TRUE(v == math::Vector4d(4.4, 6.4, 8.4, 10.4));
+  EXPECT_EQ(v, math::Vector4d(4.4, 6.4, 8.4, 10.4));
 
   // ::operator- vector4
   v = v - math::Vector4d(1, 1, 1, 1);
-  EXPECT_TRUE(v == math::Vector4d(3.4, 5.4, 7.4, 9.4));
+  EXPECT_EQ(v, math::Vector4d(3.4, 5.4, 7.4, 9.4));
 
   // ::operator-= vector4
   v -= math::Vector4d(1, 1, 1, 1);
-  EXPECT_TRUE(v == math::Vector4d(2.4, 4.4, 6.4, 8.4));
+  EXPECT_EQ(v, math::Vector4d(2.4, 4.4, 6.4, 8.4));
 
 
   // ::operator/ vector4
   v = v / math::Vector4d(.1, .1, .1, .1);
-  EXPECT_TRUE(v == math::Vector4d(24, 44, 64, 84));
+  EXPECT_EQ(v, math::Vector4d(24, 44, 64, 84));
 
   // ::operator/ double
   v = v / 2.0;
-  EXPECT_TRUE(v == math::Vector4d(12, 22, 32, 42));
+  EXPECT_EQ(v, math::Vector4d(12, 22, 32, 42));
 
   // ::operator/= vector4
   v /= math::Vector4d(4, 4, 4, 4);
-  EXPECT_TRUE(v == math::Vector4d(3, 5.5, 8, 10.5));
+  EXPECT_EQ(v, math::Vector4d(3, 5.5, 8, 10.5));
 
   // ::operator/= double
   v /= .1;
-  EXPECT_TRUE(v == math::Vector4d(30, 55, 80, 105));
+  EXPECT_EQ(v, math::Vector4d(30, 55, 80, 105));
 
   // ::operator * matrix4
   v = v * math::Matrix4d(1, 2, 3, 4,
                         5, 6, 7, 8,
                         9, 10, 11, 12,
                         13, 14, 15, 16);
-  EXPECT_TRUE(v == math::Vector4d(2390, 2660, 2930, 3200));
+  EXPECT_EQ(v, math::Vector4d(2390, 2660, 2930, 3200));
 
 
   // ::operator * vector4
   v = v * math::Vector4d(.2, .3, .4, .5);
-  EXPECT_TRUE(v == math::Vector4d(478, 798, 1172, 1600));
+  EXPECT_EQ(v, math::Vector4d(478, 798, 1172, 1600));
 
   // ::operator *= vector4
   v *= math::Vector4d(2, 4, 6, 8);
-  EXPECT_TRUE(v == math::Vector4d(956, 3192, 7032, 12800));
+  EXPECT_EQ(v, math::Vector4d(956, 3192, 7032, 12800));
 
   // ::operator * double
   v = v * 5.2;
-  EXPECT_TRUE(v == math::Vector4d(4971.2, 16598.4, 36566.4, 66560));
+  EXPECT_EQ(v, math::Vector4d(4971.2, 16598.4, 36566.4, 66560));
 
   // ::operator *= double
   v *= 10.0;
-  EXPECT_TRUE(v == math::Vector4d(49712, 1.65984e+05, 3.65664e+05, 6.656e+05));
+  EXPECT_EQ(v, math::Vector4d(49712, 1.65984e+05, 3.65664e+05, 6.656e+05));
 
   // ::operator != vector4
-  EXPECT_TRUE(v != math::Vector4d());
+  EXPECT_NE(v, math::Vector4d());
 
   // ::IsFinite
   EXPECT_TRUE(v.IsFinite());
