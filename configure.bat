a0b36c56147f657505421df7668417739bccd42b
Silvio Traversaro <silvio.traversaro@iit.it>
2015-09-05 15:52:48 +0200

    Fixes for compiling and running tests on Windows 7/Visual Studio 2013
    * Compilation fix in Kmeans_TEST.cc
    * Disable failing test due to different RNG implementations
    * Enable check_* test files only if Python is found

--- configure.bat
+++ configure.bat
@@ -2,4 +2,4 @@
 @if not "%1"=="" set build_type=%1
 @echo Configuring for build type %build_type%
 
-cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX="install\%build_type%" -DCMAKE_BUILD_TYPE="%build_type%" -DENABLE_TESTS_COMPILATION:BOOL=False ..
+cmake -G "NMake Makefiles" -DCMAKE_INSTALL_PREFIX="install\%build_type%" -DCMAKE_BUILD_TYPE="%build_type%" -DENABLE_TESTS_COMPILATION:BOOL=True ..
--- src/Kmeans_TEST.cc
+++ src/Kmeans_TEST.cc
@@ -100,7 +100,7 @@ TEST(KmeansTest, Kmeans)
   EXPECT_FALSE(kmeans.Cluster(0, centroids, labels));
 
   // Try to use a k > num_observations.
-  EXPECT_FALSE(kmeans.Cluster(obs.size() + 1, centroids, labels));
+  EXPECT_FALSE(kmeans.Cluster((int)(obs.size() + 1), centroids, labels));
 }
 
 //////////////////////////////////////////////////
@@ -131,7 +131,9 @@ TEST(KmeansTest, Append)
 
   std::vector<math::Vector3d> obsCopy;
   obsCopy = kmeans.Observations();
-  EXPECT_EQ(obsTotal, obsCopy);
+
+  for (auto i = 0; i < obsCopy.size(); ++i)
+    EXPECT_EQ(obsTotal[i], obsCopy[i]);
 
   // Append an empty vector.
   std::vector<math::Vector3d> emptyVector;
