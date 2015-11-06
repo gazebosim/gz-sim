04a8cd513cba675afb0f1156bbe02e38444b4fdc
Steve Peters <scpeters@osrfoundation.org>
2015-11-05 17:48:43 -0800

    Add Matrix3::Determinant function, copying code from Matrix3::Inverse

--- include/ignition/math/Matrix3.hh
+++ include/ignition/math/Matrix3.hh
@@ -322,6 +322,24 @@ namespace ignition
         return this->data[_row][_col];
       }
 
+      /// \brief Return the determinant of the matrix
+      /// \return Determinant of this matrix.
+      public: T Determinant() const
+      {
+        double t0 = this->data[2][2]*this->data[1][1] -
+                    this->data[2][1]*this->data[1][2];
+
+        double t1 = -(this->data[2][2]*this->data[1][0] -
+                      this->data[2][0]*this->data[1][2]);
+
+        double t2 = this->data[2][1]*this->data[1][0] -
+                    this->data[2][0]*this->data[1][1];
+
+        return t0 * this->data[0][0]
+             + t1 * this->data[0][1]
+             + t2 * this->data[0][2];
+      }
+
       /// \brief Return the inverse matrix
       /// \return Inverse of this matrix.
       public: Matrix3<T> Inverse() const
