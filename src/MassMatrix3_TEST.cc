4b3b39e2700534658d094dde3f3067a9db251ffd
Steve Peters <scpeters@osrfoundation.org>
2015-12-03 17:35:55 -0800

    fix windows build

--- src/MassMatrix3_TEST.cc
+++ src/MassMatrix3_TEST.cc
@@ -15,7 +15,10 @@
  *
 */
 
-#define _USE_MATH_DEFINES
+#ifndef _USE_MATH_DEFINES
+# define _USE_MATH_DEFINES
+#endif
+#include <cmath>
 #include <gtest/gtest.h>
 
 #include "ignition/math/Helpers.hh"
