96234209e28f120d2fce5dfc7c6ca29cc9f993d2
Steve Peters <scpeters@osrfoundation.org>
2016-05-10 02:41:43 -0700

    Replace M_PI with IGN_PI in MassMatrix3.hh

--- include/ignition/math/MassMatrix3.hh
+++ include/ignition/math/MassMatrix3.hh
@@ -370,8 +370,8 @@ namespace ignition
 
         // sort the moments from smallest to largest
         T moment0 = (b + 2*sqrt(p) * cos(delta / 3.0)) / 3.0;
-        T moment1 = (b + 2*sqrt(p) * cos((delta + 2*M_PI)/3.0)) / 3.0;
-        T moment2 = (b + 2*sqrt(p) * cos((delta - 2*M_PI)/3.0)) / 3.0;
+        T moment1 = (b + 2*sqrt(p) * cos((delta + 2*IGN_PI)/3.0)) / 3.0;
+        T moment2 = (b + 2*sqrt(p) * cos((delta - 2*IGN_PI)/3.0)) / 3.0;
         sort3(moment0, moment1, moment2);
         return Vector3<T>(moment0, moment1, moment2);
       }
