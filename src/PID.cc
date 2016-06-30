5ba1b865f3dc2cde97a089fd752cb049e4cfeb3d
nkoenig <devnull@localhost>
2016-06-30 09:32:20 -0700

    Add PID class

---
+++ src/PID.cc
@@ -0,0 +1,220 @@
+/*
+ * Copyright (C) 2016 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+// #include <math.h>
+// #include <cmath>
+// #include <stdio.h>
+
+#include "ignition/math/PID.hh"
+
+using namespace ignition;
+using namespace math;
+
+/////////////////////////////////////////////////
+PID::PID(double _p, double _i, double _d, double _imax, double _imin,
+         double _cmdMax, double _cmdMin)
+  : pGain(_p), iGain(_i), dGain(_d), iMax(_imax), iMin(_imin),
+    cmdMax(_cmdMax), cmdMin(_cmdMin)
+{
+  this->Reset();
+}
+
+/////////////////////////////////////////////////
+PID::~PID()
+{
+}
+
+/////////////////////////////////////////////////
+void PID::Init(double _p, double _i, double _d, double _imax, double _imin,
+         double _cmdMax, double _cmdMin)
+{
+  this->pGain = _p;
+  this->iGain = _i;
+  this->dGain = _d;
+  this->iMax = _imax;
+  this->iMin = _imin;
+  this->cmdMax = _cmdMax;
+  this->cmdMin = _cmdMin;
+
+  this->Reset();
+}
+
+/////////////////////////////////////////////////
+void PID::SetPGain(double _p)
+{
+  this->pGain = _p;
+}
+
+/////////////////////////////////////////////////
+void PID::SetIGain(double _i)
+{
+  this->iGain = _i;
+}
+
+/////////////////////////////////////////////////
+void PID::SetDGain(double _d)
+{
+  this->dGain = _d;
+}
+
+/////////////////////////////////////////////////
+void PID::SetIMax(double _i)
+{
+  this->iMax = _i;
+}
+
+/////////////////////////////////////////////////
+void PID::SetIMin(double _i)
+{
+  this->iMin = _i;
+}
+
+/////////////////////////////////////////////////
+void PID::SetCmdMax(double _c)
+{
+  this->cmdMax = _c;
+}
+
+/////////////////////////////////////////////////
+void PID::SetCmdMin(double _c)
+{
+  this->cmdMin = _c;
+}
+
+/////////////////////////////////////////////////
+void PID::Reset()
+{
+  this->pErrLast = 0.0;
+  this->pErr = 0.0;
+  this->iErr = 0.0;
+  this->dErr = 0.0;
+  this->cmd = 0.0;
+}
+
+/////////////////////////////////////////////////
+double PID::Update(double _error, std::chrono::duration<double> _dt)
+{
+  double pTerm, dTerm, iTerm;
+  this->pErr = _error;
+
+  if (_dt == std::chrono::duration<double>(0) ||
+      ignition::math::isnan(_error) || std::isinf(_error))
+  {
+    return 0.0;
+  }
+
+  // Calculate proportional contribution to command
+  pTerm = this->pGain * this->pErr;
+
+  // Calculate the integral error
+  this->iErr = this->iErr + _dt.count() * this->pErr;
+
+  // Calculate integral contribution to command
+  iTerm = this->iGain * this->iErr;
+
+  // Limit iTerm so that the limit is meaningful in the output
+  if (iTerm > this->iMax)
+  {
+    std::cout << "A\n";
+    iTerm = this->iMax;
+    this->iErr = iTerm / this->iGain;
+  }
+  else if (iTerm < this->iMin)
+  {
+    std::cout << "B\n";
+    iTerm = this->iMin;
+    this->iErr = iTerm / this->iGain;
+  }
+
+  // Calculate the derivative error
+  if (_dt != std::chrono::duration<double>(0))
+  {
+    this->dErr = (this->pErr - this->pErrLast) / _dt.count();
+    this->pErrLast = this->pErr;
+  }
+
+  // Calculate derivative contribution to command
+  dTerm = this->dGain * this->dErr;
+  this->cmd = -pTerm - iTerm - dTerm;
+
+  // Check the command limits
+  if (!(this->cmdMax < this->cmdMin))
+      this->cmd = clamp(this->cmd, this->cmdMin, this->cmdMax);
+
+  return this->cmd;
+}
+
+/////////////////////////////////////////////////
+void PID::SetCmd(double _cmd)
+{
+  this->cmd = _cmd;
+}
+
+/////////////////////////////////////////////////
+double PID::Cmd()
+{
+  return this->cmd;
+}
+
+/////////////////////////////////////////////////
+void PID::Errors(double &_pe, double &_ie, double &_de)
+{
+  _pe = this->pErr;
+  _ie = this->iErr;
+  _de = this->dErr;
+}
+
+/////////////////////////////////////////////////
+double PID::PGain() const
+{
+  return this->pGain;
+}
+
+/////////////////////////////////////////////////
+double PID::IGain() const
+{
+  return this->iGain;
+}
+
+/////////////////////////////////////////////////
+double PID::DGain() const
+{
+  return this->dGain;
+}
+
+/////////////////////////////////////////////////
+double PID::IMax() const
+{
+  return this->iMax;
+}
+
+/////////////////////////////////////////////////
+double PID::IMin() const
+{
+  return this->iMin;
+}
+
+/////////////////////////////////////////////////
+double PID::CmdMax() const
+{
+  return this->cmdMax;
+}
+
+/////////////////////////////////////////////////
+double PID::CmdMin() const
+{
+  return this->cmdMin;
+}
---
+++ src/PID_TEST.cc
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2012-2014 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+
+#include "ignition/math/PID.hh"
+#include "ignition/math/Helpers.hh"
+
+using namespace ignition;
+
+/////////////////////////////////////////////////
+TEST(PidTest, PID)
+{
+  math::PID pid;
+  EXPECT_NEAR(pid.PGain(), 0, 1e-6);
+  EXPECT_NEAR(pid.IGain(), 0, 1e-6);
+  EXPECT_NEAR(pid.DGain(), 0, 1e-6);
+  EXPECT_NEAR(pid.IMax(), 0, 1e-6);
+  EXPECT_NEAR(pid.IMin(), 0, 1e-6);
+  EXPECT_NEAR(pid.CmdMax(), -1.0, 1e-6);
+  EXPECT_NEAR(pid.CmdMin(), 0, 1e-6);
+  EXPECT_NEAR(pid.Cmd(), 0, 1e-6);
+
+  math::PID pid2(1.0, 2.1, -4.5, 10.5, 1.4, 45, -35);
+  EXPECT_NEAR(pid2.PGain(), 1.0, 1e-6);
+  EXPECT_NEAR(pid2.IGain(), 2.1, 1e-6);
+  EXPECT_NEAR(pid2.DGain(), -4.5, 1e-6);
+  EXPECT_NEAR(pid2.IMax(), 10.5, 1e-6);
+  EXPECT_NEAR(pid2.IMin(), 1.4, 1e-6);
+  EXPECT_NEAR(pid2.CmdMax(), 45.0, 1e-6);
+  EXPECT_NEAR(pid2.CmdMin(), -35.0, 1e-6);
+  EXPECT_NEAR(pid2.Cmd(), 0.0, 1e-6);
+
+  pid.SetPGain(pid2.PGain());
+  pid.SetIGain(pid2.IGain());
+  pid.SetDGain(pid2.DGain());
+  pid.SetIMax(pid2.IMax());
+  pid.SetIMin(pid2.IMin());
+  pid.SetCmdMax(pid2.CmdMax());
+  pid.SetCmdMin(pid2.CmdMin());
+  pid.SetCmd(10.4);
+
+  EXPECT_NEAR(pid.PGain(), pid2.PGain(), 1e-6);
+  EXPECT_NEAR(pid.IGain(), pid2.IGain(), 1e-6);
+  EXPECT_NEAR(pid.DGain(), pid2.DGain(), 1e-6);
+  EXPECT_NEAR(pid.IMax(), pid2.IMax(), 1e-6);
+  EXPECT_NEAR(pid.IMin(), pid2.IMin(), 1e-6);
+  EXPECT_NEAR(pid.CmdMax(), pid2.CmdMax(), 1e-6);
+  EXPECT_NEAR(pid.CmdMin(), pid2.CmdMin(), 1e-6);
+  EXPECT_NEAR(pid.Cmd(), 10.4, 1e-6);
+
+  math::PID pid3;
+  pid3 = pid;
+  EXPECT_NEAR(pid.PGain(), pid3.PGain(), 1e-6);
+  EXPECT_NEAR(pid.IGain(), pid3.IGain(), 1e-6);
+  EXPECT_NEAR(pid.DGain(), pid3.DGain(), 1e-6);
+  EXPECT_NEAR(pid.IMax(), pid3.IMax(), 1e-6);
+  EXPECT_NEAR(pid.IMin(), pid3.IMin(), 1e-6);
+  EXPECT_NEAR(pid.CmdMax(), pid3.CmdMax(), 1e-6);
+  EXPECT_NEAR(pid.CmdMin(), pid3.CmdMin(), 1e-6);
+  EXPECT_NEAR(pid.Cmd(), pid3.Cmd(), 1e-6);
+
+  double pe, ie, de;
+  pid.Errors(pe, ie, de);
+  EXPECT_NEAR(pe, 0.0, 1e-6);
+  EXPECT_NEAR(ie, 0.0, 1e-6);
+  EXPECT_NEAR(de, 0.0, 1e-6);
+}
+
+/////////////////////////////////////////////////
+TEST(PidTest, EqualOperatorCornerCase)
+{
+  math::PID pid(1.0, 2.1, -4.5, 10.5, 1.4, 45, -35);
+  EXPECT_NEAR(pid.PGain(), 1.0, 1e-6);
+  EXPECT_NEAR(pid.IGain(), 2.1, 1e-6);
+  EXPECT_NEAR(pid.DGain(), -4.5, 1e-6);
+  EXPECT_NEAR(pid.IMax(), 10.5, 1e-6);
+  EXPECT_NEAR(pid.IMin(), 1.4, 1e-6);
+  EXPECT_NEAR(pid.CmdMax(), 45.0, 1e-6);
+  EXPECT_NEAR(pid.CmdMin(), -35.0, 1e-6);
+  EXPECT_NEAR(pid.Cmd(), 0.0, 1e-6);
+
+  pid = pid;
+
+  EXPECT_NEAR(pid.PGain(), 1.0, 1e-6);
+  EXPECT_NEAR(pid.IGain(), 2.1, 1e-6);
+  EXPECT_NEAR(pid.DGain(), -4.5, 1e-6);
+  EXPECT_NEAR(pid.IMax(), 10.5, 1e-6);
+  EXPECT_NEAR(pid.IMin(), 1.4, 1e-6);
+  EXPECT_NEAR(pid.CmdMax(), 45.0, 1e-6);
+  EXPECT_NEAR(pid.CmdMin(), -35.0, 1e-6);
+  EXPECT_NEAR(pid.Cmd(), 0.0, 1e-6);
+}
+
+/////////////////////////////////////////////////
+TEST(PidTest, Update)
+{
+  math::PID pid;
+  pid.Init(1.0, 0.1, 0.5, 10, 0, 20, -20);
+
+  double result = pid.Update(5.0, std::chrono::duration<double>(0.0));
+  EXPECT_NEAR(result, 0.0, 1e-6);
+
+  result = pid.Update(5.0, std::chrono::duration<double>(10.0));
+  EXPECT_NEAR(result, -10.25, 1e-6);
+
+  double pe, ie, de;
+  pid.Errors(pe, ie, de);
+  EXPECT_NEAR(pe, 5, 1e-6);
+  EXPECT_NEAR(ie, 50, 1e-6);
+  EXPECT_NEAR(de, 0.5, 1e-6);
+
+  // Test max integral term
+  pid.SetIMax(0.2);
+  pid.SetIGain(10.0);
+  result = pid.Update(5.0, std::chrono::duration<double>(10.0));
+  EXPECT_NEAR(result, -5.2, 1e-6);
+  pid.Errors(pe, ie, de);
+  EXPECT_NEAR(pe, 5, 1e-6);
+  EXPECT_NEAR(ie, 0.02, 1e-6);
+  EXPECT_NEAR(de, 0.0, 1e-6);
+
+  // Test min integral term
+  pid.SetIMax(20);
+  pid.SetIMin(1.4);
+  pid.SetIGain(0.01);
+  result = pid.Update(5.0, std::chrono::duration<double>(10.0));
+  EXPECT_NEAR(result, -6.4, 1e-6);
+  pid.Errors(pe, ie, de);
+  EXPECT_NEAR(pe, 5, 1e-6);
+  EXPECT_NEAR(ie, 140, 1e-6);
+  EXPECT_NEAR(de, 0.0, 1e-6);
+}
