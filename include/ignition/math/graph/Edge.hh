108211a13c928abd86397569be67ca6a235ea556
Carlos Agüero <caguero@osrfoundation.org>
2017-07-07 15:53:38 -0700

    Rename header guard

--- include/ignition/math/graph/Edge.hh
+++ include/ignition/math/graph/Edge.hh
@@ -14,8 +14,8 @@
  * limitations under the License.
  *
 */
-#ifndef IGNITION_MATH_EDGE_HH_
-#define IGNITION_MATH_EDGE_HH_
+#ifndef IGNITION_MATH_GRAPH_EDGE_HH_
+#define IGNITION_MATH_GRAPH_EDGE_HH_
 
 // uint64_t
 #include <cstdint>
