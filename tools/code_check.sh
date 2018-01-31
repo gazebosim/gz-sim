60512561c69277889769f3348044c85a5efba795
Steve Peters <scpeters@openrobotics.org>
2018-01-31 12:54:31 -0800

    remove cppcheck defines, since that was messing up noExplicitConstructor suppressions

--- tools/code_check.sh
+++ tools/code_check.sh
@@ -74,7 +74,7 @@ if [ $CPPCHECK_LT_157 -eq 0 ]; then
   CPPCHECK_BASE="$CPPCHECK_BASE --language=c++"
 fi
 CPPCHECK_INCLUDES="-I ./include -I $builddir -I test -I ./include/ignition/math"
-CPPCHECK_RULES="-DIGNITION_MATH_VISIBLE"\
+CPPCHECK_RULES="-UM_PI"\
 " --rule-file=./tools/cppcheck_rules/header_guard.rule"\
 " --rule-file=./tools/cppcheck_rules/namespace_AZ.rule"
 CPPCHECK_CMD1A="-j 4 --enable=style,performance,portability,information"
