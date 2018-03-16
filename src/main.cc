875e213ab441848c4c69150cf79a3d30cfe5fe99
nkoenig <nkoenig@osrfoundation.org>
2018-03-16 14:28:43 -0700

    Added main and server::run

---
+++ src/main.cc
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <signal.h>
+#include <gflags/gflags.h>
+#include <iostream>
+#include <ignition/common/Console.hh>
+#include "ignition/gazebo/config.hh"
+#include "ignition/gazebo/Server.hh"
+
+// Gflag command line argument definitions
+// This flag is an abbreviation for the longer gflags built-in help flag.
+DEFINE_bool(h, false, "");
+DEFINE_int32(verbose, 1, "");
+DEFINE_int32(v, 1, "");
+
+//////////////////////////////////////////////////
+void Help()
+{
+  std::cout
+  << "gazebo -- Run the Gazebo server and GUI." << std::endl
+  << std::endl
+  << "`gazebo` [options] <world_file>" << std::endl
+  << std::endl
+  << std::endl
+  << "Options:" << std::endl
+  << "  -h [ --help ]                 Print help message." << std::endl
+  << "  --version                     Print version information." << std::endl
+  << "  -v [--verbose] arg            Adjust the level of console output (0~4)."
+  << std::endl
+  << "  -f [ --file ] FILE            SDF file to load on start." << std::endl
+  << std::endl;
+}
+
+//////////////////////////////////////////////////
+void Version()
+{
+  std::cout << IGNITION_GAZEBO_VERSION_HEADER << std::endl;
+}
+
+//////////////////////////////////////////////////
+void Verbose()
+{
+  // This also applies to all upstream libraries using ignition::common::Console
+  ignition::common::Console::SetVerbosity(FLAGS_verbose);
+}
+
+//////////////////////////////////////////////////
+static bool VerbosityValidator(const char */*_flagname*/, int _value)
+{
+  return _value >= 0 && _value <= 4;
+}
+
+//////////////////////////////////////////////////
+int main(int _argc, char **_argv)
+{
+  // Register validators
+  gflags::RegisterFlagValidator(&FLAGS_verbose, &VerbosityValidator);
+  gflags::RegisterFlagValidator(&FLAGS_v, &VerbosityValidator);
+
+  // Parse command line
+  gflags::ParseCommandLineNonHelpFlags(&_argc, &_argv, true);
+
+  // Hold info as we parse it
+  gflags::CommandLineFlagInfo info;
+
+  // Help
+  // Parse out the help flag in such a way that the full help text
+  // is suppressed: if --help* or -h is specified, override the default
+  // help behavior and turn on --helpmatch, to only shows help for the
+  // current executable (instead of showing a huge list of gflags built-ins).
+  gflags::GetCommandLineFlagInfo("help", &info);
+  bool showHelp = FLAGS_h || (info.current_value == "true");
+
+  // Version
+  gflags::GetCommandLineFlagInfo("version", &info);
+  bool showVersion = (info.current_value == "true");
+
+  // Verbosity
+  gflags::GetCommandLineFlagInfo("verbose", &info);
+  if (info.is_default)
+  {
+    gflags::GetCommandLineFlagInfo("v", &info);
+    if (!info.is_default)
+      FLAGS_verbose = FLAGS_v;
+  }
+
+  // If help message is requested, substitute in the override help function.
+  if (showHelp)
+  {
+    gflags::SetCommandLineOption("help", "false");
+    gflags::SetCommandLineOption("helpshort", "false");
+    gflags::SetCommandLineOption("helpfull", "false");
+    gflags::SetCommandLineOption("helpmatch", "");
+    Help();
+  }
+  // If version is requested, override with custom version print function.
+  else if (showVersion)
+  {
+    gflags::SetCommandLineOption("version", "false");
+    Version();
+  }
+  // Run Gazebo
+  else
+  {
+    // Set verbosity level
+    Verbose();
+
+    // Create the Gazebo server
+    ignition::gazebo::Server server;
+
+    // Run the server, and block.
+    server.Run(true);
+  }
+
+  igndbg << "Shutting down" << std::endl;
+  return 0;
+}
