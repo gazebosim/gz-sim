d439e87749a460eb015555e059f7b4cd6daf1b3a
Jose Luis Rivero <jrivero@osrfoundation.org>
2018-04-02 11:28:01 +0000

    Added precission for non C++17 to fix problems with mipsel/hppa
    See https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=888220#10

--- src/Helpers.cc
+++ src/Helpers.cc
@@ -48,7 +48,8 @@ namespace ignition
       // compilers from 6.x/7.x series. The native support for C++ is defined
       // in C++17 See https://bugzilla.redhat.com/show_bug.cgi?id=1321986
 #if (__cplusplus < 201703L)
-      uint64_t sqrt = static_cast<uint64_t>(std::sqrt(_key) - 0.000001);
+      uint64_t sqrt = static_cast<uint64_t>(std::sqrt(_key) -
+          0.00000000000000000001);
 #else
       uint64_t sqrt = static_cast<uint64_t>(std::sqrt(_key) - 0x1p-20);
 #endif
