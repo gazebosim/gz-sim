228cc541fba6d763dc1de00a1dd467e52bb20003
chapulina <burajiru.no.chapulina@gmail.com>
2018-04-07 08:30:52 -0300

    Small tweaks and a failing test case

--- src/ServerPrivate.cc
+++ src/ServerPrivate.cc
@@ -15,9 +15,10 @@
  *
 */
 
-#include "ServerPrivate.hh"
 #include <signal.h>
 #include <ignition/common/Console.hh>
+
+#include "ServerPrivate.hh"
 
 using namespace ignition;
 using namespace gazebo;
@@ -69,7 +70,7 @@ void ServerPrivate::Run(const uint64_t _iterations)
   this->running = true;
 
   uint64_t startingIterations = this->iterations;
-  // Execute all the system until we are told to stop, or the number of
+  // Execute all the systems until we are told to stop, or the number of
   // iterations is reached.
   for (; this->running && (_iterations == 0 ||
                            this->iterations < _iterations + startingIterations);
