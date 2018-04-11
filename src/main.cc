f920a20b11e0e6ae954ec77f0fd2621da3c0efd8
nate koenig <natekoenig@gmail.com>
2018-04-10 18:44:15 -0700

    added a main

---
+++ src/main.cc
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <signal.h>
+#include <gflags/gflags.h>
+#include <ignition/common/Console.hh>
+
+#include <iostream>
+
+#include "ignition/gazebo/config.hh"
+
+// Gflag command line argument definitions
+// This flag is an abbreviation for the longer gflags built-in help flag.
+DEFINE_bool(h, false, "");
+DEFINE_int32(verbose, 1, "");
+DEFINE_int32(v, 1, "");
+DEFINE_uint64(iterations, 0, "Number of iterations to execute.");
+
+//////////////////////////////////////////////////
+void Help()
+{
+  std::cout
+  << "gazebo -- Run the Gazebo server and GUI." << std::endl
+  << std::endl
+  << "`gazebo` [options] <world_file>" << std::endl
+  << std::endl
+  << std::endl
+  << "Options:" << std::endl
+  << "  -h [ --help ]          Print help message."
+  << std::endl
+  << "  --version              Print version information."
+  << std::endl
+  << "  -v [--verbose] arg     Adjust the level of console output (0~4)."
+  << " The default verbosity is 1"
+  << std::endl
+  << "  --iterations arg       Number of iterations to execute."
+  << std::endl
+  << std::endl;
+}
+
+//////////////////////////////////////////////////
+void Version()
+{
+  std::cout << IGNITION_GAZEBO_VERSION_HEADER << std::endl;
+}
+
+//////////////////////////////////////////////////
+static bool VerbosityValidator(const char */*_flagname*/, int _value)
+{
+  return _value >= 0 && _value <= 4;
+}
+
+//////////////////////////////////////////////////
+int main(int _argc, char **_argv)
+{
+  // Register validators
+  gflags::RegisterFlagValidator(&FLAGS_verbose, &VerbosityValidator);
+  gflags::RegisterFlagValidator(&FLAGS_v, &VerbosityValidator);
+
+  // Parse command line
+  gflags::ParseCommandLineNonHelpFlags(&_argc, &_argv, true);
+
+  // Hold info as we parse it
+  gflags::CommandLineFlagInfo info;
+
+  // Help
+  // Parse out the help flag in such a way that the full help text
+  // is suppressed: if --help* or -h is specified, override the default
+  // help behavior and turn on --helpmatch, to only shows help for the
+  // current executable (instead of showing a huge list of gflags built-ins).
+  gflags::GetCommandLineFlagInfo("help", &info);
+  bool showHelp = FLAGS_h || (info.current_value == "true");
+
+  // Version
+  gflags::GetCommandLineFlagInfo("version", &info);
+  bool showVersion = (info.current_value == "true");
+
+  // Verbosity
+  gflags::GetCommandLineFlagInfo("verbose", &info);
+  if (info.is_default)
+  {
+    gflags::GetCommandLineFlagInfo("v", &info);
+    if (!info.is_default)
+      FLAGS_verbose = FLAGS_v;
+    else
+      FLAGS_verbose = 1;
+  }
+
+  // If help message is requested, substitute in the override help function.
+  if (showHelp)
+  {
+    gflags::SetCommandLineOption("help", "false");
+    gflags::SetCommandLineOption("helpshort", "false");
+    gflags::SetCommandLineOption("helpfull", "false");
+    gflags::SetCommandLineOption("helpmatch", "");
+    Help();
+  }
+  // If version is requested, override with custom version print function.
+  else if (showVersion)
+  {
+    gflags::SetCommandLineOption("version", "false");
+    Version();
+  }
+  // Run Gazebo
+  else
+  {
+    // Set verbosity
+    ignition::common::Console::SetVerbosity(FLAGS_verbose);
+    ignmsg << "Ignition Gazebo v" << IGNITION_GAZEBO_VERSION_FULL << std::endl;
+  }
+
+  igndbg << "Shutting down" << std::endl;
+  return 0;
+}
