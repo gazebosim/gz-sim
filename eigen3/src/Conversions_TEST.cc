1a0f19715e1f36413c4fc0c05c45a2d2295c2a02
Steve Peters <scpeters@openrobotics.org>
2018-05-10 11:30:28 -0700

    tests for Quaternion, Matrix3, and Pose3

--- eigen3/src/Conversions_TEST.cc
+++ eigen3/src/Conversions_TEST.cc
@@ -45,3 +45,105 @@ TEST(EigenConversions, ConvertVector3)
   }
 }
 
+/////////////////////////////////////////////////
+/// Check Quaternion conversions
+TEST(EigenConversions, ConvertQuaternion)
+{
+  {
+    ignition::math::Quaterniond iQuat, iQuat2;
+    Eigen::Quaterniond eQuat = ignition::math::eigen3::convert(iQuat);
+    EXPECT_DOUBLE_EQ(1, eQuat.w());
+    EXPECT_DOUBLE_EQ(0, eQuat.x());
+    EXPECT_DOUBLE_EQ(0, eQuat.y());
+    EXPECT_DOUBLE_EQ(0, eQuat.z());
+    iQuat2 = ignition::math::eigen3::convert(eQuat);
+    EXPECT_EQ(iQuat, iQuat2);
+  }
+
+  {
+    ignition::math::Quaterniond iQuat(0.1, 0.2, 0.3), iQuat2;
+    Eigen::Quaterniond eQuat = ignition::math::eigen3::convert(iQuat);
+    EXPECT_DOUBLE_EQ(iQuat.W(), eQuat.w());
+    EXPECT_DOUBLE_EQ(iQuat.X(), eQuat.x());
+    EXPECT_DOUBLE_EQ(iQuat.Y(), eQuat.y());
+    EXPECT_DOUBLE_EQ(iQuat.Z(), eQuat.z());
+    iQuat2 = ignition::math::eigen3::convert(eQuat);
+    EXPECT_EQ(iQuat, iQuat2);
+  }
+}
+
+/////////////////////////////////////////////////
+/// Check Matrix3 conversions
+TEST(EigenConversions, ConvertMatrix3)
+{
+  {
+    ignition::math::Matrix3d iMat, iMat2;
+    Eigen::Matrix3d eMat = ignition::math::eigen3::convert(iMat);
+    EXPECT_DOUBLE_EQ(0, eMat(0, 0));
+    EXPECT_DOUBLE_EQ(0, eMat(0, 1));
+    EXPECT_DOUBLE_EQ(0, eMat(0, 2));
+    EXPECT_DOUBLE_EQ(0, eMat(1, 0));
+    EXPECT_DOUBLE_EQ(0, eMat(1, 1));
+    EXPECT_DOUBLE_EQ(0, eMat(1, 2));
+    EXPECT_DOUBLE_EQ(0, eMat(2, 0));
+    EXPECT_DOUBLE_EQ(0, eMat(2, 1));
+    EXPECT_DOUBLE_EQ(0, eMat(2, 2));
+    iMat2 = ignition::math::eigen3::convert(eMat);
+    EXPECT_EQ(iMat, iMat2);
+  }
+
+
+  {
+    ignition::math::Matrix3d iMat(1, 2, 3, 4, 5, 6, 7, 8, 9), iMat2;
+    Eigen::Matrix3d eMat = ignition::math::eigen3::convert(iMat);
+    EXPECT_DOUBLE_EQ(iMat(0, 0), eMat(0, 0));
+    EXPECT_DOUBLE_EQ(iMat(0, 1), eMat(0, 1));
+    EXPECT_DOUBLE_EQ(iMat(0, 2), eMat(0, 2));
+    EXPECT_DOUBLE_EQ(iMat(1, 0), eMat(1, 0));
+    EXPECT_DOUBLE_EQ(iMat(1, 1), eMat(1, 1));
+    EXPECT_DOUBLE_EQ(iMat(1, 2), eMat(1, 2));
+    EXPECT_DOUBLE_EQ(iMat(2, 0), eMat(2, 0));
+    EXPECT_DOUBLE_EQ(iMat(2, 1), eMat(2, 1));
+    EXPECT_DOUBLE_EQ(iMat(2, 2), eMat(2, 2));
+    iMat2 = ignition::math::eigen3::convert(eMat);
+    EXPECT_EQ(iMat, iMat2);
+  }
+}
+
+/////////////////////////////////////////////////
+/// Check Pose conversions
+TEST(EigenConversions, ConvertPose3)
+{
+  {
+    ignition::math::Pose3d iPose, iPose2;
+    Eigen::Isometry3d ePose = ignition::math::eigen3::convert(iPose);
+    Eigen::Vector3d eVec = ePose.translation();
+    EXPECT_DOUBLE_EQ(0, eVec[0]);
+    EXPECT_DOUBLE_EQ(0, eVec[1]);
+    EXPECT_DOUBLE_EQ(0, eVec[2]);
+    Eigen::Quaterniond eQuat(ePose.linear());
+    EXPECT_DOUBLE_EQ(1, eQuat.w());
+    EXPECT_DOUBLE_EQ(0, eQuat.x());
+    EXPECT_DOUBLE_EQ(0, eQuat.y());
+    EXPECT_DOUBLE_EQ(0, eQuat.z());
+    iPose2 = ignition::math::eigen3::convert(ePose);
+    EXPECT_EQ(iPose, iPose2);
+  }
+
+  {
+    ignition::math::Pose3d iPose(105.4, 3.1, -0.34, 3.14/8, 3.14/16, -3.14/2);
+    ignition::math::Pose3d iPose2;
+    Eigen::Isometry3d ePose = ignition::math::eigen3::convert(iPose);
+    Eigen::Vector3d eVec = ePose.translation();
+    EXPECT_DOUBLE_EQ(iPose.Pos()[0], eVec[0]);
+    EXPECT_DOUBLE_EQ(iPose.Pos()[1], eVec[1]);
+    EXPECT_DOUBLE_EQ(iPose.Pos()[2], eVec[2]);
+    Eigen::Quaterniond eQuat(ePose.linear());
+    EXPECT_DOUBLE_EQ(iPose.Rot().W(), eQuat.w());
+    EXPECT_DOUBLE_EQ(iPose.Rot().X(), eQuat.x());
+    EXPECT_DOUBLE_EQ(iPose.Rot().Y(), eQuat.y());
+    EXPECT_DOUBLE_EQ(iPose.Rot().Z(), eQuat.z());
+    iPose2 = ignition::math::eigen3::convert(ePose);
+    EXPECT_EQ(iPose, iPose2);
+  }
+}
