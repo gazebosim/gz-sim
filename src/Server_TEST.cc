6015f00aafcce3ed3c036600cf4a9b4963c149e2
Nate Koenig <natekoenig@gmail.com>
2018-07-31 16:43:26 -0700

    Removed world stats system

--- src/Server_TEST.cc
+++ src/Server_TEST.cc
@@ -37,7 +37,7 @@ TEST_P(ServerFixture, Constructor)
   EXPECT_FALSE(server.Running());
   EXPECT_EQ(0u, server.IterationCount());
   EXPECT_EQ(0u, server.EntityCount());
-  EXPECT_EQ(2u, server.SystemCount());
+  EXPECT_EQ(1u, server.SystemCount());
 
   gazebo::EntityComponentManager &mgr = server.EntityComponentMgr();
   EXPECT_EQ(0u, mgr.EntityCount());
