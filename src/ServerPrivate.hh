7db8f5857d634b6b4f2a5d868c08c29cb235c7c1
Nate Koenig <natekoenig@gmail.com>
2018-08-08 07:53:17 -0700

    Init and update systems in parallel, reduce unit_server iterations.

--- src/ServerPrivate.hh
+++ src/ServerPrivate.hh
@@ -31,6 +31,7 @@
 
 #include <ignition/transport/Node.hh>
 #include <ignition/common/SignalHandler.hh>
+#include <ignition/common/WorkerPool.hh>
 
 #include "ignition/gazebo/Entity.hh"
 #include "ignition/gazebo/EntityComponentManager.hh"
@@ -82,6 +83,9 @@ namespace ignition
       /// \param[in] _root SDF root object.
       public: void CreateEntities(const sdf::Root &_root);
 
+      /// \brief Initialize all the systems.
+      public: void InitSystems();
+
       /// \brief Signal handler callback
       /// \param[in] _sig The signal number
       private: void OnSignal(int _sig);
@@ -108,9 +112,11 @@ namespace ignition
       /// \brief Manager of all components.
       public: std::shared_ptr<EntityComponentManager> entityCompMgr;
 
-      public: EntityQueryRegistrar entityQueryRegistrar;
-
+      /// \brief All the systems.
       public: std::vector<SystemInternal> systems;
+
+      /// \brief A pool of worker threads.
+      public: common::WorkerPool workerPool;
     };
     }
   }
