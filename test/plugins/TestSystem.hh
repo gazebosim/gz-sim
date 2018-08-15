98f88e89ed51b300ca1a083d11807af2fe8fc731
Michael Carroll <michael@openrobotics.org>
2018-08-15 10:57:57 -0500

    Load systems by name, in the case that multiple are in the same library.

--- test/plugins/TestSystem.hh
+++ test/plugins/TestSystem.hh
@@ -1,20 +1,24 @@
 #ifndef IGNITION_GAZEBO_TEST_TEST_SYSTEM_HH_
 #define IGNITION_GAZEBO_TEST_TEST_SYSTEM_HH_
 
+#include <ignition/gazebo/config.hh>
+#include <ignition/gazebo/EntityQueryRegistrar.hh>
 #include <ignition/gazebo/System.hh>
 
 namespace ignition
 {
   namespace gazebo
   {
+    inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
     class TestSystem: public System
     {
       public: TestSystem();
 
       public: virtual ~TestSystem();
 
-      public: virtual void Init(EntityQueryRegistrar &_registrar) override final {};
+      public: virtual void Init(EntityQueryRegistrar &_registrar) override final;
     };
+    }
   }
 }
 
