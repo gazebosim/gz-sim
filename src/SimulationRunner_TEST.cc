37d4b2a56add43977597ab380ec71a7ffe06b91d
chapulina <burajiru.no.chapulina@gmail.com>
2018-08-22 18:49:37 -0700

    parent entity component

--- src/SimulationRunner_TEST.cc
+++ src/SimulationRunner_TEST.cc
@@ -24,6 +24,7 @@
 #include "ignition/gazebo/components/Link.hh"
 #include "ignition/gazebo/components/Model.hh"
 #include "ignition/gazebo/components/Name.hh"
+#include "ignition/gazebo/components/ParentEntity.hh"
 #include "ignition/gazebo/components/Pose.hh"
 #include "ignition/gazebo/components/Visual.hh"
 #include "ignition/gazebo/components/World.hh"
@@ -60,15 +61,20 @@ TEST_P(SimulationRunnerTest, CreateEntities)
       EntityComponentManager::ComponentType<components::Collision>()));
   EXPECT_TRUE(runner.entityCompMgr.HasComponentType(
       EntityComponentManager::ComponentType<components::Visual>()));
+  EXPECT_TRUE(runner.entityCompMgr.HasComponentType(
+      EntityComponentManager::ComponentType<components::Name>()));
+  EXPECT_TRUE(runner.entityCompMgr.HasComponentType(
+      EntityComponentManager::ComponentType<components::ParentEntity>()));
 
   // Check entities
   EXPECT_EQ(13u, runner.entityCompMgr.EntityCount());
 
   // Check worlds
   unsigned int worldCount{0};
+  EntityId worldEntity = kNullEntity;
   runner.entityCompMgr.Each<components::World,
                             components::Name>(
-    [&](const EntityId &/*_entity*/,
+    [&](const EntityId &_entity,
         const components::World *_world,
         const components::Name *_name)
     {
@@ -81,9 +87,12 @@ TEST_P(SimulationRunnerTest, CreateEntities)
       EXPECT_EQ("default", _name->Data());
 
       worldCount++;
+
+      worldEntity = _entity;
     });
 
   EXPECT_EQ(1u, worldCount);
+  EXPECT_NE(kNullEntity, worldEntity);
 
   // Check models
   unsigned int modelCount{0};
@@ -92,10 +101,12 @@ TEST_P(SimulationRunnerTest, CreateEntities)
   EntityId sphModelEntity = kNullEntity;
   runner.entityCompMgr.Each<components::Model,
                             components::Pose,
+                            components::ParentEntity,
                             components::Name>(
     [&](const EntityId &_entity,
         const components::Model *_model,
         const components::Pose *_pose,
+        const components::ParentEntity *_parent,
         const components::Name *_name)
     {
       // \todo(louise) Fix Each so it only returns matching entities
@@ -104,25 +115,30 @@ TEST_P(SimulationRunnerTest, CreateEntities)
 
       ASSERT_NE(nullptr, _model);
       ASSERT_NE(nullptr, _pose);
+      ASSERT_NE(nullptr, _parent);
       ASSERT_NE(nullptr, _name);
 
       modelCount++;
 
+      EXPECT_EQ(worldEntity, _parent->Id());
       if (modelCount == 1)
       {
-        EXPECT_EQ(ignition::math::Pose3d(1, 2, 3, 0, 0, 1), _pose->Data());
+        EXPECT_EQ(ignition::math::Pose3d(1, 2, 3, 0, 0, 1),
+            _pose->Data());
         EXPECT_EQ("box", _name->Data());
         boxModelEntity = _entity;
       }
       else if (modelCount == 2)
       {
-        EXPECT_EQ(ignition::math::Pose3d(-1, -2, -3, 0, 0, 1), _pose->Data());
+        EXPECT_EQ(ignition::math::Pose3d(-1, -2, -3, 0, 0, 1),
+            _pose->Data());
         EXPECT_EQ("cylinder", _name->Data());
         cylModelEntity = _entity;
       }
       else if (modelCount == 3)
       {
-        EXPECT_EQ(ignition::math::Pose3d(0, 0, 0, 0, 0, 1), _pose->Data());
+        EXPECT_EQ(ignition::math::Pose3d(0, 0, 0, 0, 0, 1),
+            _pose->Data());
         EXPECT_EQ("sphere", _name->Data());
         sphModelEntity = _entity;
       }
@@ -135,12 +151,17 @@ TEST_P(SimulationRunnerTest, CreateEntities)
 
   // Check links
   unsigned int linkCount{0};
+  EntityId boxLinkEntity = kNullEntity;
+  EntityId cylLinkEntity = kNullEntity;
+  EntityId sphLinkEntity = kNullEntity;
   runner.entityCompMgr.Each<components::Link,
                             components::Pose,
+                            components::ParentEntity,
                             components::Name>(
-    [&](const EntityId &/*_entity*/,
+    [&](const EntityId &_entity,
         const components::Link *_link,
         const components::Pose *_pose,
+        const components::ParentEntity *_parent,
         const components::Name *_name)
     {
       // \todo(louise) Fix Each so it only returns matching entities
@@ -149,23 +170,52 @@ TEST_P(SimulationRunnerTest, CreateEntities)
 
       ASSERT_NE(nullptr, _link);
       ASSERT_NE(nullptr, _pose);
+      ASSERT_NE(nullptr, _parent);
       ASSERT_NE(nullptr, _name);
 
-      EXPECT_EQ("link", _name->Data());
-
       linkCount++;
+
+      if (linkCount == 1)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.1, 0.1, 0.1, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("box_link", _name->Data());
+        EXPECT_EQ(boxModelEntity, _parent->Id());
+        boxLinkEntity = _entity;
+      }
+      else if (linkCount == 2)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.2, 0.2, 0.2, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("cylinder_link", _name->Data());
+        EXPECT_EQ(cylModelEntity, _parent->Id());
+        cylLinkEntity = _entity;
+      }
+      else if (linkCount == 3)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.3, 0.3, 0.3, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("sphere_link", _name->Data());
+        EXPECT_EQ(sphModelEntity, _parent->Id());
+        sphLinkEntity = _entity;
+      }
     });
 
-  EXPECT_EQ(3u, modelCount);
+  EXPECT_EQ(3u, linkCount);
+  EXPECT_NE(kNullEntity, boxLinkEntity);
+  EXPECT_NE(kNullEntity, cylLinkEntity);
+  EXPECT_NE(kNullEntity, sphLinkEntity);
 
   // Check collisions
   unsigned int collisionCount{0};
   runner.entityCompMgr.Each<components::Collision,
                             components::Pose,
+                            components::ParentEntity,
                             components::Name>(
     [&](const EntityId &/*_entity*/,
         const components::Collision *_collision,
         const components::Pose *_pose,
+        const components::ParentEntity *_parent,
         const components::Name *_name)
     {
       // \todo(louise) Fix Each so it only returns matching entities
@@ -174,11 +224,32 @@ TEST_P(SimulationRunnerTest, CreateEntities)
 
       ASSERT_NE(nullptr, _collision);
       ASSERT_NE(nullptr, _pose);
+      ASSERT_NE(nullptr, _parent);
       ASSERT_NE(nullptr, _name);
 
-      EXPECT_EQ("col", _name->Data());
-
       collisionCount++;
+
+      if (collisionCount == 1)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.11, 0.11, 0.11, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("box_collision", _name->Data());
+        EXPECT_EQ(boxLinkEntity, _parent->Id());
+      }
+      else if (collisionCount == 2)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.21, 0.21, 0.21, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("cylinder_collision", _name->Data());
+        EXPECT_EQ(cylLinkEntity, _parent->Id());
+      }
+      else if (collisionCount == 3)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.31, 0.31, 0.31, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("sphere_collision", _name->Data());
+        EXPECT_EQ(sphLinkEntity, _parent->Id());
+      }
     });
 
   EXPECT_EQ(3u, collisionCount);
@@ -187,10 +258,12 @@ TEST_P(SimulationRunnerTest, CreateEntities)
   unsigned int visualCount{0};
   runner.entityCompMgr.Each<components::Visual,
                             components::Pose,
+                            components::ParentEntity,
                             components::Name>(
     [&](const EntityId &/*_entity*/,
         const components::Visual *_visual,
         const components::Pose *_pose,
+        const components::ParentEntity *_parent,
         const components::Name *_name)
     {
       // \todo(louise) Fix Each so it only returns matching entities
@@ -199,11 +272,32 @@ TEST_P(SimulationRunnerTest, CreateEntities)
 
       ASSERT_NE(nullptr, _visual);
       ASSERT_NE(nullptr, _pose);
+      ASSERT_NE(nullptr, _parent);
       ASSERT_NE(nullptr, _name);
 
-      EXPECT_EQ("vis", _name->Data());
-
       visualCount++;
+
+      if (visualCount == 1)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.12, 0.12, 0.12, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("box_visual", _name->Data());
+        EXPECT_EQ(boxLinkEntity, _parent->Id());
+      }
+      else if (visualCount == 2)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.22, 0.22, 0.22, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("cylinder_visual", _name->Data());
+        EXPECT_EQ(cylLinkEntity, _parent->Id());
+      }
+      else if (visualCount == 3)
+      {
+        EXPECT_EQ(ignition::math::Pose3d(0.32, 0.32, 0.32, 0, 0, 0),
+            _pose->Data());
+        EXPECT_EQ("sphere_visual", _name->Data());
+        EXPECT_EQ(sphLinkEntity, _parent->Id());
+      }
     });
 
   EXPECT_EQ(3u, visualCount);
