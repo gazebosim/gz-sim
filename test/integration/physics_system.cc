c012256bf78199a7154a204078a7d93753a14a66
Addisu Z. Taddese <addisu@openrobotics.org>
2018-09-05 18:06:09 -0500

    Add physics_system test

---
+++ test/integration/physics_system.cc
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+#include <ignition/common/Console.hh>
+#include <sdf/Root.hh>
+#include <sdf/World.hh>
+
+#include "ignition/gazebo/Server.hh"
+#include "ignition/gazebo/test_config.hh"  // NOLINT(build/include)
+
+#include "ignition/gazebo/components/Collision.hh"
+#include "ignition/gazebo/components/Geometry.hh"
+#include "ignition/gazebo/components/Link.hh"
+#include "ignition/gazebo/components/Material.hh"
+#include "ignition/gazebo/components/Model.hh"
+#include "ignition/gazebo/components/Name.hh"
+#include "ignition/gazebo/components/ParentEntity.hh"
+#include "ignition/gazebo/components/Pose.hh"
+#include "ignition/gazebo/components/Visual.hh"
+#include "ignition/gazebo/components/World.hh"
+
+using namespace ignition;
+using namespace std::chrono_literals;
+
+class PhysicsSystemFixture : public ::testing::Test
+{
+  protected:
+    virtual void SetUp() override
+    {
+      // Augment the system plugin path.  In SetUp to avoid test order issues.
+      setenv("IGN_GAZEBO_SYSTEM_PLUGIN_PATH",
+             (std::string(PROJECT_BINARY_PATH) + "/lib").c_str(), 1);
+    }
+};
+
+/////////////////////////////////////////////////
+TEST_F(PhysicsSystemFixture, CreatePhysicsWorld)
+{
+  ignition::gazebo::ServerConfig serverConfig;
+
+  serverConfig.SetSdfFile(std::string(PROJECT_SOURCE_PATH) +
+      "/test/worlds/shapes.sdf");
+
+  gazebo::Server server(serverConfig);
+
+  server.SetUpdatePeriod(1ns);
+
+  for (uint64_t i = 1; i < 10; ++i)
+  {
+    EXPECT_FALSE(*server.Running());
+    server.Run(true, i);
+    EXPECT_FALSE(*server.Running());
+  }
+
+}
+
