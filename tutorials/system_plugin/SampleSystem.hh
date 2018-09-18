42e2c47684833faa309eb691044136737d13e4ce
Michael Carroll <michael@openrobotics.org>
2018-09-18 16:12:31 -0500

    Add tutorial for implementing a system plugin.

---
+++ tutorials/system_plugin/SampleSystem.hh
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef SYSTEM_PLUGIN_SAMPLESYSTEM_HH_
+#define SYSTEM_PLUGIN_SAMPLESYSTEM_HH_
+
+#include <ignition/gazebo/System.hh>
+
+namespace sample_system
+{
+  class SampleSystem:
+    public ignition::gazebo::System,
+    public ignition::gazebo::ISystemPostUpdate
+  {
+    public: SampleSystem();
+
+    public: ~SampleSystem() override;
+
+    public: void PostUpdate(const ignition::gazebo::UpdateInfo &_info,
+                const ignition::gazebo::EntityComponentManager &_ecm);
+  };
+
+  class SampleSystem2:
+    public ignition::gazebo::System,
+    public ignition::gazebo::ISystemPreUpdate,
+    public ignition::gazebo::ISystemUpdate,
+    public ignition::gazebo::ISystemPostUpdate
+  {
+    public: SampleSystem2();
+
+    public: ~SampleSystem2() override;
+
+    public: void PreUpdate(const ignition::gazebo::UpdateInfo &_info,
+                ignition::gazebo::EntityComponentManager &_ecm);
+
+    public: void Update(const ignition::gazebo::UpdateInfo &_info,
+                ignition::gazebo::EntityComponentManager &_ecm);
+
+    public: void PostUpdate(const ignition::gazebo::UpdateInfo &_info,
+                const ignition::gazebo::EntityComponentManager &_ecm);
+  };
+}
+
+#endif
