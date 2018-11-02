647efcfbbb194b41544fce65a04370e4f2bf0d4c
Addisu Z. Taddese <addisu@openrobotics.org>
2018-11-01 19:29:06 -0500

    Add integration test for revolute joint

--- test/integration/physics_system.cc
+++ test/integration/physics_system.cc
@@ -16,14 +16,19 @@
 */
 
 #include <gtest/gtest.h>
+
+#include <algorithm>
+#include <vector>
+
 #include <ignition/common/Console.hh>
-#include <sdf/Root.hh>
-#include <sdf/World.hh>
-#include <sdf/Model.hh>
-#include <sdf/Link.hh>
 #include <sdf/Collision.hh>
+#include <sdf/Cylinder.hh>
 #include <sdf/Geometry.hh>
+#include <sdf/Link.hh>
+#include <sdf/Model.hh>
+#include <sdf/Root.hh>
 #include <sdf/Sphere.hh>
+#include <sdf/World.hh>
 
 #include "ignition/gazebo/Server.hh"
 #include "ignition/gazebo/SystemManager.hh"
@@ -204,7 +209,7 @@ TEST_F(PhysicsSystemFixture, CanonicalLink)
   server.SetUpdatePeriod(1us);
 
   const std::string modelName{"canonical"};
-  std::vector<std::string> linksToCheck {"base_link", "link1", "link2"};
+  std::vector<std::string> linksToCheck{"base_link", "link1", "link2"};
 
   const sdf::Model *model = world->ModelByIndex(0);
 
@@ -252,4 +257,84 @@ TEST_F(PhysicsSystemFixture, CanonicalLink)
     // joints are fixed joints
     EXPECT_EQ(expectedLinPoses[link], postUpLinkPoses[link]) << link;
   }
+}
+
+/////////////////////////////////////////////////
+// Test physics integration with revolute joints
+TEST_F(PhysicsSystemFixture, RevoluteJoint)
+{
+  ignition::gazebo::ServerConfig serverConfig;
+
+  const auto sdfFile = std::string(PROJECT_SOURCE_PATH) +
+    "/test/worlds/revolute_joint.sdf";
+
+  sdf::Root root;
+  root.Load(sdfFile);
+  const sdf::World *world = root.WorldByIndex(0);
+  ASSERT_TRUE(nullptr != world);
+
+  serverConfig.SetSdfFile(sdfFile);
+
+  gazebo::Server server(serverConfig);
+
+  server.SetUpdatePeriod(1us);
+
+  const std::string modelName{"revolute_demo"};
+  const std::string rotatingLinkName{"link2"};
+
+  Relay testSystem;
+
+  std::vector<double> armDistances;
+
+  // The test checks if the link connected to the joint swings from side to
+  // side. To do this, we check that the maximum and minimum distances of the
+  // swinging arm from it's parent model frame. The maximum distance is when the
+  // arm is in its initial position. The minimum distance is when the arm is in
+  // line with the support arm.
+  testSystem.OnPostUpdate(
+    [&rotatingLinkName, &armDistances](const gazebo::UpdateInfo &,
+    const gazebo::EntityComponentManager &_ecm)
+    {
+      _ecm.Each<components::Link, components::Name, components::Pose>(
+        [&](const ignition::gazebo::EntityId &, const components::Link *,
+        const components::Name *_name, const components::Pose *_pose)->bool
+        {
+          if (rotatingLinkName == _name->Data())
+          {
+            auto pos = _pose->Data().Pos();
+            // we ignore the x axis to simplify distance comparisons
+            pos.X() = 0;
+            armDistances.push_back(pos.Length());
+          }
+          return true;
+        });
+    });
+
+  server.AddSystem(testSystem.systemPtr);
+  server.Run(true, 1000, false);
+
+  ASSERT_GT(armDistances.size(), 0u);
+
+  const sdf::Model *model = world->ModelByIndex(1);
+
+  auto getCylinderLength = [&model](const std::string &linkName)
+  {
+    auto collision = model->LinkByName(linkName)->CollisionByIndex(0);
+    auto cylinder = collision->Geom()->CylinderShape();
+    return cylinder->Length();
+  };
+
+  const double link1Length = getCylinderLength("link1");
+  const double link2Length = getCylinderLength("link2");
+  // divide by 2.0 because the position is the center of the link.
+  const double expMinDist = link1Length - link2Length/2.0;
+  auto link2InitialPos = model->LinkByName("link2")->Pose().Pos();
+  // we ignore the x axis to simplify distance comparisons
+  link2InitialPos.X() = 0;
+  const double expMaxDist = link2InitialPos.Length();
+
+  auto minmax = std::minmax_element(armDistances.begin(), armDistances.end());
+
+  EXPECT_NEAR(expMinDist, *minmax.first, 1e-3);
+  EXPECT_NEAR(expMaxDist, *minmax.second, 1e-3);
 }
