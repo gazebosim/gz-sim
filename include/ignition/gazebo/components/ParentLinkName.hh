f82efd40fa1f94d7718cc2c0646c0f99c4b64313
chapulina <burajiru.no.chapulina@gmail.com>
2018-12-10 21:23:44 -0800

    Fix components namespace

--- include/ignition/gazebo/components/ParentLinkName.hh
+++ include/ignition/gazebo/components/ParentLinkName.hh
@@ -27,14 +27,14 @@ namespace ignition
 {
 namespace gazebo
 {
+// Inline bracket to help doxygen filtering.
+inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
 namespace components
 {
-  // Inline bracket to help doxygen filtering.
-  inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
   /// \brief A component used to indicate that a model is parentlinkname (i.e.
   /// not moveable).
   using ParentLinkName = SimpleWrapper<std::string, class ParentLinkNameTag>;
-  }
+}
 }
 }
 }
