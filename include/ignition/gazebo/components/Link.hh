36b33d02e18c07d1f555001aaee71859c8e0dc93
chapulina <burajiru.no.chapulina@gmail.com>
2018-12-17 16:16:41 -0800

    Add equality operators to several component types

--- include/ignition/gazebo/components/Link.hh
+++ include/ignition/gazebo/components/Link.hh
@@ -20,6 +20,8 @@
 #include <ignition/gazebo/config.hh>
 #include <ignition/gazebo/Export.hh>
 
+#include "ignition/gazebo/components/TagWrapper.hh"
+
 namespace ignition
 {
 namespace gazebo
@@ -28,10 +30,8 @@ namespace gazebo
 inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
 namespace components
 {
-  /// \brief This component identifies an entity as being a link.
-  class IGNITION_GAZEBO_VISIBLE Link
-  {
-  };
+  /// \brief A component that identifies an entity as being a link.
+  using Link = TagWrapper<class LinkTag>;
 }
 }
 }
