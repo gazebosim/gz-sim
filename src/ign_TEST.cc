f2c4ad78a729c5a5c5d87ff3a03e113db2fa6a49
chapulina <burajiru.no.chapulina@gmail.com>
2018-12-21 16:22:00 -0800

    more tests

---
+++ src/ign_TEST.cc
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2018 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string>
+
+#include "ignition/gazebo/test_config.hh"  // NOLINT(build/include)
+
+/////////////////////////////////////////////////
+std::string custom_exec_str(std::string _cmd)
+{
+  _cmd += " 2>&1";
+  FILE *pipe = popen(_cmd.c_str(), "r");
+
+  if (!pipe)
+    return "ERROR";
+
+  char buffer[128];
+  std::string result = "";
+
+  while (!feof(pipe))
+  {
+    if (fgets(buffer, 128, pipe) != NULL)
+      result += buffer;
+  }
+
+  pclose(pipe);
+  return result;
+}
+
+const char *kPluginPath = "IGN_GAZEBO_SYSTEM_PLUGIN_PATH="
+    PROJECT_BINARY_PATH "/lib ";
+
+/////////////////////////////////////////////////
+TEST(CmdLine, Server)
+{
+  std::string cmd = std::string(kPluginPath) +
+    "ign-gazebo-server -r -v 4 --iterations 5 -f " +
+    std::string(PROJECT_SOURCE_PATH) + "/test/worlds/plugins.sdf";
+
+  std::cout << "Running command [" << cmd << "]" << std::endl;
+
+  std::string output = custom_exec_str(cmd);
+
+  for (auto i : {"1", "2", "3", "4", "5"})
+  {
+    EXPECT_NE(output.find(i), std::string::npos);
+  }
+}
+
+/////////////////////////////////////////////////
+TEST(CmdLine, GazeboServer)
+{
+  std::string cmd = std::string(kPluginPath) +
+    "ign-gazebo -s -r -v 4 --iterations 5 -f " +
+    std::string(PROJECT_SOURCE_PATH) + "/test/worlds/plugins.sdf";
+
+  std::cout << "Running command [" << cmd << "]" << std::endl;
+
+  std::string output = custom_exec_str(cmd);
+
+  for (auto i : {1, 2, 3, 4, 5})
+  {
+    EXPECT_NE(output.find("iteration " + std::to_string(i)), std::string::npos);
+  }
+}
+
+/////////////////////////////////////////////////
+TEST(CmdLine, Gazebo)
+{
+  std::string cmd = std::string(kPluginPath) +
+    "ign-gazebo -r -v 4 --iterations 5 -f " +
+    std::string(PROJECT_SOURCE_PATH) + "/test/worlds/plugins.sdf";
+
+  std::cout << "Running command [" << cmd << "]" << std::endl;
+
+  std::string output = custom_exec_str(cmd);
+
+  for (auto i : {1, 2, 3, 4, 5})
+  {
+    EXPECT_NE(output.find("iteration " + std::to_string(i)), std::string::npos);
+  }
+}
+
