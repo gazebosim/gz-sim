3cbb09610330db7abdba106c19ba1c98fb76d406
Carlos Agüero <caguero@osrfoundation.org>
2019-02-05 14:26:07 -0800

    And one more.

--- include/ignition/gazebo/components/Factory.hh
+++ include/ignition/gazebo/components/Factory.hh
@@ -37,7 +37,6 @@ namespace components
 {
   /// \brief A factory that generates a component based on a string type.
   class IGNITION_GAZEBO_VISIBLE Factory
-      : public ignition::common::SingletonT<Factory>
   {
     /// \brief Register a component.
     /// \param[in] _type Type of component to register.
@@ -158,7 +157,7 @@ namespace components
   { \
     public: IgnGazeboComponents##_classname() \
     { \
-      ignition::gazebo::components::Factory::Instance()->Register<_classname>(\
+      ignition::gazebo::components::Factory::Register<_classname>(\
         _compType);\
     } \
   }; \
