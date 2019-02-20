312f3b9388a9bb316ccdfc737f59a49c20bf1cc5
Addisu Z. Taddese <addisu@openrobotics.org>
2019-02-20 14:17:27 -0600

    Add mutex to contact_system test

--- test/integration/contact_system.cc
+++ test/integration/contact_system.cc
@@ -19,6 +19,8 @@
 
 #include <ignition/msgs/contacts.pb.h>
 
+#include <thread>
+
 #include <ignition/common/Console.hh>
 #include <ignition/math/Pose3.hh>
 #include <ignition/transport/Node.hh>
@@ -64,10 +66,12 @@ TEST_F(ContactSystemTest, MultipleCollisionsAsContactSensors)
   using namespace std::chrono_literals;
   server.SetUpdatePeriod(1ns);
 
+  std::mutex contactMutex;
   std::vector<msgs::Contacts> contactMsgs;
 
-  auto contactCb = [&contactMsgs](const msgs::Contacts &_msg) -> void
+  auto contactCb = [&](const msgs::Contacts &_msg) -> void
   {
+    std::lock_guard<std::mutex> lock(contactMutex);
     contactMsgs.push_back(_msg);
   };
 
@@ -80,24 +84,30 @@ TEST_F(ContactSystemTest, MultipleCollisionsAsContactSensors)
   // Run server
   size_t iters = 1000;
   server.Run(true, iters, false);
-  ASSERT_GE(contactMsgs.size(), 1u);
+  {
+    std::lock_guard<std::mutex> lock(contactMutex);
+    ASSERT_GE(contactMsgs.size(), 1u);
+  }
 
   // "contact_model" has one contact sensor, but the sensor uses two collisions
   // as sensors. Therefore, once the "contact_model" model has fallen and has
   // stabilized, we expect that each collision will have 1 contact point.
-  const auto &lastContacts = contactMsgs.back();
-  EXPECT_EQ(4, lastContacts.contact_size());
-
-  // It is easy to determine the contact points because the sphere falls and
-  // rests on the edges of the boxes. The position of the boxes is symmetric
-  // about the centers of the spheres, thus, the points of contact in each axis
-  // only differ by their signs
-  for (const auto &contact : lastContacts.contact())
   {
-    ASSERT_EQ(1, contact.position_size());
-    EXPECT_NEAR(0.25, std::abs(contact.position(0).x()), 5e-2);
-    EXPECT_NEAR(1, std::abs(contact.position(0).y()), 5e-2);
-    EXPECT_NEAR(1, contact.position(0).z(), 5e-2);
+    std::lock_guard<std::mutex> lock(contactMutex);
+    const auto &lastContacts = contactMsgs.back();
+    EXPECT_EQ(4, lastContacts.contact_size());
+
+    // It is easy to determine the contact points because the sphere falls and
+    // rests on the edges of the boxes. The position of the boxes is symmetric
+    // about the centers of the spheres, thus, the points of contact in each axis
+    // only differ by their signs
+    for (const auto &contact : lastContacts.contact())
+    {
+      ASSERT_EQ(1, contact.position_size());
+      EXPECT_NEAR(0.25, std::abs(contact.position(0).x()), 5e-2);
+      EXPECT_NEAR(1, std::abs(contact.position(0).y()), 5e-2);
+      EXPECT_NEAR(1, contact.position(0).z(), 5e-2);
+    }
   }
 
   // Remove the colliding boxes and check that contacts are no longer generated.
@@ -108,7 +118,10 @@ TEST_F(ContactSystemTest, MultipleCollisionsAsContactSensors)
 
   contactMsgs.clear();
   server.Run(true, 10, false);
-  EXPECT_EQ(0u, contactMsgs.size());
+  {
+    std::lock_guard<std::mutex> lock(contactMutex);
+    EXPECT_EQ(0u, contactMsgs.size());
+  }
 }
 
 TEST_F(ContactSystemTest, RemoveContactSensor)
@@ -121,10 +134,12 @@ TEST_F(ContactSystemTest, RemoveContactSensor)
 
   Server server(serverConfig);
 
+  std::mutex contactMutex;
   std::vector<msgs::Contacts> contactMsgs;
 
-  auto contactCb = [&contactMsgs](const msgs::Contacts &_msg) -> void
+  auto contactCb = [&](const msgs::Contacts &_msg) -> void
   {
+    std::lock_guard<std::mutex> lock(contactMutex);
     contactMsgs.push_back(_msg);
   };
 
@@ -136,7 +151,10 @@ TEST_F(ContactSystemTest, RemoveContactSensor)
 
   // Run server for a few iterations before removing sensor
   server.Run(true, 1000, false);
-  EXPECT_GE(contactMsgs.size(), 1u);
+  {
+    std::lock_guard<std::mutex> lock(contactMutex);
+    EXPECT_GE(contactMsgs.size(), 1u);
+  }
 
   // Remove the contact sensor and check that contacts are no longer generated.
   server.RequestRemoveEntity("sensor_contact");
@@ -148,5 +166,8 @@ TEST_F(ContactSystemTest, RemoveContactSensor)
 
   contactMsgs.clear();
   server.Run(true, 10, false);
-  EXPECT_EQ(0u, contactMsgs.size());
+  {
+    std::lock_guard<std::mutex> lock(contactMutex);
+    EXPECT_EQ(0u, contactMsgs.size());
+  }
 }
