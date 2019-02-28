d7d5f981789f5983ed9d8ec81368cf0b4d4ae502
Addisu Z. Taddese <addisu@openrobotics.org>
2019-02-28 03:02:35 -0600

    Handle entities spawned after touch plugin is initialized

--- test/integration/touch_plugin.cc
+++ test/integration/touch_plugin.cc
@@ -34,10 +34,12 @@ class TouchPluginTest : public ::testing::Test
     common::Console::SetVerbosity(4);
     setenv("IGN_GAZEBO_SYSTEM_PLUGIN_PATH",
            (std::string(PROJECT_BINARY_PATH) + "/lib").c_str(), 1);
+  }
 
+  public: void StartServer(const std::string &_sdfFile)
+  {
     ServerConfig serverConfig;
-    serverConfig.SetSdfFile(std::string(PROJECT_SOURCE_PATH) +
-        "/test/worlds/touch_plugin.sdf");
+    serverConfig.SetSdfFile(std::string(PROJECT_SOURCE_PATH) + _sdfFile);
     server = std::make_unique<Server>(serverConfig);
     using namespace std::chrono_literals;
     server->SetUpdatePeriod(0ns);
@@ -52,6 +54,8 @@ class TouchPluginTest : public ::testing::Test
 /////////////////////////////////////////////////
 TEST_F(TouchPluginTest, OneLink)
 {
+  this->StartServer("/test/worlds/touch_plugin.sdf");
+
   bool whiteTouched{false};
   auto whiteTouchCb =
       std::function([&](const msgs::Boolean &) { whiteTouched = true; });
@@ -95,6 +99,8 @@ TEST_F(TouchPluginTest, OneLink)
 //////////////////////////////////////////////////
 TEST_F(TouchPluginTest, MultiLink)
 {
+  this->StartServer("/test/worlds/touch_plugin.sdf");
+
   bool redTouched{false};
   auto redTouchedCb =
       std::function([&](const msgs::Boolean &) { redTouched  = true; });
@@ -122,6 +128,8 @@ TEST_F(TouchPluginTest, MultiLink)
 //////////////////////////////////////////////////
 TEST_F(TouchPluginTest, StartDisabled)
 {
+  this->StartServer("/test/worlds/touch_plugin.sdf");
+
   // Subscribe to plugin notifications
   bool blueTouched{false};
   auto blueTouchedCb =
@@ -156,6 +164,8 @@ TEST_F(TouchPluginTest, StartDisabled)
 //////////////////////////////////////////////////
 TEST_F(TouchPluginTest, RemovalOfParentModel)
 {
+  this->StartServer("/test/worlds/touch_plugin.sdf");
+
   bool redTouched{false};
   auto redTouchedCb =
       std::function([&](const msgs::Boolean &) { redTouched  = true; });
@@ -181,4 +191,116 @@ TEST_F(TouchPluginTest, RemovalOfParentModel)
 
   // Check that we don't get a touched event
   EXPECT_FALSE(redTouched);
+}
+
+
+//////////////////////////////////////////////////
+/// Tests whether the plugin works when it is spawned after other entities have
+/// already been created and vice versa
+/// This test uses depends on the user_commands system
+TEST_F(TouchPluginTest, SpawnedEntities)
+{
+  std::string whiteBox = R"EOF(
+  <?xml version="1.0" ?>
+  <sdf version="1.6">
+      <model name="white_box">
+        <pose>0 0 4 0 0 0</pose>
+        <link name="link">
+          <collision name="collision">
+            <geometry>
+              <box>
+                <size>0.5 0.5 0.5</size>
+              </box>
+            </geometry>
+          </collision>
+          <sensor name="white_box_sensor" type="contact">
+            <contact>
+              <collision>collision</collision>
+            </contact>
+          </sensor>
+        </link>
+        <plugin
+          filename="libignition-gazebo-touchplugin-system.so"
+          name="ignition::gazebo::systems::v0::TouchPlugin">
+          <target>green_box_for_white</target>
+          <time>0.2</time>
+          <namespace>white_touches_only_green</namespace>
+          <enabled>true</enabled>
+        </plugin>
+      </model>
+  </sdf>)EOF";
+
+  std::string greenBox = R"EOF(
+  <?xml version="1.0" ?>
+  <sdf version="1.6">
+      <model name="green_box_for_white">
+        <static>1</static>
+        <pose>0 0 0.5 0 0 0</pose>
+        <link name="link">
+          <collision name="collision">
+            <geometry>
+              <box>
+                <size>1 1 1</size>
+              </box>
+            </geometry>
+          </collision>
+        </link>
+      </model>
+  </sdf>)EOF";
+
+  transport::Node node;
+
+  bool whiteTouched{false};
+  auto whiteTouchCb =
+      std::function([&](const msgs::Boolean &) { whiteTouched = true; });
+
+  node.Subscribe("/white_touches_only_green/touched", whiteTouchCb);
+
+  // Request entity spawn
+  msgs::EntityFactory req;
+  unsigned int timeout = 5000;
+  std::string service{"/world/empty/create"};
+
+  msgs::Boolean res;
+  bool result;
+
+  auto testFunc = [&](const std::string &_box1, const std::string &_box2)
+  {
+    this->server.reset();
+    this->StartServer("/examples/worlds/empty.sdf");
+
+    whiteTouched = false;
+    req.set_sdf(_box1);
+
+    EXPECT_TRUE(node.Request(service, req, timeout, res, result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(res.data());
+
+    // Run the server to actually create the entities
+    server->Run(true, 100, false);
+
+    req.set_sdf(_box2);
+
+    EXPECT_TRUE(node.Request(service, req, timeout, res, result));
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(res.data());
+    server->Run(true, 100, false);
+
+    // Check boxes haven't touched yet
+    EXPECT_FALSE(whiteTouched);
+
+    // Let white box fall on top of green box
+    server->Run(true, 500, false);
+
+    // Check it hasn't touched for long enough
+    EXPECT_FALSE(whiteTouched);
+
+    // Give it time to touch for at least 0.2 seconds
+    server->Run(true, 500, false);
+    EXPECT_TRUE(whiteTouched);
+  };
+
+  // Test different spawn orders
+  testFunc(whiteBox, greenBox);
+  testFunc(greenBox, whiteBox);
 }
