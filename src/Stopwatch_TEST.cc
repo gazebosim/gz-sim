2a70d67b6306d76ada3a77c62b70e34b819b160a
Steve Peters <scpeters@openrobotics.org>
2019-03-12 21:53:42 -0700

    backport fix for Stopwatch.StartStopReset flakiness on windows to ign-math5

--- src/Stopwatch_TEST.cc
+++ src/Stopwatch_TEST.cc
@@ -37,6 +37,15 @@ TEST(Stopwatch, Constructor)
 // Helper function that runs a few tests
 void runTimer(math::Stopwatch &_time)
 {
+  // Windows uses a system_clock for std::this_thread::sleep_for. This can
+  // cause incorrect sleep durations. So, we add some room for error on
+  // windows.
+  std::chrono::duration<int, std::milli> handleSteadyClock =
+    std::chrono::milliseconds(0);
+#ifdef _WIN32
+  handleSteadyClock = std::chrono::milliseconds(100);
+#endif
+
   // Start the timer
   EXPECT_TRUE(_time.Start());
   // The timer should be running.
@@ -50,7 +59,8 @@ void runTimer(math::Stopwatch &_time)
   // Wait for some time...
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   // Now the elapsed time should be greater than or equal to the time slept.
-  EXPECT_GE(_time.ElapsedRunTime(), std::chrono::milliseconds(1000));
+  EXPECT_GE(_time.ElapsedRunTime() + handleSteadyClock,
+      std::chrono::milliseconds(1000));
 
   // Stop the timer.
   EXPECT_TRUE(_time.Stop());
@@ -59,7 +69,8 @@ void runTimer(math::Stopwatch &_time)
   // The stop time should be greater than the start time.
   EXPECT_GT(_time.StopTime(), _time.StartTime());
   // The elapsed time should still be greater than the time slept.
-  EXPECT_GE(_time.ElapsedRunTime(), std::chrono::milliseconds(1000));
+  EXPECT_GE(_time.ElapsedRunTime() + handleSteadyClock,
+      std::chrono::milliseconds(1000));
 
   // Save the elapsed time.
   auto elapsedTime = _time.ElapsedRunTime();
@@ -68,7 +79,8 @@ void runTimer(math::Stopwatch &_time)
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   // The elapsed stop time should be greater than or equal to the time
   // slept.
-  EXPECT_GE(_time.ElapsedStopTime(), std::chrono::milliseconds(1000));
+  EXPECT_GE(_time.ElapsedStopTime() + handleSteadyClock,
+      std::chrono::milliseconds(1000));
   // The elapsed time should be the same.
   EXPECT_EQ(elapsedTime, _time.ElapsedRunTime());
 
@@ -86,7 +98,8 @@ void runTimer(math::Stopwatch &_time)
   EXPECT_GT(_time.ElapsedRunTime(), elapsedTime);
   // The elapsed time should be greater than or equal to the the previous
   // two sleep times.
-  EXPECT_GE(_time.ElapsedRunTime(), std::chrono::milliseconds(2000));
+  EXPECT_GE(_time.ElapsedRunTime() + handleSteadyClock,
+      std::chrono::milliseconds(2000));
 }
 
 /////////////////////////////////////////////////
