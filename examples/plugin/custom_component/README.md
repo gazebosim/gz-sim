1ad8b844b84da186d5100f2e9570f2abbbc380bc
chapulina <burajiru.no.chapulina@gmail.com>
2019-03-25 15:47:27 -0700

    [Acropolis] Enforce component type uniqueness

---
+++ examples/plugin/custom_component/README.md
@@ -0,0 +1,33 @@
+# Custom components
+
+This example shows how to create a custom component from a system plugin.
+
+See `CustomComponentPlugin.hh` for more information.
+
+## Build
+
+From the root of the `ign-gazebo` repository, do the following to build the example:
+
+~~~
+cd ign-gazebo/examples/plugins/custom_component
+mkdir build
+cd build
+cmake ..
+make
+~~~
+
+This will generate the `libCustomComponent.so` library under `build`.
+
+## Run
+
+Add the lbrary to the path:
+
+~~~
+cd ign-gazebo/examples/plugins/custom_component
+export IGN_GAZEBO_SYSTEM_PLUGIN_PATH=`pwd`/build
+~~~
+
+Then run a world that loads the plugin as follows:
+
+    ign-gazebo-server -v 4 -f custom_component.sdf
+
--- test/plugins/TestWorldSystem.hh
+++ test/plugins/TestWorldSystem.hh
@@ -28,9 +28,10 @@ namespace gazebo
 inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
 namespace components
 {
-using DoubleComponent = components::Component<double, class DoubleComponentTag>;
-IGN_GAZEBO_REGISTER_COMPONENT("ign_gazebo_components.DoubleComponent",
-    DoubleComponent)
+using WorldPluginComponent =
+    components::Component<double, class WorldPluginComponentTag>;
+IGN_GAZEBO_REGISTER_COMPONENT("WorldPluginComponent",
+    WorldPluginComponent)
 }
 }
 
@@ -40,6 +41,10 @@ class TestWorldSystem :
   public ISystemUpdate
 {
   public: TestWorldSystem() = default;
+  public: ~TestWorldSystem()
+        {
+          igndbg << "Destroying TestWorldSystem" << std::endl;
+        }
 
   public: void Configure(const Entity &_entity,
                          const std::shared_ptr<const sdf::Element> &_sdf,
@@ -47,7 +52,8 @@ class TestWorldSystem :
                          EventManager &/*_eventManager*/) override
         {
           auto value = _sdf->Get<double>("world_key");
-          _ecm.CreateComponent(_entity, components::DoubleComponent(value));
+          _ecm.CreateComponent(_entity,
+              components::WorldPluginComponent(value));
         }
 
   public: void Update(const gazebo::UpdateInfo &_info,
