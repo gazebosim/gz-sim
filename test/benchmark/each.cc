3dc0183ab2b57fb7420f2715760175882675c325
Michael Carroll <michael@openrobotics.org>
2019-03-28 14:53:02 -0500

    Tidy up.

--- test/benchmark/each.cc
+++ test/benchmark/each.cc
@@ -17,6 +17,8 @@
 
 #include <benchmark/benchmark.h>
 
+#include <memory>
+
 #include "ignition/gazebo/Entity.hh"
 #include "ignition/gazebo/EntityComponentManager.hh"
 #include "ignition/gazebo/components/World.hh"
@@ -29,46 +31,41 @@ constexpr const int kEachIterations {100};
 
 class EntityComponentManagerFixture: public benchmark::Fixture
 {
-  virtual void SetUp(const ::benchmark::State& _state)
+  protected: void SetUp(const ::benchmark::State& _state) override
   {
-    mgr = new EntityComponentManager();
+    mgr = std::make_unique<EntityComponentManager>();
     auto matchingEntityCount = _state.range(0);
     auto nonmatchingEntityCount = _state.range(1);
-    this->populate(matchingEntityCount, nonmatchingEntityCount);
+    this->Populate(matchingEntityCount, nonmatchingEntityCount);
   }
 
-  virtual void TearDown(const ::benchmark::State& /*_state*/)
+  protected: void Populate(int _matchingEntityCount, int _nonmatchingEntityCount)
   {
-    delete mgr;
-  }
-
-  protected:
-
-  void populate(int matchingEntityCount, int nonmatchingEntityCount)
-  {
-    for (int i = 0; i < matchingEntityCount; ++i)
+    for (int i = 0; i < _matchingEntityCount; ++i)
     {
       Entity worldEntity = mgr->CreateEntity();
       mgr->CreateComponent(worldEntity, components::World());
       mgr->CreateComponent(worldEntity, components::Name("world_name"));
     }
 
-    for (int i = 0; i < nonmatchingEntityCount; ++i)
+    for (int i = 0; i < _nonmatchingEntityCount; ++i)
     {
       Entity worldEntity = mgr->CreateEntity();
       mgr->CreateComponent(worldEntity, components::Name("world_name"));
     }
   }
 
-  EntityComponentManager* mgr;
+  std::unique_ptr<EntityComponentManager> mgr;
 };
 
-BENCHMARK_DEFINE_F(EntityComponentManagerFixture, EachNoCache)(benchmark::State& st)
+BENCHMARK_DEFINE_F(EntityComponentManagerFixture, EachNoCache)(benchmark::State& _st)
 {
-  for (auto _ : st)
+  for (auto _ : _st)
   {
-    auto matchingEntityCount = st.range(0);
-    st.counters.insert({{"matching", st.range(0)}, {"nonmatching", st.range(1)}});
+    auto matchingEntityCount = _st.range(0);
+    _st.counters.insert({
+        {"matching", _st.range(0)},
+        {"nonmatching", _st.range(1)}});
 
     for (int eachIter = 0; eachIter < kEachIterations; eachIter++)
     {
@@ -84,18 +81,20 @@ BENCHMARK_DEFINE_F(EntityComponentManagerFixture, EachNoCache)(benchmark::State&
 
       if (entitiesMatched != matchingEntityCount)
       {
-        st.SkipWithError("Failed to match correct number of entities");
+        _st.SkipWithError("Failed to match correct number of entities");
       }
     }
   }
 }
 
-BENCHMARK_DEFINE_F(EntityComponentManagerFixture, EachCache)(benchmark::State& st)
+BENCHMARK_DEFINE_F(EntityComponentManagerFixture, EachCache)(benchmark::State& _st)
 {
-  for (auto _ : st)
+  for (auto _ : _st)
   {
-    auto matchingEntityCount = st.range(0);
-    st.counters.insert({{"matching", st.range(0)}, {"nonmatching", st.range(1)}});
+    auto matchingEntityCount = _st.range(0);
+    _st.counters.insert({
+        {"matching", _st.range(0)},
+        {"nonmatching", _st.range(1)}});
 
     for (int eachIter = 0; eachIter < kEachIterations; eachIter++)
     {
@@ -111,7 +110,7 @@ BENCHMARK_DEFINE_F(EntityComponentManagerFixture, EachCache)(benchmark::State& s
 
       if (entitiesMatched != matchingEntityCount)
       {
-        st.SkipWithError("Failed to match correct number of entities");
+        _st.SkipWithError("Failed to match correct number of entities");
       }
     }
   }
@@ -119,7 +118,7 @@ BENCHMARK_DEFINE_F(EntityComponentManagerFixture, EachCache)(benchmark::State& s
 
 /// Method to generate test argument combinations.  google/benchmark does
 // powers of 2 by default, which looks kind of ugly.
-static void EachTestArgs(benchmark::internal::Benchmark* b)
+static void EachTestArgs(benchmark::internal::Benchmark* _b)
 {
   int maxEntityCount = 1000;
   int step = maxEntityCount/5;
@@ -131,7 +130,7 @@ static void EachTestArgs(benchmark::internal::Benchmark* b)
          nonmatchingEntityCount <= maxEntityCount;
          nonmatchingEntityCount += step)
     {
-      b->Args({matchingEntityCount, nonmatchingEntityCount});
+      _b->Args({matchingEntityCount, nonmatchingEntityCount});
     }
   }
 }
