45892037b7d93910849b6836485ea048cb19e614
Nate Koenig <natekoenig@gmail.com>
2019-04-04 14:54:20 -0700

    reorg

--- docker/scripts/install_all.sh
+++ docker/scripts/install_all.sh
@@ -3,14 +3,15 @@
 set -o errexit
 set -o verbose
 
-
 # Install base dependencies
 ./docker/scripts/install_common_deps.sh
-./docker/scripts/install_ign_deps.sh
 ./docker/scripts/enable_gcc8.sh
+
 # Enable relevant package repositories
 ./docker/scripts/enable_ign_stable.sh
 ./docker/scripts/enable_ign_prerelease.sh
+
+./docker/scripts/install_ign_deps.sh
 
 # Install Ignition libraries from Debian.
 ./docker/scripts/install_ign.sh
