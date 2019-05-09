bd523827512cba79a4fd1dd31dec1c509de3bf7c
Nate Koenig <natekoenig@gmail.com>
2019-05-09 16:55:56 -0700

    Fix sim runner test

--- src/SimulationRunner_TEST.cc
+++ src/SimulationRunner_TEST.cc
@@ -1199,7 +1199,7 @@ TEST_P(SimulationRunnerTest, GuiInfo)
 
   auto plugin = res.plugin(0);
   EXPECT_EQ("3D View", plugin.name());
-  EXPECT_EQ("Scene3D", plugin.filename());
+  EXPECT_EQ("GzScene3D", plugin.filename());
   EXPECT_NE(plugin.innerxml().find("<ignition-gui>"), std::string::npos);
   EXPECT_NE(plugin.innerxml().find("<ambient_light>"), std::string::npos);
   EXPECT_NE(plugin.innerxml().find("<pose_topic>"), std::string::npos);
