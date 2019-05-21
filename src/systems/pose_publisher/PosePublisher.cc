0a68844abaa79c691b90e0d8994d562e5604115a
Addisu Z. Taddese <addisu@openrobotics.org>
2019-05-21 16:55:47 -0500

    Allow setting the update frequency of pose publisher

--- src/systems/pose_publisher/PosePublisher.cc
+++ src/systems/pose_publisher/PosePublisher.cc
@@ -71,6 +71,16 @@ class ignition::gazebo::systems::PosePublisherPrivate
 
   /// \brief True to publish nested model pose
   public: bool publishNestedModelPose = false;
+
+  /// \brief Frequency of pose publications in Hz. A negative frequency
+  /// publishes as fast as possible (i.e, at the rate of the simulation step)
+  public: double updateFrequency = -1;
+
+  /// \brief Last time poses were published.
+  public: std::chrono::steady_clock::duration lastPosePubTime{0};
+
+  /// \brief Update period calculated from updateFrequency in nanoseconds
+  public: std::chrono::steady_clock::duration updatePeriod{0};
 };
 
 //////////////////////////////////////////////////
@@ -118,6 +128,17 @@ void PosePublisher::Configure(const Entity &_entity,
   this->dataPtr->publishSensorPose =
     _sdf->Get<bool>("publish_sensor_pose",
         this->dataPtr->publishSensorPose).first;
+
+  this->dataPtr->updateFrequency =
+    _sdf->Get<double>("update_frequency",
+        this->dataPtr->updateFrequency).first;
+
+  if (this->dataPtr->updateFrequency > 0)
+  {
+    std::chrono::duration<double> period{1 / this->dataPtr->updateFrequency};
+    this->dataPtr->updatePeriod =
+        std::chrono::duration_cast<std::chrono::steady_clock::duration>(period);
+  }
 }
 
 //////////////////////////////////////////////////
@@ -127,6 +148,16 @@ void PosePublisher::PostUpdate(const UpdateInfo &_info,
   // Nothing left to do if paused.
   if (_info.paused)
     return;
+
+  auto diff = _info.simTime - this->dataPtr->lastPosePubTime;
+  // If the diff is positive and it's less than the update period, we skip
+  // publication. If the diff is negative, then time has gone backward, we go
+  // ahead publish and allow the time to be reset
+  if ((diff > std::chrono::steady_clock::duration::zero()) &&
+      (diff < this->dataPtr->updatePeriod))
+  {
+    return;
+  }
 
   // pose frame, child_frame, pose
   std::vector<std::tuple<std::string, std::string, math::Pose3d>> poses;
@@ -158,6 +189,8 @@ void PosePublisher::PostUpdate(const UpdateInfo &_info,
     msgs::Set(&poseMsg, transform);
     this->dataPtr->posePub.Publish(poseMsg);
   }
+
+  this->dataPtr->lastPosePubTime = _info.simTime;
 }
 
 //////////////////////////////////////////////////
