74cbbdb98fa8177458ff945afaeea89deff2bfca
Nate Koenig <natekoenig@gmail.com>
2019-06-28 11:41:46 -0700

    Update and publish odometry

--- src/systems/diff_drive/DiffDrive.cc
+++ src/systems/diff_drive/DiffDrive.cc
@@ -14,9 +14,13 @@
  * limitations under the License.
  *
  */
+#include <ignition/math/Odometry.hh>
+#include <ignition/math/Quaternion.hh>
+#include <ignition/msgs/odometry.pb.h>
 #include <ignition/plugin/Register.hh>
 #include <ignition/transport/Node.hh>
 
+#include "ignition/gazebo/components/JointPosition.hh"
 #include "ignition/gazebo/components/JointVelocityCmd.hh"
 #include "ignition/gazebo/Model.hh"
 
@@ -31,6 +35,13 @@ class ignition::gazebo::systems::DiffDrivePrivate
   /// \brief Callback for velocity subscription
   /// \param[in] _msg Velocity message
   public: void OnCmdVel(const ignition::msgs::Twist &_msg);
+
+  /// \brief Update odometry and publish an odometry message.
+  /// \param[in] _info System update information.
+  /// \param[in] _ecm The EntityComponentManager of the given simulation
+  /// instance.
+  public: void UpdateOdometry(const ignition::gazebo::UpdateInfo &_info,
+    ignition::gazebo::EntityComponentManager &_ecm);
 
   /// \brief Ignition communication node.
   public: transport::Node node;
@@ -61,6 +72,12 @@ class ignition::gazebo::systems::DiffDrivePrivate
 
   /// \brief Model interface
   public: Model model{kNullEntity};
+
+  /// \brief Diff drive odometry.
+  public: math::DiffDriveOdometry odom;
+
+  /// \brief Diff drive odometry message publisher.
+  public: transport::Node::Publisher odomPub;
 };
 
 //////////////////////////////////////////////////
@@ -107,12 +124,23 @@ void DiffDrive::Configure(const Entity &_entity,
   this->dataPtr->wheelRadius = _sdf->Get<double>("wheel_radius",
       this->dataPtr->wheelRadius).first;
 
+  // Setup odometry.
+  this->dataPtr->odom.SetWheelParams(this->dataPtr->wheelSeparation,
+      this->dataPtr->wheelRadius, this->dataPtr->wheelRadius);
+
   // Subscribe to commands
   std::string topic{"/model/" + this->dataPtr->model.Name(_ecm) + "/cmd_vel"};
   if (_sdf->HasElement("topic"))
     topic = _sdf->Get<std::string>("topic");
   this->dataPtr->node.Subscribe(topic, &DiffDrivePrivate::OnCmdVel,
       this->dataPtr.get());
+
+  std::string odomTopic{"/model/" + this->dataPtr->model.Name(_ecm) +
+    "/odometry"};
+  transport::AdvertiseMessageOptions opts;
+  opts.SetMsgsPerSec(50);
+  this->dataPtr->odomPub = this->dataPtr->node.Advertise<msgs::Odometry>(
+      odomTopic, opts);
 
   ignmsg << "DiffDrive subscribing to twist messages on [" << topic << "]"
          << std::endl;
@@ -162,6 +190,7 @@ void DiffDrive::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
     {
       *vel = components::JointVelocityCmd({this->dataPtr->leftJointSpeed});
     }
+
   }
 
   for (Entity joint : this->dataPtr->rightJoints)
@@ -179,6 +208,56 @@ void DiffDrive::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
       *vel = components::JointVelocityCmd({this->dataPtr->rightJointSpeed});
     }
   }
+
+  // Update odometry.
+  this->dataPtr->UpdateOdometry(_info, _ecm);
+}
+
+//////////////////////////////////////////////////
+void DiffDrivePrivate::UpdateOdometry(const ignition::gazebo::UpdateInfo &_info,
+    ignition::gazebo::EntityComponentManager &_ecm)
+{
+  // Get the first joint positions for the left and right side.
+  auto leftPos = _ecm.Component<components::JointPosition>(this->leftJoints[0]);
+  if (!leftPos)
+  {
+    _ecm.CreateComponent(this->leftJoints[0], components::JointPosition());
+  }
+
+  auto rightPos = _ecm.Component<components::JointPosition>(
+      this->rightJoints[0]);
+  if (!rightPos)
+  {
+    _ecm.CreateComponent(this->rightJoints[0], components::JointPosition());
+  }
+
+  // Initialize, if not already initialized.
+  if (!this->odom.Initialized())
+  {
+    this->odom.Init(std::chrono::steady_clock::time_point(_info.simTime));
+    return;
+  }
+
+  // Abort if the joints were not found or just created.
+  if (!leftPos || !rightPos)
+    return;
+
+  this->odom.Update(leftPos->Data()[0], rightPos->Data()[0],
+      std::chrono::steady_clock::time_point(_info.simTime));
+
+  // Construct the odometry message and publish it.
+  msgs::Odometry msg;
+  msg.mutable_pose()->mutable_position()->set_x(this->odom.X());
+  msg.mutable_pose()->mutable_position()->set_y(this->odom.Y());
+
+  math::Quaterniond orientation(0, 0, *this->odom.Heading());
+  msgs::Set(msg.mutable_pose()->mutable_orientation(), orientation);
+
+  msg.mutable_twist()->mutable_linear()->set_x(this->odom.LinearVelocity());
+  msg.mutable_twist()->mutable_angular()->set_z(*this->odom.AngularVelocity());
+
+  // Publish the message
+  this->odomPub.Publish(msg);
 }
 
 //////////////////////////////////////////////////
