a3ff44df09d4a2757dc081a49cdac1bc1e36995c
Ian Chen <ichen@osrfoundation.org>
2019-07-12 16:53:55 -0700

    testing perf changes

--- src/SimulationRunner.cc
+++ src/SimulationRunner.cc
@@ -547,6 +547,7 @@ void SimulationRunner::Step(const UpdateInfo &_info)
 void SimulationRunner::LoadPlugins(const Entity _entity,
     const sdf::ElementPtr &_sdf)
 {
+  std::cerr << "===========================================  sim runner loadpluginS " << std::endl;
   sdf::ElementPtr pluginElem = _sdf->GetElement("plugin");
   while (pluginElem)
   {
@@ -561,7 +562,9 @@ void SimulationRunner::LoadPlugins(const Entity _entity,
       std::optional<SystemPluginPtr> system;
       {
         std::lock_guard<std::mutex> lock(this->systemLoaderMutex);
+        std::cerr << "===========================================  sim runner load plugin " << pluginElem->Get<std::string>("filename")  << std::endl;
         system = this->systemLoader->LoadPlugin(pluginElem);
+        std::cerr << "===========================================  sim runner load plugind done " <<  std::endl;
       }
       if (system)
       {
@@ -633,6 +636,7 @@ void SimulationRunner::LoadPlugins(const Entity _entity,
     std::optional<SystemPluginPtr> system;
     {
       std::lock_guard<std::mutex> lock(this->systemLoaderMutex);
+      std::cerr << "======22222==================  sim runner load plugin " << plugin.Filename()  << " vs " << plugin.Name() <<  std::endl;
       system = this->systemLoader->LoadPlugin(plugin.Filename(), plugin.Name(),
                                               nullptr);
     }
