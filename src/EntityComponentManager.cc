900a80f7fd736b8c0404d6258886c7feeb7e37f1
chapulina <burajiru.no.chapulina@gmail.com>
2019-07-17 12:04:03 -0700

    fix primary too

--- src/EntityComponentManager.cc
+++ src/EntityComponentManager.cc
@@ -1123,6 +1123,8 @@ void EntityComponentManager::SetState(
       {
         std::istringstream istr(compMsg.component());
         comp->Deserialize(istr);
+        this->SetChanged(entity, compIter->first,
+            ComponentState::OneTimeChange);
       }
     }
   }
