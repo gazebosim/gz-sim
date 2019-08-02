6e3d2a5e0e3fe4fd68a7f975a9dbc23b8e256f53
Steve Peters <scpeters@openrobotics.org>
2019-08-02 14:29:00 -0700

    initial version of wheel_slip system copied from apply_joint_force

---
+++ src/systems/wheel_slip/WheelSlip.hh
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2019 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifndef IGNITION_GAZEBO_SYSTEMS_WHEELSLIP_HH_
+#define IGNITION_GAZEBO_SYSTEMS_WHEELSLIP_HH_
+
+#include <ignition/gazebo/System.hh>
+#include <memory>
+
+namespace ignition
+{
+namespace gazebo
+{
+// Inline bracket to help doxygen filtering.
+inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
+namespace systems
+{
+  // Forward declaration
+  class WheelSlipPrivate;
+
+  /// \brief This system applies a force to the first axis of a specified joint.
+  class IGNITION_GAZEBO_VISIBLE WheelSlip
+      : public System,
+        public ISystemConfigure,
+        public ISystemPreUpdate
+  {
+    /// \brief Constructor
+    public: WheelSlip();
+
+    /// \brief Destructor
+    public: ~WheelSlip() override = default;
+
+    // Documentation inherited
+    public: void Configure(const Entity &_entity,
+                           const std::shared_ptr<const sdf::Element> &_sdf,
+                           EntityComponentManager &_ecm,
+                           EventManager &_eventMgr) override;
+
+    // Documentation inherited
+    public: void PreUpdate(
+                const ignition::gazebo::UpdateInfo &_info,
+                ignition::gazebo::EntityComponentManager &_ecm) override;
+
+    /// \brief Private data pointer
+    private: std::unique_ptr<WheelSlipPrivate> dataPtr;
+  };
+  }
+}
+}
+}
+
+#endif
