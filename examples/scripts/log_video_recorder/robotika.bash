45259a2f3f9a4a1eaf8125e1301e2370f6d2f37b
Nate Koenig <natekoenig@gmail.com>
2019-10-27 12:22:11 -0700

    added robotika and sophisticated

---
+++ examples/scripts/log_video_recorder/robotika.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-3-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-34-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-36-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-37-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-40-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-41-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-43-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-44-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/robotika/72401e58-d474-4778-9af3-6c6d1655cc7d-c-45-r-1/logs
+
---
+++ examples/scripts/log_video_recorder/sophisticated.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./record_one_run.bash /data/subt/gz-logs/Sophisticated%20Engineering%20UG/19b7bb01-333a-4316-9f4d-27780d18a1fa-c-43-r-3/logs
