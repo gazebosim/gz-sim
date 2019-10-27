cb519cf1c7c15111d43b28fe5f0ed392952383f2
Nate Koenig <natekoenig@gmail.com>
2019-10-27 11:25:36 -0700

    added coordinated

---
+++ examples/scripts/log_video_recorder/coordinated.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-1-r-3/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-2-r-3/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-5-r-3/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-6-r-3/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-19-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-20-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-21-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-22-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-23-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-24-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-27-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-34-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-35-r-2/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-36-r-2/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-37-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-38-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-39-r-2/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-43-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-44-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-45-r-2/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-46-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-47-r-1/logs
+./record_one_run.bash /data/subt/gz-logs/Coordinated%20Robotics/760c207a-7e2b-41f7-95d5-5ab7a3b74719-c-48-r-2/logs
