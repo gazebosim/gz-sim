1d917b446935b7b56395b1e763668d960b3c2c6a
Nate Koenig <natekoenig@gmail.com>
2019-10-29 13:22:54 -0700

    upates

--- examples/scripts/log_video_recorder/mtri1.bash
+++ examples/scripts/log_video_recorder/mtri1.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-21-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-39-r-4/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-41-r-4/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-43-r-7/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-44-r-3/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-45-r-4/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-46-r-3/logs
--- examples/scripts/log_video_recorder/mtri2.bash
+++ examples/scripts/log_video_recorder/mtri2.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-20-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-22-r-5/logs
 ./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-25-r-6/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-36-r-4/logs
+./record_one_run.bash /data/subt/gz-logs/MTRI/207817a3-8ec3-4778-ad4a-9a9c8c01df4a-c-38-r-4/logs
+
