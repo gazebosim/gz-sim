34a79d8b2985201ed84c1ddfdc4b17ebfc5f8bbe
Nate Koenig <natekoenig@gmail.com>
2019-10-29 13:03:29 -0700

    sodium script

---
+++ examples/scripts/log_video_recorder/sodium.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-2-r-1/logs
+# ./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-4-r-1/logs
+# ./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-5-r-1/logs
+# ./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-6-r-1/logs
+./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-19-r-1/logs
+# ./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-20-r-1/logs
+# ./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-21-r-1/logs
+./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-34-r-1/logs
+./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-36-r-1/logs
+# ./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-37-r-1/logs
+./record_one_run.bash /big/subt/gz-logs/sodium-24-robotics/7698aa27-2619-4d5b-a8bb-d5b6ffd68020-c-39-r-1/logs
+
