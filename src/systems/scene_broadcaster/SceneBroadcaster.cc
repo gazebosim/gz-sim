170f872e12d7abfa813cb4190ef7c686a0b3f24b
Ian Chen <ichen@osrfoundation.org>
2019-11-13 15:13:17 -0800

    :Backed out changeset 0d474daa66a4

--- src/systems/scene_broadcaster/SceneBroadcaster.cc
+++ src/systems/scene_broadcaster/SceneBroadcaster.cc
@@ -25,7 +25,6 @@
 #include <ignition/plugin/Register.hh>
 #include <ignition/transport/Node.hh>
 
-#include "ignition/gazebo/components/CastShadows.hh"
 #include "ignition/gazebo/components/Geometry.hh"
 #include "ignition/gazebo/components/Light.hh"
 #include "ignition/gazebo/components/Link.hh"
@@ -625,13 +624,10 @@ void SceneBroadcasterPrivate::SceneGraphAddEntities(
 
   // Visuals
   _manager.EachNew<components::Visual, components::Name,
-                   components::ParentEntity,
-                   components::CastShadows,
-                   components::Pose>(
+                   components::ParentEntity, components::Pose>(
       [&](const Entity &_entity, const components::Visual *,
           const components::Name *_nameComp,
           const components::ParentEntity *_parentComp,
-          const components::CastShadows *_castShadowsComp,
           const components::Pose *_poseComp) -> bool
       {
         auto visualMsg = std::make_shared<msgs::Visual>();
@@ -639,7 +635,6 @@ void SceneBroadcasterPrivate::SceneGraphAddEntities(
         visualMsg->set_parent_id(_parentComp->Data());
         visualMsg->set_name(_nameComp->Data());
         visualMsg->mutable_pose()->CopyFrom(msgs::Convert(_poseComp->Data()));
-        visualMsg->set_cast_shadows(_castShadowsComp->Data());
 
         // Geometry is optional
         auto geometryComp = _manager.Component<components::Geometry>(_entity);
