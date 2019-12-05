9b2812011ee7f54b25cf8059442f4b26eb85a7ef
Addisu Z. Taddese <addisu@openrobotics.org>
2019-12-04 20:41:29 -0600

    Add test for nondefault canonical link

--- test/integration/physics_system.cc
+++ test/integration/physics_system.cc
@@ -266,6 +266,61 @@ TEST_F(PhysicsSystemFixture, CanonicalLink)
 }
 
 /////////////////////////////////////////////////
+// Same as the CanonicalLink test, but with a non-default canonical link
+TEST_F(PhysicsSystemFixture, NonDefaultCanonicalLink)
+{
+  ignition::gazebo::ServerConfig serverConfig;
+
+  const auto sdfFile = std::string(PROJECT_SOURCE_PATH) +
+    "/test/worlds/nondefault_canonical.sdf";
+
+  sdf::Root root;
+  root.Load(sdfFile);
+  const sdf::World *world = root.WorldByIndex(0);
+  ASSERT_TRUE(nullptr != world);
+
+  serverConfig.SetSdfFile(sdfFile);
+
+  gazebo::Server server(serverConfig);
+
+  server.SetUpdatePeriod(1ns);
+
+  const std::string modelName{"nondefault_canonical"};
+
+  // Create a system that records the pose of the model.
+  Relay testSystem;
+
+  std::vector<ignition::math::Pose3d> modelPoses;
+  testSystem.OnPostUpdate(
+    [&modelName, &modelPoses](const gazebo::UpdateInfo &,
+    const gazebo::EntityComponentManager &_ecm)
+    {
+      _ecm.Each<components::Model, components::Name, components::Pose>(
+        [&](const ignition::gazebo::Entity &, const components::Model *,
+        const components::Name *_name, const components::Pose *_pose)->bool
+        {
+          if (_name->Data() == modelName)
+          {
+            modelPoses.push_back(_pose->Data());
+          }
+          return true;
+        });
+      return true;
+    });
+
+  server.AddSystem(testSystem.systemPtr);
+  std::size_t nIters{2000};
+  server.Run(true, nIters, false);
+
+  EXPECT_EQ(nIters, modelPoses.size());
+
+  // The model is attached to link2 (it's canonical link) so it will fall during
+  // simulation. The new Z position of the model is an offset of -2 in the Z
+  // direction from the center of link2.
+  EXPECT_NEAR(-(2.0 - 0.2), modelPoses.back().Pos().Z(), 1e-2);
+}
+
+/////////////////////////////////////////////////
 // Test physics integration with revolute joints
 TEST_F(PhysicsSystemFixture, RevoluteJoint)
 {
