92ea93da23af6e3b17ad5c8b77a2fe60e7affee9
chapulina <burajiru.no.chapulina@gmail.com>
2020-02-07 18:42:22 -0800

    wip, implementing reusable TypeIcon

---
+++ src/gui/plugins/modules/TypeIcon.cc
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include "TypeIcon.hh"
+
+namespace ignition::gazebo
+{
+  /// \brief Private data class for TypeIcon
+  class TypeIconPrivate
+  {
+  };
+}
+
+using namespace ignition;
+using namespace gazebo;
+
+/////////////////////////////////////////////////
+TypeIcon::TypeIcon()
+  : dataPtr(std::make_unique<TypeIconPrivate>())
+{
+}
+
+/////////////////////////////////////////////////
+TypeIcon::~TypeIcon() = default;
