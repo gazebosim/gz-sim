7ed25d52dfb800016146120cee10f256f18a211f
John Shepherd <john@openrobotics.org>
2020-02-20 11:10:05 -0800

    Add in citadel updates

--- src/rendering/RenderUtil.cc
+++ src/rendering/RenderUtil.cc
@@ -18,6 +18,7 @@
 #include <map>
 #include <vector>
 
+#include <sdf/Actor.hh>
 #include <sdf/Element.hh>
 #include <sdf/Light.hh>
 #include <sdf/Link.hh>
@@ -31,6 +32,7 @@
 
 #include <ignition/math/Color.hh>
 #include <ignition/math/Helpers.hh>
+#include <ignition/math/Matrix4.hh>
 #include <ignition/math/Pose3.hh>
 
 #include <ignition/rendering.hh>
@@ -38,6 +40,7 @@
 #include <ignition/rendering/RenderingIface.hh>
 #include <ignition/rendering/Scene.hh>
 
+#include "ignition/gazebo/components/Actor.hh"
 #include "ignition/gazebo/components/Camera.hh"
 #include "ignition/gazebo/components/CastShadows.hh"
 #include "ignition/gazebo/components/DepthCamera.hh"
@@ -52,12 +55,15 @@
 #include "ignition/gazebo/components/Pose.hh"
 #include "ignition/gazebo/components/RgbdCamera.hh"
 #include "ignition/gazebo/components/Scene.hh"
+#include "ignition/gazebo/components/Temperature.hh"
+#include "ignition/gazebo/components/ThermalCamera.hh"
 #include "ignition/gazebo/components/Visual.hh"
 #include "ignition/gazebo/components/World.hh"
 #include "ignition/gazebo/EntityComponentManager.hh"
 
 #include "ignition/gazebo/rendering/RenderUtil.hh"
 #include "ignition/gazebo/rendering/SceneManager.hh"
+#include "ignition/gazebo/rendering/MarkerManager.hh"
 
 #include "ignition/gazebo/Util.hh"
 
@@ -84,11 +90,18 @@ class ignition::gazebo::RenderUtilPrivate
   /// \param[in] _ecm The entity-component manager
   public: void UpdateRenderingEntities(const EntityComponentManager &_ecm);
 
+  /// \brief Total time elapsed in simulation. This will not increase while
+  /// paused.
+  public: std::chrono::steady_clock::duration simTime{0};
+
   /// \brief Name of rendering engine
   public: std::string engineName = "ogre2";
 
   /// \brief Name of scene
   public: std::string sceneName = "scene";
+
+  /// \brief Initial Camera pose
+  public: math::Pose3d cameraPose = math::Pose3d(0, 0, 2, 0, 0.4, 0);
 
   /// \brief Scene background color
   public: math::Color backgroundColor = math::Color::Black;
@@ -98,6 +111,9 @@ class ignition::gazebo::RenderUtilPrivate
 
   /// \brief Scene manager
   public: SceneManager sceneManager;
+
+  /// \brief Marker manager
+  public: MarkerManager markerManager;
 
   /// \brief Pointer to rendering engine.
   public: ignition::rendering::RenderEngine *engine{nullptr};
@@ -125,6 +141,10 @@ class ignition::gazebo::RenderUtilPrivate
   /// [0] entity id, [1], SDF DOM, [2] parent entity id
   public: std::vector<std::tuple<Entity, sdf::Visual, Entity>> newVisuals;
 
+  /// \brief New actors to be created. The elements in the tuple are:
+  /// [0] entity id, [1], SDF DOM, [2] parent entity id
+  public: std::vector<std::tuple<Entity, sdf::Actor, Entity>> newActors;
+
   /// \brief New lights to be created. The elements in the tuple are:
   /// [0] entity id, [1], SDF DOM, [2] parent entity id
   public: std::vector<std::tuple<Entity, sdf::Light, Entity>> newLights;
@@ -140,6 +160,13 @@ class ignition::gazebo::RenderUtilPrivate
 
   /// \brief A map of entity ids and pose updates.
   public: std::map<Entity, math::Pose3d> entityPoses;
+
+  /// \brief A map of entity ids and actor transforms.
+  public: std::map<Entity, std::map<std::string, math::Matrix4d>>
+                          actorTransforms;
+
+  /// \brief A map of entity ids and temperature
+  public: std::map<Entity, float> entityTemp;
 
   /// \brief Mutex to protect updates
   public: std::mutex updateMutex;
@@ -194,10 +221,13 @@ void RenderUtil::UpdateFromECM(const UpdateInfo &_info,
 {
   IGN_PROFILE("RenderUtil::UpdateFromECM");
   std::lock_guard<std::mutex> lock(this->dataPtr->updateMutex);
+  this->dataPtr->simTime = _info.simTime;
+
   this->dataPtr->CreateRenderingEntities(_ecm, _info);
   if (_info.dt != std::chrono::steady_clock::duration::zero())
     this->dataPtr->UpdateRenderingEntities(_ecm);
   this->dataPtr->RemoveRenderingEntities(_ecm, _info);
+  this->dataPtr->markerManager.SetSimTime(_info.simTime);
 }
 
 //////////////////////////////////////////////////
@@ -230,17 +260,25 @@ void RenderUtil::Update()
   auto newModels = std::move(this->dataPtr->newModels);
   auto newLinks = std::move(this->dataPtr->newLinks);
   auto newVisuals = std::move(this->dataPtr->newVisuals);
+  auto newActors = std::move(this->dataPtr->newActors);
   auto newLights = std::move(this->dataPtr->newLights);
   auto removeEntities = std::move(this->dataPtr->removeEntities);
   auto entityPoses = std::move(this->dataPtr->entityPoses);
+  auto actorTransforms = std::move(this->dataPtr->actorTransforms);
+  auto entityTemp = std::move(this->dataPtr->entityTemp);
 
   this->dataPtr->newScenes.clear();
   this->dataPtr->newModels.clear();
   this->dataPtr->newLinks.clear();
   this->dataPtr->newVisuals.clear();
+  this->dataPtr->newActors.clear();
   this->dataPtr->newLights.clear();
   this->dataPtr->removeEntities.clear();
   this->dataPtr->entityPoses.clear();
+  this->dataPtr->actorTransforms.clear();
+  this->dataPtr->entityTemp.clear();
+
+  this->dataPtr->markerManager.Update();
 
   std::vector<std::tuple<Entity, sdf::Sensor, Entity>> newSensors;
   if (this->dataPtr->enableSensors)
@@ -281,7 +319,7 @@ void RenderUtil::Update()
   // create new entities
   {
     IGN_PROFILE("RenderUtil::Update Create");
-    for (auto &model : newModels)
+    for (const auto &model : newModels)
     {
       uint64_t iteration = std::get<3>(model);
       Entity entityId = std::get<0>(model);
@@ -302,19 +340,25 @@ void RenderUtil::Update()
           entityId, std::get<1>(model), std::get<2>(model));
     }
 
-    for (auto &link : newLinks)
+    for (const auto &link : newLinks)
     {
       this->dataPtr->sceneManager.CreateLink(
           std::get<0>(link), std::get<1>(link), std::get<2>(link));
     }
 
-    for (auto &visual : newVisuals)
+    for (const auto &visual : newVisuals)
     {
       this->dataPtr->sceneManager.CreateVisual(
           std::get<0>(visual), std::get<1>(visual), std::get<2>(visual));
     }
 
-    for (auto &light : newLights)
+    for (const auto &actor : newActors)
+    {
+      this->dataPtr->sceneManager.CreateActor(
+          std::get<0>(actor), std::get<1>(actor), std::get<2>(actor));
+    }
+
+    for (const auto &light : newLights)
     {
       this->dataPtr->sceneManager.CreateLight(
           std::get<0>(light), std::get<1>(light), std::get<2>(light));
@@ -322,10 +366,10 @@ void RenderUtil::Update()
 
     if (this->dataPtr->enableSensors && this->dataPtr->createSensorCb)
     {
-      for (auto &sensor : newSensors)
+      for (const auto &sensor : newSensors)
       {
          Entity entity = std::get<0>(sensor);
-         sdf::Sensor dataSdf = std::get<1>(sensor);
+         const sdf::Sensor &dataSdf = std::get<1>(sensor);
          Entity parent = std::get<2>(sensor);
 
          // two sensors with the same name cause conflicts. We'll need to use
@@ -374,6 +418,37 @@ void RenderUtil::Update()
 
       node->SetLocalPose(pose.second);
     }
+
+    // update entities' local transformations
+    for (auto &tf : actorTransforms)
+    {
+      auto actorMesh = this->dataPtr->sceneManager.ActorMeshById(tf.first);
+      auto actorVisual = this->dataPtr->sceneManager.NodeById(tf.first);
+      if (!actorMesh || !actorVisual)
+        continue;
+
+      math::Pose3d actorPose;
+      actorPose.Pos() = tf.second["actorPose"].Translation();
+      actorPose.Rot() = tf.second["actorPose"].Rotation();
+      actorVisual->SetLocalPose(actorPose);
+      tf.second.erase("actorPose");
+      actorMesh->SetSkeletonLocalTransforms(tf.second);
+    }
+
+    // set visual temperature
+    for (auto &temp : entityTemp)
+    {
+      auto node = this->dataPtr->sceneManager.NodeById(temp.first);
+      if (!node)
+        continue;
+
+      auto visual =
+          std::dynamic_pointer_cast<rendering::Visual>(node);
+      if (!visual)
+        continue;
+
+      visual->SetUserData("temperature", temp.second);
+    }
   }
 }
 
@@ -402,6 +477,7 @@ void RenderUtilPrivate::CreateRenderingEntities(
   const std::string cameraSuffix{"/image"};
   const std::string depthCameraSuffix{"/depth_image"};
   const std::string rgbdCameraSuffix{""};
+  const std::string thermalCameraSuffix{""};
   const std::string gpuLidarSuffix{"/scan"};
 
   // Treat all pre-existent entities as new at startup
@@ -433,7 +509,7 @@ void RenderUtilPrivate::CreateRenderingEntities(
         {
           sdf::Model model;
           model.SetName(_name->Data());
-          model.SetPose(_pose->Data());
+          model.SetRawPose(_pose->Data());
           this->newModels.push_back(
               std::make_tuple(_entity, model, _parent->Data(),
               _info.iterations));
@@ -450,7 +526,7 @@ void RenderUtilPrivate::CreateRenderingEntities(
         {
           sdf::Link link;
           link.SetName(_name->Data());
-          link.SetPose(_pose->Data());
+          link.SetRawPose(_pose->Data());
           this->newLinks.push_back(
               std::make_tuple(_entity, link, _parent->Data()));
           return true;
@@ -482,8 +558,28 @@ void RenderUtilPrivate::CreateRenderingEntities(
             visual.SetMaterial(material->Data());
           }
 
+          // todo(anyone) make visual updates more generic without using extra
+          // variables like entityTemp just for storing one specific visual
+          // param?
+          auto temp = _ecm.Component<components::Temperature>(_entity);
+          if (temp)
+          {
+            this->entityTemp[_entity] = temp->Data().Kelvin();
+          }
+
           this->newVisuals.push_back(
               std::make_tuple(_entity, visual, _parent->Data()));
+          return true;
+        });
+
+    // actors
+    _ecm.Each<components::Actor, components::ParentEntity>(
+        [&](const Entity &_entity,
+            const components::Actor *_actor,
+            const components::ParentEntity *_parent) -> bool
+        {
+          this->newActors.push_back(
+              std::make_tuple(_entity, _actor->Data(), _parent->Data()));
           return true;
         });
 
@@ -543,6 +639,17 @@ void RenderUtilPrivate::CreateRenderingEntities(
                          gpuLidarSuffix);
             return true;
           });
+
+      // Create thermal camera
+      _ecm.Each<components::ThermalCamera, components::ParentEntity>(
+        [&](const Entity &_entity,
+            const components::ThermalCamera *_thermalCamera,
+            const components::ParentEntity *_parent)->bool
+          {
+            addNewSensor(_entity, _thermalCamera->Data(), _parent->Data(),
+                         thermalCameraSuffix);
+            return true;
+          });
     }
     this->initialized = true;
   }
@@ -572,7 +679,7 @@ void RenderUtilPrivate::CreateRenderingEntities(
         {
           sdf::Model model;
           model.SetName(_name->Data());
-          model.SetPose(_pose->Data());
+          model.SetRawPose(_pose->Data());
           this->newModels.push_back(
               std::make_tuple(_entity, model, _parent->Data(),
               _info.iterations));
@@ -589,7 +696,7 @@ void RenderUtilPrivate::CreateRenderingEntities(
         {
           sdf::Link link;
           link.SetName(_name->Data());
-          link.SetPose(_pose->Data());
+          link.SetRawPose(_pose->Data());
           this->newLinks.push_back(
               std::make_tuple(_entity, link, _parent->Data()));
           return true;
@@ -610,7 +717,7 @@ void RenderUtilPrivate::CreateRenderingEntities(
         {
           sdf::Visual visual;
           visual.SetName(_name->Data());
-          visual.SetPose(_pose->Data());
+          visual.SetRawPose(_pose->Data());
           visual.SetGeom(_geom->Data());
           visual.SetCastShadows(_castShadows->Data());
 
@@ -623,6 +730,17 @@ void RenderUtilPrivate::CreateRenderingEntities(
 
           this->newVisuals.push_back(
               std::make_tuple(_entity, visual, _parent->Data()));
+          return true;
+        });
+
+    // actors
+    _ecm.EachNew<components::Actor, components::ParentEntity>(
+        [&](const Entity &_entity,
+            const components::Actor *_actor,
+            const components::ParentEntity *_parent) -> bool
+        {
+          this->newActors.push_back(
+              std::make_tuple(_entity, _actor->Data(), _parent->Data()));
           return true;
         });
 
@@ -682,6 +800,17 @@ void RenderUtilPrivate::CreateRenderingEntities(
                          gpuLidarSuffix);
             return true;
           });
+
+      // Create thermal camera
+      _ecm.EachNew<components::ThermalCamera, components::ParentEntity>(
+        [&](const Entity &_entity,
+            const components::ThermalCamera *_thermalCamera,
+            const components::ParentEntity *_parent)->bool
+          {
+            addNewSensor(_entity, _thermalCamera->Data(), _parent->Data(),
+                         thermalCameraSuffix);
+            return true;
+          });
     }
   }
 }
@@ -716,6 +845,19 @@ void RenderUtilPrivate::UpdateRenderingEntities(
         const components::Pose *_pose)->bool
       {
         this->entityPoses[_entity] = _pose->Data();
+        return true;
+      });
+
+  // actors
+  _ecm.Each<components::Actor, components::Pose>(
+      [&](const Entity &_entity,
+        const components::Actor *,
+        const components::Pose *)->bool
+      {
+        // TODO(anyone) Support setting actor pose from other systems
+        // this->entityPoses[_entity] = _pose->Data();
+        this->actorTransforms[_entity] =
+              this->sceneManager.ActorMeshAnimationAt(_entity, this->simTime);
         return true;
       });
 
@@ -763,6 +905,16 @@ void RenderUtilPrivate::UpdateRenderingEntities(
   _ecm.Each<components::GpuLidar, components::Pose>(
       [&](const Entity &_entity,
         const components::GpuLidar *,
+        const components::Pose *_pose)->bool
+      {
+        this->entityPoses[_entity] = _pose->Data();
+        return true;
+      });
+
+  // Update thermal cameras
+  _ecm.Each<components::ThermalCamera, components::Pose>(
+      [&](const Entity &_entity,
+        const components::ThermalCamera *,
         const components::Pose *_pose)->bool
       {
         this->entityPoses[_entity] = _pose->Data();
@@ -836,6 +988,14 @@ void RenderUtilPrivate::RemoveRenderingEntities(
         this->removeEntities[_entity] = _info.iterations;
         return true;
       });
+
+  // thermal cameras
+  _ecm.EachRemoved<components::ThermalCamera>(
+    [&](const Entity &_entity, const components::ThermalCamera *)->bool
+      {
+        this->removeEntities[_entity] = _info.iterations;
+        return true;
+      });
 }
 
 /////////////////////////////////////////////////
@@ -864,6 +1024,7 @@ void RenderUtil::Init()
     this->dataPtr->scene->SetBackgroundColor(this->dataPtr->backgroundColor);
   }
   this->dataPtr->sceneManager.SetScene(this->dataPtr->scene);
+  this->dataPtr->markerManager.Init(this->dataPtr->scene);
 }
 
 /////////////////////////////////////////////////
@@ -952,6 +1113,12 @@ void RenderUtil::SetEnableSensors(bool _enable,
 SceneManager &RenderUtil::SceneManager()
 {
   return this->dataPtr->sceneManager;
+}
+
+/////////////////////////////////////////////////
+MarkerManager &RenderUtil::MarkerManager()
+{
+  return this->dataPtr->markerManager;
 }
 
 /////////////////////////////////////////////////
