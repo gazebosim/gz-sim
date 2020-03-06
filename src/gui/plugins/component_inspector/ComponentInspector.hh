d706ced6cbe50d7896b3549354afec28a3a01192
chapulina <burajiru.no.chapulina@gmail.com>
2020-03-06 12:27:37 -0800

    linters

--- src/gui/plugins/component_inspector/ComponentInspector.hh
+++ src/gui/plugins/component_inspector/ComponentInspector.hh
@@ -42,8 +42,8 @@ namespace gazebo
   template <class DataType>
   void setData(QStandardItem *_item, const DataType &_data)
   {
-    // cppcheck-suppress syntaxError
-    if constexpr (traits::IsOutStreamable<std::ostream, DataType>::value)
+    if constexpr
+        (traits::IsOutStreamable<std::ostream, DataType>::value)  // NOLINT
     {
       std::stringstream ss;
       ss << _data;
