ce20b1eed6942f6611646e1b50edb1b0e4b7d9cc
Michael Carroll <michael@openrobotics.org>
2020-03-16 10:46:01 -0500

    Remove references to GazeboServer
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- src/ServerConfig.cc
+++ src/ServerConfig.cc
@@ -636,7 +636,7 @@ std::list<ServerConfig::PluginInfo> Parse(tinyxml2::XMLDocument &_doc)
     std::string name = nameStr == nullptr ? "" : nameStr;
     if (name.empty())
     {
-      ignerr << "GazeboServer plugin is missing the name attribute. "
+      ignerr << "Plugin is missing the name attribute. "
         << "Skipping this plugin.\n";
       continue;
     }
@@ -646,7 +646,7 @@ std::list<ServerConfig::PluginInfo> Parse(tinyxml2::XMLDocument &_doc)
     std::string file = fileStr == nullptr ? "" : fileStr;
     if (file.empty())
     {
-      ignerr << "A GazeboServer plugin with name[" << name << "] is "
+      ignerr << "A Plugin with name[" << name << "] is "
         << "missing the filename attribute. Skipping this plugin.\n";
       continue;
     }
@@ -656,7 +656,7 @@ std::list<ServerConfig::PluginInfo> Parse(tinyxml2::XMLDocument &_doc)
     std::string entityName = entityNameStr == nullptr ? "" : entityNameStr;
     if (entityName.empty())
     {
-      ignerr << "A GazeboServer plugin with name[" << name << "] and "
+      ignerr << "A Plugin with name[" << name << "] and "
         << "filename[" << file << "] is missing the entity_name attribute. "
         << "Skipping this plugin.\n";
       continue;
@@ -667,7 +667,7 @@ std::list<ServerConfig::PluginInfo> Parse(tinyxml2::XMLDocument &_doc)
     std::string entityType = entityTypeStr == nullptr ? "" : entityTypeStr;
     if (entityType.empty())
     {
-      ignerr << "A GazeboServer plugin with name[" << name << "] and "
+      ignerr << "A Plugin with name[" << name << "] and "
         << "filename[" << file << "] is missing the entity_type attribute. "
         << "Skipping this plugin.\n";
       continue;
