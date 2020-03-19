e5d476021b757b00ac6009d2bb68233a05d057db
Addisu Z. Taddese <addisu@openrobotics.org>
2020-03-19 16:50:25 -0500

    Add documentation, serializer, == operator to DetachableJoint component

--- include/ignition/gazebo/components/DetachableJoint.hh
+++ include/ignition/gazebo/components/DetachableJoint.hh
@@ -31,18 +31,69 @@ namespace gazebo
 inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
 namespace components
 {
+  /// \brief Data structure to hold information about the parent and child links
+  /// connected by a detachable joint
   struct DetachableJointInfo
   {
+    /// \brief Entity of the parent link
     Entity parentLink;
+    /// \brief Entity of the echild link
     Entity childLink;
-    // Only the "fixed" joint type is currently supported.
+    // \brief Type of joint. Only the "fixed" joint type is currently supported.
     std::string jointType = {"fixed"};
-  };
 
+    public: bool operator==(const DetachableJointInfo &_info) const
+    {
+      return (this->parentLink == _info.parentLink) &&
+             (this->childLink == _info.childLink) &&
+             (this->jointType == _info.jointType);
+    }
+
+    public: bool operator!=(const DetachableJointInfo &_info) const
+    {
+      return !(*this == _info);
+    }
+  };
+}
+
+namespace serializers
+{
+  /// \brief Serializer for DetachableJointInfo object
+  class DetachableJointInfoSerializer
+  {
+    /// \brief Serialization for `DetachableJointInfo`.
+    /// \param[in] _out Output stream.
+    /// \param[in] _info DetachableJointInfo object to stream
+    /// \return The stream.
+    public: static std::ostream &Serialize(
+                std::ostream &_out,
+                const components::DetachableJointInfo &_info)
+    {
+      _out << _info.parentLink << " " << _info.childLink << " "
+           << _info.jointType;
+      return _out;
+    }
+
+    /// \brief Deserialization for `std::set<std::string>`.
+    /// \param[in] _in Input stream.
+    /// \param[in] _info DetachableJointInfo object to populate
+    /// \return The stream.
+    public: static std::istream &Deserialize(
+                std::istream &_in, components::DetachableJointInfo &_info)
+    {
+      _in >> _info.parentLink >> _info.childLink >> _info.jointType;
+      return _in;
+    }
+  };
+}
+
+namespace components
+{
   /// \brief A component that identifies an entity as being a detachable joint.
   /// It also contains additional information about the joint.
   using DetachableJoint =
-      Component<DetachableJointInfo, class DetachableJointTag>;
+      Component<DetachableJointInfo, class DetachableJointTag,
+                serializers::DetachableJointInfoSerializer>;
   IGN_GAZEBO_REGISTER_COMPONENT("ign_gazebo_components.DetachableJoint",
                                 DetachableJoint)
 }
