3e4d5c0df85ba89e656495f510120e19bcaaf122
chapulina <burajiru.no.chapulina@gmail.com>
2020-03-31 19:00:39 -0700

    port custom physics engine to Citadel

--- src/cmd/ign.cc
+++ src/cmd/ign.cc
@@ -61,7 +61,7 @@ extern "C" IGNITION_GAZEBO_VISIBLE int runServer(const char *_sdfString,
     int _iterations, int _run, float _hz, int _levels, const char *_networkRole,
     int _networkSecondaries, int _record, const char *_recordPath,
     int _logOverwrite, int _logCompress,
-    const char *_playback, const char *_file)
+    const char *_playback, const char *_physicsEngine, const char *_file)
 {
   ignition::gazebo::ServerConfig serverConfig;
 
@@ -266,6 +266,11 @@ extern "C" IGNITION_GAZEBO_VISIBLE int runServer(const char *_sdfString,
       serverConfig.SetLogPlaybackPath(ignition::common::absPath(
         std::string(_playback)));
     }
+  }
+
+  if (_physicsEngine != nullptr && std::strlen(_physicsEngine) > 0)
+  {
+    serverConfig.SetPhysicsEngine(_physicsEngine);
   }
 
   // Create the Gazebo server
--- src/cmd/ign.hh
+++ src/cmd/ign.hh
@@ -47,13 +47,15 @@ extern "C" IGNITION_GAZEBO_VISIBLE const char *worldInstallDir();
 /// \param[in] _logOverwrite --log-overwrite option
 /// \param[in] _logCompress --log-compress option
 /// \param[in] _playback --playback option
+/// \param[in] _physicsEngine --physics-engine option
 /// \param[in] _file Path to file being loaded
 /// \return 0 if successful, 1 if not.
 extern "C" IGNITION_GAZEBO_VISIBLE int runServer(const char *_sdfString,
     int _iterations, int _run, float _hz, int _levels,
     const char *_networkRole, int _networkSecondaries, int _record,
     const char *_recordPath, int _logOverwrite,
-    int _logCompress, const char *_playback, const char *_file);
+    int _logCompress, const char *_playback,
+    const char *_physicsEngine, const char *_file);
 
 /// \brief External hook to run simulation GUI.
 /// \param[in] _guiConfig Path to Ignition GUI configuration file.
