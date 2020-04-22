1b98bf14fc26844a9a0c80cce8343b7bd847943a
chapulina <lupoubel@hotmail.com>
2020-04-22 16:35:55 -0700

    [ign-gazebo2] Update BitBucket links (#63)
    * [ign-gazebo2] Update BitBucket links
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * [ign-gazebo2] Update BitBucket links
    
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    * Apply suggestions from code review
    
    Co-authored-by: Marya Belanger <marya@openrobotics.org>
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- docker/scripts/build_ign.sh
+++ docker/scripts/build_ign.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Command line parameters:
-# 1 - bitbucket organization name. For example ignitionrobotics or osrf.
+# 1 - github organization name. For example ignitionrobotics or osrf.
 # 2 - the name of the ignition repository. For example ign-math.
 # 3 - the name of the branch. For example ign-math6
 
 set -o errexit
 set -o verbose
 
-hg clone https://bitbucket.org/$1/$2 -b $3
+git clone https://github.com/$1/$2 -b $3
 cd $2 
 mkdir build
 cd build
