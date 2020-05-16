6da8157c8a0ebe1884fcfdbb2ad74196f92abbe0
John Shepherd <john@openrobotics.org>
2020-05-15 18:57:33 -0700

    updated tests
    Signed-off-by: John Shepherd <john@openrobotics.org>

--- test/integration/wheel_slip.cc
+++ test/integration/wheel_slip.cc
@@ -16,118 +16,149 @@
 */
 
 #include <gtest/gtest.h>
-#include "ignition/gazebo/Model.hh"
-#include "ignition/gazebo/test_config.hh"
-#include <ignition/math/Angle.hh>
+
+#include <ignition/msgs/entity_factory.pb.h>
+
+#include <ignition/common/Console.hh>
 #include <ignition/math/Pose3.hh>
-#include <ignition/math/SignalStats.hh>
 #include <ignition/transport/Node.hh>
+
+#include "ignition/gazebo/components/Collision.hh"
+#include "ignition/gazebo/components/Geometry.hh"
+#include "ignition/gazebo/components/Light.hh"
+#include "ignition/gazebo/components/Link.hh"
+#include "ignition/gazebo/components/Model.hh"
+#include "ignition/gazebo/components/Name.hh"
+#include "ignition/gazebo/components/ParentEntity.hh"
+#include "ignition/gazebo/components/Pose.hh"
+#include "ignition/gazebo/Server.hh"
+#include "ignition/gazebo/SystemLoader.hh"
+#include "ignition/gazebo/test_config.hh"
+
+#include "plugins/MockSystem.hh"
 
 using namespace ignition;
 using namespace gazebo;
 
+/// \brief Test DiffDrive system
 class WheelSlipTest : public ::testing::Test
-{ 
-  /// \brief Class to hold parameters for tire tests.
-  public: class WheelSlipState
-  { 
-    /// \brief Constructor.
-    public: WheelSlipState()
-    {
-    }
-    
-    /// \brief Destructor.
-    public: ~WheelSlipState() = default;
-    
-    /// \brief Axel force in lateral direction to expect.
-    public: double axelForceLateral = 0.0;
-    
-    /// \brief Axel force in lateral direction to expect.
-    public: double axelForceLongitudinal = 0.0;
-    
-    /// \brief Description to print during test loop.
-    public: std::string description;
-    
-    /// \brief Drum spin speed in rad/s.
-    public: double drumSpeed = 0.0;
-    
-    /// \brief Steer angle to apply.
-    public: ignition::math::Angle steer;
-    
-    /// \brief Suspension force to apply in N.
-    public: double suspForce = 0.0;
-    
-    /// \brief Wheel slip compliance in lateral direction;
-    public: double wheelSlipComplianceLateral = 0.01;
-    
-    /// \brief Wheel slip compliance in longitudinal direction;
-    public: double wheelSlipComplianceLongitudinal = 0.01;
-    
-    /// \brief Wheel spin speed in rad/s.
-    public: double wheelSpeed = 0.0;
-    
-    /// \brief P gain with wheel spin speed.
-    public: double wheelSpeedGain = 0.0;
-    
-    /// \brief Wheel torque in Nm.
-    public: double wheelTorque = 0.0;
-  };
-  
-  /// \brief Class to hold parameters for each model in the TriballDrift test.
-  public: class TriballDriftMeasurement
-  { 
-    /// \brief Reset stats and set reference pose.
-    public: void Reset()
-    { 
-      //this->pose0 = this->model->WorldPose();
-      this->statsPositionX.Reset();
-      this->statsVelocityX.Reset();
-    }
-    
-    /// \brief Take new measurements.
-    public: void Update()
-    {
-      //this->statsPositionX.InsertData(
-      //  this->pose0.Pos().X() - this->model->WorldPose().Pos().X());
-      //this->statsVelocityX.InsertData(
-      //  this->model->WorldLinearVel().X());
-    }
-    
-    /// \brief Model pointer. 
-    public: Model model;
-    
-    /// \brief Reference pose.
-    public: ignition::math::Pose3d pose0;
-    
-    /// \brief Statistics on position error in X.
-    public: ignition::math::SignalMaxAbsoluteValue statsPositionX;
-    
-    /// \brief Statistics on velocity error in X.
-    public: ignition::math::SignalMaxAbsoluteValue statsVelocityX;
-  };
-  
-  /// \brief Set joint commands for tire testrig.
-  /// \param[in] _wheelSpeed Wheel spin speed in rad/s.
-  /// \param[in] _drumSpeed Drum spin speed in rad/s.
-  /// \param[in] _suspForce Suspension force to apply in N.
-  /// \param[in] _steer Steer angle to apply.
-  public: void SetCommands(const WheelSlipState &_state);
-  
-  /// \brief Node for Ignition Transport topics.
-  protected: ignition::transport::Node ignNode;
-  
-  /// \brief Publisher of joint commands for the tire model.
-  protected: ignition::transport::Node::Publisher tireJointCmdPub;
-  
-  /// \brief Publisher of joint commands for the drum model.
-  protected: ignition::transport::Node::Publisher drumJointCmdPub;
-
-  /// \brief Joint pointer for drum spin joint.
-  protected: Entity drumJoint;
-
-  /// \brief Joint pointer for spin joint.
-  protected: Entity spinJoint;
-
-  /// \brief Joint pointer for steering joint.
-  protected: Entity steerJoint;
+{
+  // Documentation inherited
+  protected: void SetUp() override
+  {
+    common::Console::SetVerbosity(4);
+    setenv("IGN_GAZEBO_SYSTEM_PLUGIN_PATH",
+           (std::string(PROJECT_BINARY_PATH) + "/lib").c_str(), 1);
+  }
 };
+
+class Relay
+{
+  public: Relay()
+  {
+    auto plugin = loader.LoadPlugin("libMockSystem.so",
+                                "ignition::gazebo::MockSystem",
+                                nullptr);
+    EXPECT_TRUE(plugin.has_value());
+
+    this->systemPtr = plugin.value();
+
+    this->mockSystem = static_cast<MockSystem *>(
+        systemPtr->QueryInterface<System>());
+    EXPECT_NE(nullptr, this->mockSystem);
+  }
+
+  public: Relay &OnPreUpdate(MockSystem::CallbackType _cb)
+  {
+    this->mockSystem->preUpdateCallback = std::move(_cb);
+    return *this;
+  }
+
+  public: Relay &OnUpdate(MockSystem::CallbackType _cb)
+  {
+    this->mockSystem->updateCallback = std::move(_cb);
+    return *this;
+  }
+
+  public: Relay &OnPostUpdate(MockSystem::CallbackTypeConst _cb)
+  {
+    this->mockSystem->postUpdateCallback = std::move(_cb);
+    return *this;
+  }
+
+  public: SystemPluginPtr systemPtr;
+
+  private: SystemLoader loader;
+  private: MockSystem *mockSystem;
+};
+
+/*
+TEST_F(WheelSlipTest, TireDrum)
+{
+  const double metersPerMile = 1609.34;
+  const double secondsPerHour = 3600.0;
+
+  ServerConfig serverConfig;
+  const auto sdfFile = std::string(PROJECT_SOURCE_PATH) +
+    "test/worlds/tire_drum.sdf";
+  serverConfig.SetSdfFile(sdfFile);
+
+  Server server(serverConfig);
+  EXPECT_FALSE(server.Running());
+  EXPECT_FALSE(*server.Running(0));
+}
+*/
+TEST_F(WheelSlipTest, TricyclesUphill)
+{
+  ServerConfig serverConfig;
+  const auto sdfFile = std::string(PROJECT_SOURCE_PATH) +
+    "examples/worlds/trisphere_cycle_wheel_slip.sdf";
+  serverConfig.SetSdfFile(sdfFile);
+
+  Server server(serverConfig);
+  EXPECT_FALSE(server.Running());
+  EXPECT_FALSE(*server.Running(0));
+
+  gazebo::EntityComponentManager *ecm = nullptr;
+  Relay testSystem;
+  testSystem.OnPreUpdate([&](const gazebo::UpdateInfo &,
+                             gazebo::EntityComponentManager &_ecm)
+      {
+        ecm = &_ecm;
+      });
+
+  // Create a system that records the vehicle poses
+  std::vector<math::Pose3d> poses;
+  server.AddSystem(testSystem.systemPtr);
+
+  // Run server and check we have the ECM
+  EXPECT_EQ(nullptr, ecm);
+  server.Run(true, 1, false);
+  EXPECT_NE(nullptr, ecm);
+
+
+  Entity trisphereCycle0Entity =
+    ecm->EntityByComponents(components::Model(),
+        components::Name("trisphere_cycle0"));
+
+  EXPECT_NE(gazebo::kNullEntity, trisphereCycle0Entity);
+
+  Entity trisphereCycle1Entity =
+    ecm->EntityByComponents(components::Model(),
+        components::Name("trisphere_cycle1"));
+
+  EXPECT_NE(gazebo::kNullEntity, trisphereCycle1Entity);
+
+  Entity wheelRearLeftEntity = ecm->EntityByComponents(
+      components::ParentEntity(trisphereCycle0Entity),
+      components::Name("wheel_rear_left"),
+      components::Link());
+
+  EXPECT_NE(gazebo::kNullEntity, wheelRearLeftEntity);
+  
+  Entity wheelRearLeftCollisionEntity = ecm->EntityByComponents(
+      components::ParentEntity(wheelRearLeftEntity),
+      components::Collision());
+
+  auto collisionGeometry = ecm->Component<components::Geometry>(wheelRearLeftCollisionEntity);
+}
