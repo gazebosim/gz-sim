97776932dec53aea171b5c73411cb847064259c3
John Shepherd <john@openrobotics.org>
2020-05-18 18:39:34 -0700

    add tricycle uphill test
    Signed-off-by: John Shepherd <john@openrobotics.org>

--- test/integration/wheel_slip.cc
+++ test/integration/wheel_slip.cc
@@ -23,14 +23,21 @@
 #include <ignition/math/Pose3.hh>
 #include <ignition/transport/Node.hh>
 
+#include <sdf/Sphere.hh>
+
 #include "ignition/gazebo/components/Collision.hh"
 #include "ignition/gazebo/components/Geometry.hh"
+#include "ignition/gazebo/components/Gravity.hh"
+#include "ignition/gazebo/components/JointVelocity.hh"
+#include "ignition/gazebo/components/JointVelocityCmd.hh"
 #include "ignition/gazebo/components/Light.hh"
+#include "ignition/gazebo/components/LinearVelocity.hh"
 #include "ignition/gazebo/components/Link.hh"
 #include "ignition/gazebo/components/Model.hh"
 #include "ignition/gazebo/components/Name.hh"
 #include "ignition/gazebo/components/ParentEntity.hh"
 #include "ignition/gazebo/components/Pose.hh"
+#include "ignition/gazebo/components/World.hh"
 #include "ignition/gazebo/Server.hh"
 #include "ignition/gazebo/SystemLoader.hh"
 #include "ignition/gazebo/test_config.hh"
@@ -91,7 +98,6 @@ class Relay
   private: SystemLoader loader;
   private: MockSystem *mockSystem;
 };
-
 /*
 TEST_F(WheelSlipTest, TireDrum)
 {
@@ -136,7 +142,18 @@ TEST_F(WheelSlipTest, TricyclesUphill)
   server.Run(true, 1, false);
   EXPECT_NE(nullptr, ecm);
 
+  // Get world and gravity
+  Entity worldEntity =
+    ecm->EntityByComponents(components::World());
 
+  EXPECT_NE(gazebo::kNullEntity, worldEntity);
+
+  auto gravity = ecm->Component<components::Gravity>(worldEntity);
+
+  EXPECT_NE(nullptr, gravity);
+  EXPECT_EQ(math::Vector3d(-2, 0, -9.8), gravity->Data());
+
+  // Get both models
   Entity trisphereCycle0Entity =
     ecm->EntityByComponents(components::Model(),
         components::Name("trisphere_cycle0"));
@@ -149,6 +166,7 @@ TEST_F(WheelSlipTest, TricyclesUphill)
 
   EXPECT_NE(gazebo::kNullEntity, trisphereCycle1Entity);
 
+  // Check rear left wheel of first model
   Entity wheelRearLeftEntity = ecm->EntityByComponents(
       components::ParentEntity(trisphereCycle0Entity),
       components::Name("wheel_rear_left"),
@@ -161,4 +179,94 @@ TEST_F(WheelSlipTest, TricyclesUphill)
       components::Collision());
 
   auto collisionGeometry = ecm->Component<components::Geometry>(wheelRearLeftCollisionEntity);
+  EXPECT_EQ(sdf::GeometryType::SPHERE, collisionGeometry->Data().Type());
+  EXPECT_NE(nullptr, collisionGeometry->Data().SphereShape());
+
+  const double wheelRadius = collisionGeometry->Data().SphereShape()->Radius();
+  EXPECT_DOUBLE_EQ(0.15, wheelRadius);
+  
+  // Get rear wheel spins of both models
+  Entity wheelRearLeftSpin0Entity = ecm->EntityByComponents(
+      components::ParentEntity(trisphereCycle0Entity),
+      components::Name("wheel_rear_left_spin"),
+      components::Link());
+
+  EXPECT_NE(gazebo::kNullEntity, wheelRearLeftSpin0Entity);
+  
+  Entity wheelRearRightSpin0Entity = ecm->EntityByComponents(
+      components::ParentEntity(trisphereCycle0Entity),
+      components::Name("wheel_rear_right_spin"),
+      components::Link());
+
+  EXPECT_NE(gazebo::kNullEntity, wheelRearRightSpin0Entity);
+  
+  Entity wheelRearLeftSpin1Entity = ecm->EntityByComponents(
+      components::ParentEntity(trisphereCycle1Entity),
+      components::Name("wheel_rear_left_spin"),
+      components::Link());
+
+  EXPECT_NE(gazebo::kNullEntity, wheelRearLeftSpin1Entity);
+  
+  Entity wheelRearRightSpin1Entity = ecm->EntityByComponents(
+      components::ParentEntity(trisphereCycle1Entity),
+      components::Name("wheel_rear_right_spin"),
+      components::Link());
+
+  EXPECT_NE(gazebo::kNullEntity, wheelRearRightSpin1Entity);
+
+  // Set speed of both models
+  const double angularSpeed = 6.0;
+
+  ecm->CreateComponent(
+        wheelRearLeftSpin0Entity,
+        components::JointVelocityCmd({angularSpeed})
+      );
+  
+  ecm->CreateComponent(
+        wheelRearRightSpin0Entity,
+        components::JointVelocityCmd({angularSpeed})
+      );
+
+  ecm->CreateComponent(
+        wheelRearLeftSpin1Entity,
+        components::JointVelocityCmd({angularSpeed})
+      );
+  
+  ecm->CreateComponent(
+        wheelRearRightSpin1Entity,
+        components::JointVelocityCmd({angularSpeed})
+      );
+
+  server.Run(true, 2000, false);
+
+  // compute expected slip
+  // normal force as passed to Wheel Slip in test world
+  const double wheelNormalForce = 32;
+  const double mass = 14.5;
+  const double forceRatio = (mass/2) * std::abs(gravity->Data().X()) / wheelNormalForce;
+  const double noSlipLinearSpeed = wheelRadius * angularSpeed;
+
+  auto wheelRearLeftVelocity = ecm->Component<components::JointVelocity>(wheelRearLeftSpin0Entity);
+  auto wheelRearRightVelocity = ecm->Component<components::JointVelocity>(wheelRearRightSpin0Entity);
+  auto worldVel = ecm->Component<components::WorldLinearVelocity>(trisphereCycle0Entity);
+
+  EXPECT_NE(nullptr, wheelRearLeftVelocity);
+  EXPECT_NE(nullptr, wheelRearRightVelocity);
+  EXPECT_NE(nullptr, worldVel);
+
+  EXPECT_NEAR(angularSpeed, wheelRearLeftVelocity->Data()[0], 3e-3);
+  EXPECT_NEAR(angularSpeed, wheelRearRightVelocity->Data()[0], 3e-3);
+  EXPECT_NEAR(noSlipLinearSpeed - worldVel->Data()[0], 0.0, 5e-3);
+  
+  wheelRearLeftVelocity = ecm->Component<components::JointVelocity>(wheelRearLeftSpin1Entity);
+  wheelRearRightVelocity = ecm->Component<components::JointVelocity>(wheelRearRightSpin1Entity);
+  worldVel = ecm->Component<components::WorldLinearVelocity>(trisphereCycle1Entity);
+
+  EXPECT_NE(nullptr, wheelRearLeftVelocity);
+  EXPECT_NE(nullptr, wheelRearRightVelocity);
+  EXPECT_NE(nullptr, worldVel);
+
+  EXPECT_NEAR(angularSpeed, wheelRearLeftVelocity->Data()[0], 3e-3);
+  EXPECT_NEAR(angularSpeed, wheelRearRightVelocity->Data()[0], 3e-3);
+  EXPECT_NEAR(noSlipLinearSpeed - worldVel->Data()[0], noSlipLinearSpeed * forceRatio, 5e-3);
 }
