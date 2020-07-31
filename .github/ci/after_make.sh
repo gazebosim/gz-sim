2155d62568fa8d6d3327052328e9e46c4a255279
Louise Poubel <louise@openrobotics.org>
2020-07-31 09:37:01 -0700

    IGN_CONFIG_PATH
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/ci/after_make.sh
+++ .github/ci/after_make.sh
@@ -6,6 +6,10 @@ set -e
 # Install (needed for some tests)
 make install
 
+# For ign-tools
+export IGN_CONFIG_PATH=/usr/local/share/ignition
+
+# For rendering / window tests
 Xvfb :1 -screen 0 1280x1024x24 &
 export DISPLAY=:1.0
 export RENDER_ENGINE_VALUES=ogre2
