35b01b3f3652e6c562052169e07d68a355cd1eab
Louise Poubel <louise@openrobotics.org>
2020-08-04 11:42:52 -0700

    more updates
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/ci/after_make.sh
+++ .github/ci/after_make.sh
@@ -2,5 +2,14 @@
 
 set -x
 
-# Install
+# Install (needed for some tests)
 make install
+
+# For ign-tools
+export IGN_CONFIG_PATH=/usr/local/share/ignition
+
+# For rendering / window tests
+Xvfb :1 -screen 0 1280x1024x24 &
+export DISPLAY=:1.0
+export RENDER_ENGINE_VALUES=ogre2
+export MESA_GL_VERSION_OVERRIDE=3.3
