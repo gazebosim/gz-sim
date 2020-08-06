3ae4654df7e8fdc38564882aa18feaf6810b8d70
Louise Poubel <louise@openrobotics.org>
2020-08-06 08:44:46 -0700

    Modernize actions CI (#269)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/ci-bionic/after_make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -l
-
-set -x
-
-# Install
-make install
---
+++ .github/ci/after_make.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+
+set -x
+
+# Install (needed for some tests)
+make install
+
+# For ign-tools
+export IGN_CONFIG_PATH=/usr/local/share/ignition
+
+# For rendering / window tests
+Xvfb :1 -screen 0 1280x1024x24 &
+export DISPLAY=:1.0
+export RENDER_ENGINE_VALUES=ogre2
+export MESA_GL_VERSION_OVERRIDE=3.3
