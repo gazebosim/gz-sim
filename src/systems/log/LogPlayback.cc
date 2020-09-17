06ba47be6a19ecee0e03cb93a8daf80250d22e9a
Ashton Larkin <ashton@openrobotics.org>
2020-09-16 23:01:28 -0400

    Handling multiple Parse(...) calls in a single Update
    Signed-off-by: Ashton Larkin <ashton@openrobotics.org>

--- src/systems/log/LogPlayback.cc
+++ src/systems/log/LogPlayback.cc
@@ -73,7 +73,10 @@ class ignition::gazebo::systems::LogPlaybackPrivate
   /// \brief Keeps track of which entity poses have updated
   /// according to the given message.
   /// \param[in] _msg Message containing pose updates.
-  public: void Parse(const msgs::Pose_V &_msg);
+  /// \param[in] _clear Whether the most recently cached pose updates
+  /// should be deleted or not. Useful for when Parse is called multiple
+  /// times in the same Update cycle.
+  public: void Parse(const msgs::Pose_V &_msg, bool &_clear);
 
   /// \brief Updates the ECM according to the given message.
   /// \param[in] _ecm Mutable ECM.
@@ -138,15 +141,21 @@ LogPlayback::~LogPlayback()
 }
 
 //////////////////////////////////////////////////
-void LogPlaybackPrivate::Parse(const msgs::Pose_V &_msg)
+void LogPlaybackPrivate::Parse(const msgs::Pose_V &_msg, bool &_clear)
 {
+  if (_clear)
+    this->recentEntityPoseUpdates.clear();
+
   // save the new entity pose updates
-  this->recentEntityPoseUpdates.clear();
   for (auto i=0; i < _msg.pose_size(); ++i)
   {
     const auto &pose = _msg.pose(i);
     this->recentEntityPoseUpdates.insert_or_assign(pose.id(), pose);
   }
+
+  // make sure that any future detected pose updates from the same Update
+  // cycle don't overwrite already-cached pose updates from the same cycle
+  _clear = false;
 }
 
 //////////////////////////////////////////////////
@@ -475,6 +484,10 @@ void LogPlayback::Update(const UpdateInfo &_info, EntityComponentManager &_ecm)
 
   msgs::Pose_V queuedPose;
 
+  // if new pose updates are received, make sure that only the cached poses
+  // from a previous Update cycle are cleared
+  bool clearCachedPoseUpdates = true;
+
   auto iter = this->dataPtr->batch.begin();
   while (iter != this->dataPtr->batch.end())
   {
@@ -483,7 +496,7 @@ void LogPlayback::Update(const UpdateInfo &_info, EntityComponentManager &_ecm)
     // Only set the last pose of a sequence of poses.
     if (msgType != "ignition.msgs.Pose_V" && queuedPose.pose_size() > 0)
     {
-      this->dataPtr->Parse(queuedPose);
+      this->dataPtr->Parse(queuedPose, clearCachedPoseUpdates);
       queuedPose.Clear();
     }
 
@@ -560,7 +573,7 @@ void LogPlayback::Update(const UpdateInfo &_info, EntityComponentManager &_ecm)
 
   if (queuedPose.pose_size() > 0)
   {
-    this->dataPtr->Parse(queuedPose);
+    this->dataPtr->Parse(queuedPose, clearCachedPoseUpdates);
   }
 
   // flag changed entity poses as periodically changed based on
