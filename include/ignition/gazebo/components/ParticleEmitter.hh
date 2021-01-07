527a53186b1bf8c9bfe7943e8938ff5d889843e5
Carlos Agüero <caguero@openrobotics.org>
2021-01-07 17:37:55 +0100

    Update particle emitter component to use ignition::msgs::ParticleEmitter.
    Signed-off-by: Carlos Agüero <caguero@openrobotics.org>

--- include/ignition/gazebo/components/ParticleEmitter.hh
+++ include/ignition/gazebo/components/ParticleEmitter.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Open Source Robotics Foundation
+ * Copyright (C) 2021 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,11 @@
 #ifndef IGNITION_GAZEBO_COMPONENTS_PARTICLEEMITTER_HH_
 #define IGNITION_GAZEBO_COMPONENTS_PARTICLEEMITTER_HH_
 
-#include <cstdint>
+#include <ignition/msgs/particle_emitter.pb.h>
+
 #include <istream>
 #include <ostream>
-#include <string>
 
-#include <ignition/math/Color.hh>
-#include <ignition/math/Vector3.hh>
 #include <ignition/gazebo/components/Component.hh>
 #include <ignition/gazebo/components/Factory.hh>
 #include <ignition/gazebo/config.hh>
@@ -36,96 +34,34 @@ namespace gazebo
 inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
 namespace particles
 {
-  /// \brief All possible emitter type.
-  enum class EmitterType {POINT, BOX, CYLINDER, ELLIPSOID};
-
   /// \brief Properties of a particle emitter.
-  /// A particle emitter also has:
-  ///    * a pose, which can be stored as a component of
-  ///      an emitter entity via ignition::gazebo::components::Pose
-  ///    * a name, which can be stored as a component of
-  ///      an emitter entity via ignition::gazebo::components::Name
-  ///    * a material, which can be stored as a component of
-  ///      an emitter entity via ignition::gazebo::components::Material
-  struct Emitter
+  class Emitter
   {
-    /// \brief The particle emitter's id.
-    unsigned int id;
-
-    /// \brief The emitter's type.
-    particles::EmitterType type;
-
-    /// The size of the emitter where the particles are sampled.
-    ignition::math::Vector3d size;
-
-    /// \brief How many particles per second should be emitted.
-    double rate;
-
-    /// \brief The number of seconds the emitter is active.
-    double duration;
-
-    /// \brief Whether particle emitter is enabled or not.
-    bool emitting;
-
-    /// \brief The particle dimensions (width, height, depth).
-    ignition::math::Vector3d particleSize;
-
-    /// \brief The number of seconds each particle will ’live’ for before
-    /// being destroyed.
-    double lifetime;
-
-    /// \brief The minimum velocity each particle is emitted (m/s).
-    double minVelocity;
-
-    /// \brief The maximum velocity each particle is emitted (m/s).
-    double maxVelocity;
-
-    /// \brief The starting color of the particles.
-    ignition::math::Color colorStart;
-
-    /// \brief The end color of the particles.
-    ignition::math::Color colorEnd;
-
-    /// \brief The amount by which to scale the particles in both x and y
-    /// direction per second.
-    double scaleRate;
-
-    /// \brief The path to the color image used as an affector.
-    std::string colorRangeImage;
-
+    /// \brief Equality operator. This function checks if the given
+    /// emitter has identical id than this object.
+    /// \param[in] _emitter The emitter to compare against.
+    /// \return True if this object matches the provided object.
     public: bool operator==(const Emitter &_emitter) const
     {
-      return this->id == _emitter.id;
+      return this->data.id() == _emitter.data.id();
     }
 
+    /// \brief Inequality operator. This function checks if the given
+    /// emitter has identical id than this object.
+    /// \param[in] _emitter The emitter to compare against.
+    /// \return True if this object doesn't match the provided object.
     public: bool operator!=(const Emitter &_emitter) const
     {
       return !(*this == _emitter);
     }
+
+    /// \brief The particle emitter internal data.
+    public: ignition::msgs::ParticleEmitter data;
   };
 }
 
 namespace serializers
 {
-  /// \brief Output stream overload for particles::EmitterType
-  inline std::ostream &operator<<(std::ostream &_out,
-                                  const particles::EmitterType &_type)
-  {
-    auto temp = static_cast<unsigned int>(_type);
-    _out << temp;
-    return _out;
-  }
-
-  /// \brief Input stream overload for particles::EmitterType
-  inline std::istream &operator>>(std::istream &_in,
-                                  particles::EmitterType &_type)
-  {
-    unsigned int temp = 0u;
-    if (_in >> temp)
-      _type = static_cast<particles::EmitterType>(temp);
-    return _in;
-  }
-
   /// \brief Serializer for components::ParticleEmitter object
   class ParticleEmitterSerializer
   {
@@ -136,13 +72,12 @@ namespace serializers
     public: static std::ostream &Serialize(std::ostream &_out,
                                            const particles::Emitter &_emitter)
     {
-      _out <<        _emitter.id           << " " << _emitter.type
-           << " " << _emitter.size         << " " << _emitter.rate
-           << " " << _emitter.duration     << " " << _emitter.emitting
-           << " " << _emitter.particleSize << " " << _emitter.lifetime
-           << " " << _emitter.minVelocity  << " " << _emitter.maxVelocity
-           << " " << _emitter.colorStart   << " " << _emitter.colorEnd
-           << " " << _emitter.scaleRate    << " " << _emitter.colorRangeImage;
+      if (!_emitter.data.SerializeToOstream(&_out))
+      {
+        ignerr << "Error serializing particle emitter: " << std::endl;
+        ignerr << _emitter.data.DebugString() << std::endl;
+      }
+
       return _out;
     }
 
@@ -153,13 +88,12 @@ namespace serializers
     public: static std::istream &Deserialize(std::istream &_in,
                                              particles::Emitter &_emitter)
     {
-      _in >> _emitter.id           >> _emitter.type
-          >> _emitter.size         >> _emitter.rate
-          >> _emitter.duration     >> _emitter.emitting
-          >> _emitter.particleSize >> _emitter.lifetime
-          >> _emitter.minVelocity  >> _emitter.maxVelocity
-          >> _emitter.colorStart   >> _emitter.colorEnd
-          >> _emitter.scaleRate    >> _emitter.colorRangeImage;
+      if (!_emitter.data.ParseFromIstream(&_in))
+      {
+        ignerr << "Error deserializing particle emitter: " << std::endl;
+        ignerr << _emitter.data.DebugString() << std::endl;
+      }
+
       return _in;
     }
   };
