0b54e827e886bb22d8dc6bf1c841f229c6753efe
Nate Koenig <nate@openrobotics.org>
2021-01-08 15:11:00 -0800

    Added missing version namespace
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- include/ignition/gazebo/detail/EntityComponentManager.hh
+++ include/ignition/gazebo/detail/EntityComponentManager.hh
@@ -32,6 +32,9 @@ namespace ignition
 {
 namespace gazebo
 {
+// Inline bracket to help doxygen filtering.
+inline namespace IGNITION_GAZEBO_VERSION_NAMESPACE {
+
 //////////////////////////////////////////////////
 namespace traits
 {
@@ -544,6 +547,7 @@ bool EntityComponentManager::RemoveComponent(Entity _entity)
 {
   const auto typeId = ComponentTypeT::typeId;
   return this->RemoveComponent(_entity, typeId);
+}
 }
 }
 }
