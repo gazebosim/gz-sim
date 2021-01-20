60aac163add9f2b2ee831734bae14ef43e8f686b
Jose Luis Rivero <jrivero@osrfoundation.org>
2021-01-20 05:24:50 +0100

    Use common::setenv for portability to Windows (#561)
    Signed-off-by: Jose Luis Rivero <jrivero@osrfoundation.org>

--- test/integration/examples_build.cc
+++ test/integration/examples_build.cc
@@ -19,6 +19,7 @@
 #include <string>
 
 #include <ignition/common/Console.hh>
+#include <ignition/common/Util.hh>
 #include <ignition/common/Filesystem.hh>
 
 #include "ignition/gazebo/test_config.hh"
--- test/integration/follow_actor_system.cc
+++ test/integration/follow_actor_system.cc
@@ -17,6 +17,7 @@
 
 #include <gtest/gtest.h>
 #include <ignition/common/Console.hh>
+#include <ignition/common/Util.hh>
 #include <ignition/math/Pose3.hh>
 
 #include "ignition/gazebo/components/Actor.hh"
@@ -41,8 +42,8 @@ class FollowActorTest : public ::testing::TestWithParam<int>
   protected: void SetUp() override
   {
     common::Console::SetVerbosity(4);
-    setenv("IGN_GAZEBO_SYSTEM_PLUGIN_PATH",
-           (std::string(PROJECT_BINARY_PATH) + "/lib").c_str(), 1);
+    ignition::common::setenv("IGN_GAZEBO_SYSTEM_PLUGIN_PATH",
+           (std::string(PROJECT_BINARY_PATH) + "/lib").c_str());
   }
 };
 
