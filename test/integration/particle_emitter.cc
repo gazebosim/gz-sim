8a1db3d3012bfdcfb673c266c998952d77475716
Nate Koenig <nate@openrobotics.org>
2021-02-17 21:00:15 -0800

    Remove extra main
    Signed-off-by: Nate Koenig <nate@openrobotics.org>

--- test/integration/particle_emitter.cc
+++ test/integration/particle_emitter.cc
@@ -160,11 +160,3 @@ TEST_F(ParticleEmitterTest, SDFLoad)
   EXPECT_TRUE(updateCustomChecked);
   EXPECT_TRUE(updateDefaultChecked);
 }
-
-/////////////////////////////////////////////////
-/// Main
-int main(int _argc, char **_argv)
-{
-  ::testing::InitGoogleTest(&_argc, _argv);
-  return RUN_ALL_TESTS();
-}
