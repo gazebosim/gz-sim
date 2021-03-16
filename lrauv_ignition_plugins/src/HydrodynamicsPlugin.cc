49c11f31bc275e60190d94027b1fab2bcc3ef11f
Arjo Chakravarty <arjo@openrobotics.org>
2021-03-16 13:16:41 +0800

    fix compile issues
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- lrauv_ignition_plugins/src/HydrodynamicsPlugin.cc
+++ lrauv_ignition_plugins/src/HydrodynamicsPlugin.cc
@@ -17,7 +17,7 @@
 
 #include "HydrodynamicsPlugin.hh"
 
-#include <eigen/eigen.hpp>
+#include <Eigen/Eigen>
 
 namespace tethys_hydro
 {
@@ -130,14 +130,14 @@ void AddWorldPose (
 
 double SdfParamDouble(
     const std::shared_ptr<const sdf::Element> &_sdf,
-    const std::string& field,
-    double default)
+    const std::string& _field,
+    double _default)
 {
-  if(!_sdf->HasElement(field))
+  if(!_sdf->HasElement(_field))
   {
-    return default;
+    return _default;
   }
-  return _sdf->Get<double>(field);
+  return _sdf->Get<double>(_field);
 }
 
 HydrodynamicsPlugin::HydrodynamicsPlugin()
@@ -157,34 +157,35 @@ void HydrodynamicsPlugin::Configure(
   ignition::gazebo::EventManager &/*_eventMgr*/
 )
 {
-  _pimpl->waterDensity     = SdfParamDouble(_sdf, "waterDensity", 997.7735);
-  _pimpl->paramXdotU       = SdfParamDouble(_sdf, "xDotU"       , 5);
-  _pimpl->paramYdotV       = SdfParamDouble(_sdf, "yDotV"       , 5);
-  _pimpl->paramZdotW       = SdfParamDouble(_sdf, "zDotW"       , 0.1);
-  _pimpl->paramKdotP       = SdfParamDouble(_sdf, "kDotP"       , 0.1);
-  _pimpl->paramMdotQ       = SdfParamDouble(_sdf, "mDotQ"       , 0.1);
-  _pimpl->paramNdotR       = SdfParamDouble(_sdf, "nDotR"       , 1);
-  _pimpl->paramXu          = SdfParamDouble(_sdf, "xU"          , 20);
-  _pimpl->paramXuu         = SdfParamDouble(_sdf, "xUU"         , 0);
-  _pimpl->paramYv          = SdfParamDouble(_sdf, "yV"          , 20);
-  _pimpl->paramYvv         = SdfParamDouble(_sdf, "yVV"         , 0);
-  _pimpl->paramZw          = SdfParamDouble(_sdf, "zW"          , 20);
-  _pimpl->paramZww         = SdfParamDouble(_sdf, "zWW"         , 0);
-  _pimpl->paramKp          = SdfParamDouble(_sdf, "kP"          , 20);
-  _pimpl->paramKpp         = SdfParamDouble(_sdf, "kPP"         , 0);
-  _pimpl->paramMq          = SdfParamDouble(_sdf, "mQ"          , 20);
-  _pimpl->paramMqq         = SdfParamDouble(_sdf, "mQQ"         , 0);
-  _pimpl->paramNr          = SdfParamDouble(_sdf, "nR"          , 20);
-  _pimpl->paramNrr         = SdfParamDouble(_sdf, "nRR"         , 0);
+  _data->waterDensity     = SdfParamDouble(_sdf, "waterDensity", 997.7735);
+  _data->paramXdotU       = SdfParamDouble(_sdf, "xDotU"       , 5);
+  _data->paramYdotV       = SdfParamDouble(_sdf, "yDotV"       , 5);
+  _data->paramZdotW       = SdfParamDouble(_sdf, "zDotW"       , 0.1);
+  _data->paramKdotP       = SdfParamDouble(_sdf, "kDotP"       , 0.1);
+  _data->paramMdotQ       = SdfParamDouble(_sdf, "mDotQ"       , 0.1);
+  _data->paramNdotR       = SdfParamDouble(_sdf, "nDotR"       , 1);
+  _data->paramXu          = SdfParamDouble(_sdf, "xU"          , 20);
+  _data->paramXuu         = SdfParamDouble(_sdf, "xUU"         , 0);
+  _data->paramYv          = SdfParamDouble(_sdf, "yV"          , 20);
+  _data->paramYvv         = SdfParamDouble(_sdf, "yVV"         , 0);
+  _data->paramZw          = SdfParamDouble(_sdf, "zW"          , 20);
+  _data->paramZww         = SdfParamDouble(_sdf, "zWW"         , 0);
+  _data->paramKp          = SdfParamDouble(_sdf, "kP"          , 20);
+  _data->paramKpp         = SdfParamDouble(_sdf, "kPP"         , 0);
+  _data->paramMq          = SdfParamDouble(_sdf, "mQ"          , 20);
+  _data->paramMqq         = SdfParamDouble(_sdf, "mQQ"         , 0);
+  _data->paramNr          = SdfParamDouble(_sdf, "nR"          , 20);
+  _data->paramNrr         = SdfParamDouble(_sdf, "nRR"         , 0);
 
   // Create model object, to access convenient functions
   auto model = ignition::gazebo::Model(_entity);
-  _pimpl->linkEntity = model.LinkByName();
+  auto link_name = _sdf->Get<std::string>("linkName");
+  _data->linkEntity = model.LinkByName(_ecm, link_name);
 
-  _pimpl->prevState = Eigen::VectorXd::Zero(6); 
+  _data->prevState = Eigen::VectorXd::Zero(6); 
 
-  AddWorldPose(_pimpl->linkEntity, _ecm);
-  AddAngularVelocityComponent(_pimpl->linkEntity, _ecm);
+  AddWorldPose(_data->linkEntity, _ecm);
+  AddAngularVelocityComponent(_data->linkEntity, _ecm);
 
 }
 
@@ -199,64 +200,79 @@ void HydrodynamicsPlugin::PreUpdate(
   Eigen::VectorXd state    = Eigen::VectorXd(6);
   Eigen::MatrixXd Cmat     = Eigen::MatrixXd::Zero(6, 6);
   Eigen::MatrixXd Dmat     = Eigen::MatrixXd::Zero(6, 6);
-  Eigen::MatrixXd Ma = Eigen::MatrixXd::zero(6,6);
+  Eigen::MatrixXd Ma = Eigen::MatrixXd::Zero(6,6);
 
   // Get vehicle state
-  ignition::gazebo::Link baseLink(_pimpl->linkEntity);
+  ignition::gazebo::Link baseLink(_data->linkEntity);
   auto linearVelocity = baseLink.WorldLinearVelocity(_ecm);
   auto rotationalVelocity = baseLink.WorldAngularVelocity(_ecm);
 
-  state << linearVelocity->X() << linearVelocity->Y() << linearVelocity->Z()
-    << rotationalVelocity->X() << rotationalVelocity->Y() << rotationalVelocity->Z();
+  state(0) = linearVelocity->X();
+  state(1) = linearVelocity->Y();
+  state(2) = linearVelocity->Z();
+
+  state(3) = rotationalVelocity->X();
+  state(4) = rotationalVelocity->Y();
+  state(5) = rotationalVelocity->Z();
+
+  auto dt = (double)_info.dt.count()/1000;
   
-  stateDot = (state - _pimpl->prevState)/_info.dt;
+  stateDot = (state - _data->prevState)/dt;
+
+  _data->prevState = state;
 
   // Added mass according to Fossen's equations (p 37)
-  Ma(0,0) = _pimpl->paramXdotU;
-  Ma(1,1) = _pimpl->paramYdotV;
-  Ma(2,2) = _pimpl->paramZdotW;
-  Ma(3,3) = _pimpl->paramKdotP;
-  Ma(4,4) = _pimpl->paramMdotQ;
-  Ma(5,5) = _pimpl->paramNdotR;
+  Ma(0,0) = _data->paramXdotU;
+  Ma(1,1) = _data->paramYdotV;
+  Ma(2,2) = _data->paramZdotW;
+  Ma(3,3) = _data->paramKdotP;
+  Ma(4,4) = _data->paramMdotQ;
+  Ma(5,5) = _data->paramNdotR;
   const Eigen::VectorXd kAmassVec = -1.0 * Ma * stateDot;
 
   // Coriollis forces for under water vehicles (Fossen P. 37)
   // Note: this is significantly different from VRX because we need to account
   // for the under water vehicle's additional DOF
-  Cmat(0,4) = - _pimpl->paramZdotW * state(2);
-  Cmat(0,5) = - _pimpl->paramYdotV * state(1);
-  Cmat(1,3) = _pimpl->paramZdotW * state(2);
-  Cmat(1,5) = - _pimpl->paramXdotU * state(0);
-  Cmat(2,3) = - _pimpl->paramYdotV * state(1);
-  Cmat(2,4) = _pimpl->paramXdotU * state(0);
-  Cmat(3,1) = - _pimpl->paramZdotW * state(2);
-  Cmat(3,2) = _pimpl->paramYdotV * state(1);
-  Cmat(3,4) = - _pimpl->paramNdotR * state(5);
-  Cmat(3,5) = _pimpl->paramMdotQ * state(4);
-  Cmat(4,0) = _pimpl->paramZdotW * state(2);
-  Cmat(4,2) = - _pimpl->paramXdotU * state(0);
-  Cmat(4,3) = _pimpl->paramNdotR * state(5);
-  Cmat(4,5) = - _pimpl->paramKdotP * state(3);
-  Cmat(5,0) = _pimpl->paramZdotW * state(2);
-  Cmat(5,1) = _pimpl->paramXdotU * state(0);
-  Cmat(5,3) = -_pimpl->paramMdotQ * state(4);
-  Cmat(5,4) = _pimpl->paramKdotP * state(3);
+  Cmat(0,4) = - _data->paramZdotW * state(2);
+  Cmat(0,5) = - _data->paramYdotV * state(1);
+  Cmat(1,3) = _data->paramZdotW * state(2);
+  Cmat(1,5) = - _data->paramXdotU * state(0);
+  Cmat(2,3) = - _data->paramYdotV * state(1);
+  Cmat(2,4) = _data->paramXdotU * state(0);
+  Cmat(3,1) = - _data->paramZdotW * state(2);
+  Cmat(3,2) = _data->paramYdotV * state(1);
+  Cmat(3,4) = - _data->paramNdotR * state(5);
+  Cmat(3,5) = _data->paramMdotQ * state(4);
+  Cmat(4,0) = _data->paramZdotW * state(2);
+  Cmat(4,2) = - _data->paramXdotU * state(0);
+  Cmat(4,3) = _data->paramNdotR * state(5);
+  Cmat(4,5) = - _data->paramKdotP * state(3);
+  Cmat(5,0) = _data->paramZdotW * state(2);
+  Cmat(5,1) = _data->paramXdotU * state(0);
+  Cmat(5,3) = -_data->paramMdotQ * state(4);
+  Cmat(5,4) = _data->paramKdotP * state(3);
   const Eigen::VectorXd kCmatVec = -1.0 * Cmat * state;
 
   // Damping forces (Fossen P. 43)
-  Dmat(0,0) = - _pimpl->paramXu - _pimpl->paramXuu * abs(state(0));
-  Dmat(1,1) = - _pimpl->paramYv - _pimpl->paramYvv * abs(state(1));
-  Dmat(2,2) = - _pimpl->paramZw - _pimpl->paramZww * abs(state(2));
-  Dmat(3,3) = - _pimpl->paramKp - _pimpl->paramKpp * abs(state(3));
+  Dmat(0,0) = - _data->paramXu - _data->paramXuu * abs(state(0));
+  Dmat(1,1) = - _data->paramYv - _data->paramYvv * abs(state(1));
+  Dmat(2,2) = - _data->paramZw - _data->paramZww * abs(state(2));
+  Dmat(3,3) = - _data->paramKp - _data->paramKpp * abs(state(3));
 
   const Eigen::VectorXd kDvec = -1.0 * Dmat * state;
 
   const Eigen::VectorXd kTotalWrench = kAmassVec + kDvec + kCmatVec;
 
-  ign::math::Vector3d totalForce(kTotalWrench(0),  kTotalWrench(1), kTotalWrench(2));
-  ign::math::Vector3d totalTorque(kTotalWrench(3),  kTotalWrench(4), kTotalWrench(5)); 
+  ignition::math::Vector3d totalForce(kTotalWrench(0),  kTotalWrench(1), kTotalWrench(2));
+  ignition::math::Vector3d totalTorque(kTotalWrench(3),  kTotalWrench(4), kTotalWrench(5)); 
 
   baseLink.AddWorldWrench(_ecm, totalForce, totalTorque);
 }
 
 };
+
+IGNITION_ADD_PLUGIN(
+  tethys_hydro::HydrodynamicsPlugin,
+  ignition::gazebo::System,
+  tethys_hydro::HydrodynamicsPlugin::ISystemConfigure,
+  tethys_hydro::HydrodynamicsPlugin::ISystemPreUpdate)
