3424980e66ebfecccbc16de61463ceea80845e82
G.Doisy <doisyg@users.noreply.github.com>
2021-04-21 09:14:37 +0200

    [DiffDrive] add enable/disable (#772)
    * add enable/disable diffdrive
    
    Signed-off-by: Guillaume Doisy <guillaume.doisy@wyca.fr>
    
    * remove debug
    
    Signed-off-by: Guillaume Doisy <guillaume.doisy@wyca.fr>
    
    * do not subscribe to enable if topic is empty
    
    Signed-off-by: Guillaume Doisy <guillaume.doisy@wyca.fr>
    
    * add test
    
    Signed-off-by: Guillaume Doisy <guillaume.doisy@wyca.fr>
    
    * lint and style
    
    Signed-off-by: Guillaume Doisy <guillaume.doisy@wyca.fr>
    
    * change enable type to bool and renamed to enabled
    
    Signed-off-by: Guillaume Doisy <guillaume.doisy@wyca.fr>

--- src/systems/diff_drive/DiffDrive.cc
+++ src/systems/diff_drive/DiffDrive.cc
@@ -59,6 +59,10 @@ class ignition::gazebo::systems::DiffDrivePrivate
   /// \param[in] _msg Velocity message
   public: void OnCmdVel(const ignition::msgs::Twist &_msg);
 
+  /// \brief Callback for enable/disable subscription
+  /// \param[in] _msg Boolean message
+  public: void OnEnable(const ignition::msgs::Boolean &_msg);
+
   /// \brief Update odometry and publish an odometry message.
   /// \param[in] _info System update information.
   /// \param[in] _ecm The EntityComponentManager of the given simulation
@@ -135,6 +139,9 @@ class ignition::gazebo::systems::DiffDrivePrivate
 
   /// \brief Last target velocity requested.
   public: msgs::Twist targetVel;
+
+  /// \brief Enable/disable state of the controller.
+  public: bool enabled;
 
   /// \brief A mutex to protect the target velocity command.
   public: std::mutex mutex;
@@ -256,6 +263,14 @@ void DiffDrive::Configure(const Entity &_entity,
     topic = _sdf->Get<std::string>("topic");
   this->dataPtr->node.Subscribe(topic, &DiffDrivePrivate::OnCmdVel,
       this->dataPtr.get());
+
+  // Subscribe to enable/disable
+  std::string enableTopic{"/model/" + this->dataPtr->model.Name(_ecm) +
+      "/enable"};
+
+  this->dataPtr->node.Subscribe(enableTopic, &DiffDrivePrivate::OnEnable,
+      this->dataPtr.get());
+  this->dataPtr->enabled = true;
 
   std::string odomTopic{"/model/" + this->dataPtr->model.Name(_ecm) +
     "/odometry"};
@@ -521,7 +536,23 @@ void DiffDrivePrivate::UpdateVelocity(const ignition::gazebo::UpdateInfo &_info,
 void DiffDrivePrivate::OnCmdVel(const msgs::Twist &_msg)
 {
   std::lock_guard<std::mutex> lock(this->mutex);
-  this->targetVel = _msg;
+  if (this->enabled)
+  {
+    this->targetVel = _msg;
+  }
+}
+
+//////////////////////////////////////////////////
+void DiffDrivePrivate::OnEnable(const msgs::Boolean &_msg)
+{
+  std::lock_guard<std::mutex> lock(this->mutex);
+  this->enabled = _msg.data();
+  if (!this->enabled)
+  {
+    math::Vector3d zeroVector{0, 0, 0};
+    msgs::Set(this->targetVel.mutable_linear(), zeroVector);
+    msgs::Set(this->targetVel.mutable_angular(), zeroVector);
+  }
 }
 
 IGNITION_ADD_PLUGIN(DiffDrive,
