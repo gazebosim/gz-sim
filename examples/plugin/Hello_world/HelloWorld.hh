1e3121f324f2eac16e18aa1636846ceeec61f932
addy1997 <adwaitnaik2@gmail.com>
2021-04-27 04:48:41 +0530

    updated files added
    Signed-off-by: addy1997 <adwaitnaik2@gmail.com>

--- examples/plugin/Hello_world/HelloWorld.hh
+++ examples/plugin/Hello_world/HelloWorld.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Open Source Robotics Foundation
+ * Copyright (C) 2021 Open Source Robotics Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@
 #ifndef SYSTEM_PLUGIN_HELLOWORLD_HH_
 #define SYSTEM_PLUGIN_HELLOWORLD_HH_
 
-
 #include <ignition/gazebo/System.hh>
 
 namespace hello_world
@@ -27,7 +26,6 @@ namespace hello_world
   class HelloWorld: 
 
     public ignition::gazebo::System,
-
     public ignition::gazebo::ISystemPostUpdate
   
   {
