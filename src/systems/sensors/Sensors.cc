dfa8b59a793e84213e690c6ad1b48c27a7069cfa
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2021-05-16 20:38:55 -0300

    Call Scene::PostRenderGpuFlush
    Requires ign-rendering update to compile correctly
    
    Affects ignitionrobotics/ign-rendering#323
    
    Signed-off-by: Matias N. Goldberg <dark_sylinc@yahoo.com.ar>

--- src/systems/sensors/Sensors.cc
+++ src/systems/sensors/Sensors.cc
@@ -262,6 +262,15 @@ void SensorsPrivate::RunOnce()
       // publish data
       IGN_PROFILE("RunOnce");
       this->sensorManager.RunOnce(this->updateTime);
+    }
+
+    {
+      IGN_PROFILE("PostRender");
+      // Update the scene graph manually to improve performance
+      // We only need to do this once per frame It is important to call
+      // sensors::RenderingSensor::SetManualSceneUpdate and set it to true
+      // so we don't waste cycles doing one scene graph update per sensor
+      this->scene->PostRenderGpuFlush();
       this->eventManager->Emit<events::PostRender>();
     }
 
