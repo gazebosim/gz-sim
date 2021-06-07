585f31e2bafd13cda7752eb77c2fab6bbbf1ab74
Mabel Zhang <mabel@openrobotics.org>
2021-06-07 14:32:43 -0400

    change thruster unit to Newtons
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

--- examples/standalone/multi_lrauv_race/multi_lrauv_race.cc
+++ examples/standalone/multi_lrauv_race/multi_lrauv_race.cc
@@ -36,8 +36,8 @@ double random_angle_within_limits(double min=-0.261799, double max=0.261799)
     (static_cast<float>(RAND_MAX / (max - min)));
 }
 
-// Nominal speed is thruster 300 rpm = 31.4 radians per second
-double random_thrust_within_limits(double min=-31.4, double max=31.4)
+// Nominal speed is thruster 300 rpm ~ 31.4 radians per second ~ 6.14 Newtons
+double random_thrust_within_limits(double min=-6.14, double max=6.14)
 {
   return min + static_cast<float>(rand()) /
     (static_cast<float>(RAND_MAX / (max - min)));
@@ -95,13 +95,13 @@ int main(int argc, char** argv)
       rudderPubs[i].Publish(rudderMsg);
 
       propellerCmds[i] = random_thrust_within_limits(
-        -31.4 * artificial_speedup, 0);
+        -6.14 * artificial_speedup, 0);
       ignition::msgs::Double propellerMsg;
       propellerMsg.set_data(propellerCmds[i]);
       propellerPubs[i].Publish(propellerMsg);
 
       std::cout << "Commanding " << ns[i] << " rudder angle " << rudderCmds[i]
-        << " rad, thrust " << propellerCmds[i] << " rad/s" << std::endl;
+        << " rad, thrust " << propellerCmds[i] << " Newtons" << std::endl;
     }
 
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
