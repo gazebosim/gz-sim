e3ee0af764af957d2ebba6349eca7a06f47fa483
Louise Poubel <louise@openrobotics.org>
2021-07-29 18:03:37 -0700

    Fixes to forward-port: version, Windows, deprecations (#940)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- src/cmd/ModelCommandAPI.hh
+++ src/cmd/ModelCommandAPI.hh
@@ -18,14 +18,14 @@
 #include "ignition/gazebo/Export.hh"
 
 /// \brief External hook to get a list of available models.
-extern "C" IGNITION_GAZEBO_VISIBLE void cmdModelList();
+extern "C" void cmdModelList();
 
 /// \brief External hook to dump model information.
 /// \param[in] _modelName Model name.
 /// \param[in] _pose --pose option.
 /// \param[in] _link_name Link name.
 /// \param[in] _joint_name Joint name.
-extern "C" IGNITION_GAZEBO_VISIBLE void cmdModelInfo(
+extern "C" void cmdModelInfo(
     const char *_modelName, int _pose, const char *_linkName,
     const char *_jointName);
 
