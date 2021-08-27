fe95598b77d75951a14c0452cc2410cb323bd1c7
Ashton Larkin <ashton@openrobotics.org>
2021-08-26 19:43:54 -0500

    temporary patch for race condition
    Signed-off-by: Ashton Larkin <ashton@openrobotics.org>

--- include/ignition/gazebo/detail/EntityComponentManager.hh
+++ include/ignition/gazebo/detail/EntityComponentManager.hh
@@ -20,6 +20,7 @@
 #include <cstring>
 #include <map>
 #include <memory>
+#include <mutex>
 #include <optional>
 #include <set>
 #include <tuple>
@@ -486,6 +487,9 @@ void EntityComponentManager::EachRemoved(typename identity<std::function<
 template<typename ...ComponentTypeTs>
 detail::View<ComponentTypeTs...> *EntityComponentManager::FindView() const
 {
+  static std::mutex mutex;
+  std::lock_guard<std::mutex> lock(mutex);
+
   auto viewKey = std::vector<ComponentTypeId>{ComponentTypeTs::typeId...};
 
   auto baseViewPtr = this->FindView(viewKey);
@@ -494,6 +498,7 @@ detail::View<ComponentTypeTs...> *EntityComponentManager::FindView() const
     auto view = static_cast<detail::View<ComponentTypeTs...>*>(baseViewPtr);
 
     // add any new entities to the view before using it
+    view->ToAddEntities();
     for (const auto &[entity, isNew] : view->ToAddEntities())
     {
       view->AddEntityWithConstComps(entity, isNew,
