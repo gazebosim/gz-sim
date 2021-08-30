52705157144e6d981cbf300aeff554d2aa1cca77
Jorge Perez <jjperez@ekumenlabs.com>
2021-08-30 12:09:02 -0500

    Restore to match ign-gazebo4 examples
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- examples/scripts/distributed_levels/secondary.sh
+++ examples/scripts/distributed_levels/secondary.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # --levels is implied by --network-role
-ign gazebo -s -v 3 -z 100000000 --network-role secondary $DIR/secondary.sdf
+ign gazebo -s -v 4 -z 100000000 --network-role secondary $DIR/secondary.sdf
 
 # Ignition Gazebo 1.x.x and 2.x.x support using environment variables to
 # configure distributed simulation. This capability is deprecated in
