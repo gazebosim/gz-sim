c36d4aae1075aa7dce0c950a107ff05bf9ec7f54
Michael Carroll <michael@openrobotics.org>
2021-11-08 12:52:22 -0600

    Add functionality to add entities via the entity tree (#1101)
    * Add functionality to add entities via the entity tree
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * Refactor common primitive shapes and lights to header
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * Tweaked entity tree button style (#1142)
    
    Signed-off-by: Nate Koenig <nate@openrobotics.org>
    
    Co-authored-by: Nate Koenig <nate@openrobotics.org>
    
    * Reorganize location of primitives
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * Add mesh support
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    * Fix lint
    
    Signed-off-by: Michael Carroll <michael@openrobotics.org>
    
    Co-authored-by: Nate Koenig <nkoenig@users.noreply.github.com>
    Co-authored-by: Nate Koenig <nate@openrobotics.org>

--- src/gui/plugins/entity_tree/EntityTree.cc
+++ src/gui/plugins/entity_tree/EntityTree.cc
@@ -17,14 +17,18 @@
 
 #include "EntityTree.hh"
 
+#include <algorithm>
 #include <iostream>
 #include <mutex>
 #include <set>
+#include <string>
 #include <vector>
 
 #include <ignition/common/Console.hh>
+#include <ignition/common/MeshManager.hh>
 #include <ignition/common/Profiler.hh>
 #include <ignition/gui/Application.hh>
+#include <ignition/gui/GuiEvents.hh>
 #include <ignition/gui/MainWindow.hh>
 #include <ignition/plugin/Register.hh>
 
@@ -41,8 +45,10 @@
 #include "ignition/gazebo/components/Sensor.hh"
 #include "ignition/gazebo/components/Visual.hh"
 #include "ignition/gazebo/components/World.hh"
-#include "ignition/gazebo/EntityComponentManager.hh"
 #include "ignition/gazebo/gui/GuiEvents.hh"
+
+#include "ignition/gazebo/EntityComponentManager.hh"
+#include "ignition/gazebo/Primitives.hh"
 
 namespace ignition::gazebo
 {
@@ -457,6 +463,65 @@ void EntityTree::DeselectAllEntities()
   ignition::gui::App()->sendEvent(
       ignition::gui::App()->findChild<ignition::gui::MainWindow *>(),
       &event);
+}
+
+/////////////////////////////////////////////////
+void EntityTree::OnInsertEntity(const QString &_type)
+{
+  std::string modelSdfString = getPrimitive(_type.toStdString());
+  ignition::gui::events::SpawnFromDescription event(modelSdfString);
+  ignition::gui::App()->sendEvent(
+      ignition::gui::App()->findChild<ignition::gui::MainWindow *>(),
+      &event);
+}
+
+/////////////////////////////////////////////////
+void EntityTree::OnLoadMesh(const QString &_mesh)
+{
+  std::string meshStr = _mesh.toStdString();
+  if (QUrl(_mesh).isLocalFile())
+  {
+    // mesh to sdf model
+    common::rtrim(meshStr);
+
+    if (!common::MeshManager::Instance()->IsValidFilename(meshStr))
+    {
+      QString errTxt = QString::fromStdString("Invalid URI: " + meshStr +
+        "\nOnly mesh file types DAE, OBJ, and STL are supported.");
+      return;
+    }
+
+    std::string filename = common::basename(meshStr);
+    std::vector<std::string> splitName = common::split(filename, ".");
+
+    std::string sdf = "<?xml version='1.0'?>"
+      "<sdf version='" + std::string(SDF_PROTOCOL_VERSION) + "'>"
+        "<model name='" + splitName[0] + "'>"
+          "<link name='link'>"
+            "<visual name='visual'>"
+              "<geometry>"
+                "<mesh>"
+                  "<uri>" + meshStr + "</uri>"
+                "</mesh>"
+              "</geometry>"
+            "</visual>"
+            "<collision name='collision'>"
+              "<geometry>"
+                "<mesh>"
+                  "<uri>" + meshStr + "</uri>"
+                "</mesh>"
+              "</geometry>"
+            "</collision>"
+          "</link>"
+        "</model>"
+      "</sdf>";
+
+    ignition::gui::events::SpawnFromDescription event(sdf);
+    ignition::gui::App()->sendEvent(
+        ignition::gui::App()->findChild<ignition::gui::MainWindow *>(),
+        &event);
+
+  }
 }
 
 /////////////////////////////////////////////////
