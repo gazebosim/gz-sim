44ed7f23a5fa5fa1120a15c6222ab2f3c5bbb031
William Lew <WilliamMilesLew@gmail.com>
2021-11-12 15:48:12 -0800

    Added light command to light controller
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

--- src/systems/light_controller/LightController.cc
+++ src/systems/light_controller/LightController.cc
@@ -29,6 +29,7 @@
 #include <ignition/gazebo/components/Name.hh>
 #include <ignition/gazebo/components/ParentEntity.hh>
 #include <ignition/gazebo/components/Light.hh>
+#include <ignition/gazebo/components/LightCmd.hh>
 
 using namespace ignition;
 using namespace gazebo;
@@ -36,6 +37,61 @@ using namespace systems;
 
 class ignition::gazebo::systems::LightControllerPrivate
 {
+  /// \brief Light equality comparison function.
+  public: std::function<bool(const msgs::Light &, const msgs::Light &)>
+    lightEql { [](const msgs::Light &_a, const msgs::Light &_b)
+    {
+      return
+        _a.type() == _b.type() &&
+        _a.name() == _b.name() &&
+        math::equal(
+           _a.diffuse().a(), _b.diffuse().a(), 1e-6f) &&
+        math::equal(
+          _a.diffuse().r(), _b.diffuse().r(), 1e-6f) &&
+        math::equal(
+          _a.diffuse().g(), _b.diffuse().g(), 1e-6f) &&
+        math::equal(
+          _a.diffuse().b(), _b.diffuse().b(), 1e-6f) &&
+        math::equal(
+          _a.specular().a(), _b.specular().a(), 1e-6f) &&
+        math::equal(
+          _a.specular().r(), _b.specular().r(), 1e-6f) &&
+        math::equal(
+          _a.specular().g(), _b.specular().g(), 1e-6f) &&
+        math::equal(
+          _a.specular().b(), _b.specular().b(), 1e-6f) &&
+        math::equal(
+          _a.range(), _b.range(), 1e-6f) &&
+        math::equal(
+          _a.attenuation_linear(),
+          _b.attenuation_linear(),
+          1e-6f) &&
+        math::equal(
+          _a.attenuation_constant(),
+          _b.attenuation_constant(),
+          1e-6f) &&
+        math::equal(
+          _a.attenuation_quadratic(),
+          _b.attenuation_quadratic(),
+          1e-6f) &&
+        _a.cast_shadows() == _b.cast_shadows() &&
+        math::equal(
+          _a.intensity(),
+          _b.intensity(),
+          1e-6f) &&
+        math::equal(
+          _a.direction().x(), _b.direction().x(), 1e-6) &&
+        math::equal(
+          _a.direction().y(), _b.direction().y(), 1e-6) &&
+        math::equal(
+          _a.direction().z(), _b.direction().z(), 1e-6) &&
+        math::equal(
+          _a.spot_inner_angle(), _b.spot_inner_angle(), 1e-6f) &&
+        math::equal(
+          _a.spot_outer_angle(), _b.spot_outer_angle(), 1e-6f) &&
+        math::equal(_a.spot_falloff(), _b.spot_falloff(), 1e-6f);
+    }};
+
   /// \brief Callback for light subscription
   /// \param[in] _msg A new message describing a light that needs
   /// to be followed
@@ -49,6 +105,9 @@ class ignition::gazebo::systems::LightControllerPrivate
 
   /// \brief Light data
   public: ignition::msgs::Light lightData;
+
+  /// \brief Update light data
+  public: bool isUpdateRequired;
 };
 
 //////////////////////////////////////////////////
@@ -77,6 +136,11 @@ void LightController::Configure(const Entity &_entity,
   }
 
   this->dataPtr->lightEntity = entity;
+
+  // auto component =
+  //   _ecm.Component<components::Light>(this->dataPtr->lightEntity);
+
+  // this->dataPtr->lightData = component->Data();
 
   // Subscribe to light commands
   auto lightTopic = _sdf->Get<std::string>("topic");
@@ -107,13 +171,35 @@ void LightController::Configure(const Entity &_entity,
 void LightController::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
     ignition::gazebo::EntityComponentManager &_ecm)
 {
+  if (this->dataPtr->isUpdateRequired)
+  {
+    auto lightCmdComp =
+      _ecm.Component<components::LightCmd>(this->dataPtr->lightEntity);
+    if (!lightCmdComp)
+    {
+      _ecm.CreateComponent(
+          this->dataPtr->lightEntity, components::LightCmd(
+          this->dataPtr->lightData));
+    }
+    else
+    {
+      auto state = lightCmdComp->SetData(this->dataPtr->lightData,
+          this->dataPtr->lightEql) ? ComponentState::OneTimeChange :
+          ComponentState::NoChange;
+      _ecm.SetChanged(this->dataPtr->lightEntity, components::LightCmd::typeId,
+          state);
+    }
+
+    this->dataPtr->isUpdateRequired = false;
+  }
 }
 
 //////////////////////////////////////////////////
 void LightControllerPrivate::LightCallback(
     const ignition::msgs::Light &_msg)
 {
-  std::cout << "Hello World!" << std::endl;
+  this->lightData = _msg;
+  this->isUpdateRequired = true;
 }
 
 IGNITION_ADD_PLUGIN(LightController,
