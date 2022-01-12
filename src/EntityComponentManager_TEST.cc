db38ea90f09f643dc45e237a190e790d5d28212c
Louise Poubel <louise@openrobotics.org>
2022-01-11 16:05:28 -0800

    Fix skipping tests on Windows (#1277)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- src/EntityComponentManager_TEST.cc
+++ src/EntityComponentManager_TEST.cc
@@ -21,6 +21,7 @@
 #include <ignition/common/Util.hh>
 #include <ignition/math/Pose3.hh>
 #include <ignition/math/Rand.hh>
+#include <ignition/utilities/ExtraTestMacros.hh>
 #include <ignition/utils/SuppressWarning.hh>
 
 #include "ignition/gazebo/components/CanonicalLink.hh"
