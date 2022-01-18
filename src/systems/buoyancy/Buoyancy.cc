e8a23c84d1cc2673c3c20ba6a4a3022ca1a5777e
Arjo Chakravarty <arjo@openrobotics.org>
2022-01-18 13:38:50 +0800

    apply fixes
    Signed-off-by: Arjo Chakravarty <arjo@openrobotics.org>

--- src/systems/buoyancy/Buoyancy.cc
+++ src/systems/buoyancy/Buoyancy.cc
@@ -221,7 +221,7 @@ std::pair<math::Vector3d, math::Vector3d> BuoyancyPrivate::ResolveForces(
     force += b.force;
     math::Pose3d localPoint{b.point, math::Quaterniond{1, 0, 0, 0}};
     auto globalPoint = b.pose * localPoint;
-    auto offset = globalPoint.Pos() - _pose.Pos();
+    auto offset = _pose.Pos() - globalPoint.Pos();
     torque += force.Cross(offset);
   }
 
@@ -472,13 +472,6 @@ void Buoyancy::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
       else if (this->dataPtr->buoyancyType
         == BuoyancyPrivate::BuoyancyType::GRADED_BUOYANCY)
       {
-        if (newPose)
-        {
-          // Skip entity if WorldPose and inertial are not yet ready
-          // TODO(arjo): Find a way of disabling gravity effects for
-          // this first iteration.
-          return true;
-        }
         std::vector<Entity> collisions = _ecm.ChildrenByComponents(
           _entity, components::Collision());
         this->dataPtr->buoyancyForces.clear();
@@ -523,8 +516,7 @@ void Buoyancy::PreUpdate(const ignition::gazebo::UpdateInfo &_info,
             }
           }
         }
-        auto [force, torque] = this->dataPtr->ResolveForces(
-        link.WorldInertialPose(_ecm).value());
+        auto [force, torque] = this->dataPtr->ResolveForces(linkWorldPose);
         // Apply the wrench to the link. This wrench is applied in the
         // Physics System.
         link.AddWorldWrench(_ecm, force, torque);
