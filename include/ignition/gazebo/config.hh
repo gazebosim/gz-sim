02a6f2b784cbcdeb80ad4165315d087afb7f7f60
methylDragon <methylDragon@gmail.com>
2022-05-18 02:40:38 -0700

    Implement deprecation trick
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- include/ignition/gazebo/config.hh
+++ include/ignition/gazebo/config.hh
@@ -15,4 +15,14 @@
  *
  */
 
+#ifndef IGNITION_SIM__CONFIG_HH_
+#define IGNITION_SIM__CONFIG_HH_
+
 #include <gz/sim/config.hh>
+
+namespace [[deprecated("Use gz namespace.")]]  ignition
+{
+  using namespace gz;
+}
+
+#endif
