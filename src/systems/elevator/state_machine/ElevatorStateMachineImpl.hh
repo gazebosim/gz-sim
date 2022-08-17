08932672d83fafdce095b28fdf65f3c35f615810
Louise Poubel <louise@openrobotics.org>
2022-08-17 14:37:43 -0700

    Fix Windows and Doxygen (#1643)
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- src/systems/elevator/state_machine/ElevatorStateMachineImpl.hh
+++ src/systems/elevator/state_machine/ElevatorStateMachineImpl.hh
@@ -36,8 +36,7 @@ namespace gz
 namespace sim
 {
 // Inline bracket to help doxygen filtering
-inline namespace GZ_SIM_VERSION_NAMESPACE
-{
+inline namespace GZ_SIM_VERSION_NAMESPACE {
 namespace systems
 {
 class ElevatorStateMachinePrivate
