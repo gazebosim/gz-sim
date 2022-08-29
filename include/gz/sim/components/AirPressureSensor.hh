gazebosim/gz-sensors

Migrate include statements
Signed-off-by: methylDragon <methylDragon@gmail.com>

include/gz/sensors/AirPressureSensor.hh
@@ -21,13 +21,13 @@
 
 #include <sdf/sdf.hh>
 
-#include <ignition/common/SuppressWarning.hh>
-#include <ignition/common/Time.hh>
+#include <gz/common/SuppressWarning.hh>
+#include <gz/common/Time.hh>
 
-#include <ignition/sensors/config.hh>
-#include <ignition/sensors/air_pressure/Export.hh>
+#include <gz/sensors/config.hh>
+#include <gz/sensors/air_pressure/Export.hh>
 
-#include "ignition/sensors/Sensor.hh"
+#include "gz/sensors/Sensor.hh"
 
 namespace gz
 {

gazebosim/gz-sim

include/gz/sim/components/AirPressureSensor.hh
@@ -19,10 +19,10 @@
 
 #include <sdf/Sensor.hh>
 
-#include <ignition/gazebo/components/Factory.hh>
-#include <ignition/gazebo/components/Component.hh>
-#include <ignition/gazebo/components/Serialization.hh>
-#include <ignition/gazebo/config.hh>
+#include <gz/sim/components/Factory.hh>
+#include <gz/sim/components/Component.hh>
+#include <gz/sim/components/Serialization.hh>
+#include <gz/sim/config.hh>
 
 namespace gz
 {