31e3fe714bc054760b6dfc2b7acfdb4d3e535a2b
Liam Han <liamhan0905@gmail.com>
2023-05-22 14:44:12 -0700

    reverting back the '/'
    Signed-off-by: Liam Han <liamhan0905@gmail.com>

--- test/integration/detachable_joint.cc
+++ test/integration/detachable_joint.cc
@@ -63,7 +63,7 @@ TEST_F(DetachableJointTest, IGN_UTILS_TEST_DISABLED_ON_WIN32(StartConnected))
 {
   using namespace std::chrono_literals;
 
-  this->StartServer("test/worlds/detachable_joint.sdf");
+  this->StartServer("/test/worlds/detachable_joint.sdf");
 
   // A lambda that takes a model name and a mutable reference to a vector of
   // poses and returns another lambda that can be passed to
@@ -141,7 +141,7 @@ TEST_F(DetachableJointTest, IGN_UTILS_TEST_DISABLED_ON_WIN32(LinksInSameModel))
 {
   using namespace std::chrono_literals;
 
-  this->StartServer("test/worlds/detachable_joint.sdf");
+  this->StartServer("/test/worlds/detachable_joint.sdf");
 
   // A lambda that takes a model name and a mutable reference to a vector of
   // poses and returns another lambda that can be passed to
@@ -230,7 +230,7 @@ TEST_F(DetachableJointTest, IGN_UTILS_TEST_DISABLED_ON_WIN32(LinksInSameModel))
  {
    using namespace std::chrono_literals;
 
-   this->StartServer(common::joinPaths("test", "worlds",
+   this->StartServer(common::joinPaths("/test", "worlds",
        "detachable_joint.sdf"));
 
    // A lambda that takes a model name and a mutable reference to a vector of
