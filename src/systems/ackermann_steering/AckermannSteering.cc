5aaaa7e197241ce48fa9065d1f29508178090f4c
ThreonX <76905674+ThreonX@users.noreply.github.com>
2023-06-12 14:37:40 +1200

    Added additional parameters, added proportional control for steering in normal mode
    Added separate parameters for angular speed, min_angular_velocity, max_angular_velocity, min_angular_acceleration, etc.
    
    Added the gainPAng to normal mode (not steering-only mode)
    
    Signed-off-by: ThreonX <76905674+ThreonX@users.noreply.github.com>
    Signed-off-by: Bac0nEater <thenickys123@gmail.com>

--- src/systems/ackermann_steering/AckermannSteering.cc
+++ src/systems/ackermann_steering/AckermannSteering.cc
@@ -331,47 +331,78 @@ void AckermannSteering::Configure(const Entity &_entity,
     this->dataPtr->gainPAng = _sdf->Get<double>("steer_p_gain");
   }
 
-  // Instantiate the speed limiters.
+  // Instantiate the linear speed limiters.
   this->dataPtr->limiterLin = std::make_unique<math::SpeedLimiter>();
-  this->dataPtr->limiterAng = std::make_unique<math::SpeedLimiter>();
 
-  // Parse speed limiter parameters.
+  // Parse linear speed limiter parameters.
   if (_sdf->HasElement("min_velocity"))
   {
     const double minVel = _sdf->Get<double>("min_velocity");
     this->dataPtr->limiterLin->SetMinVelocity(minVel);
-    this->dataPtr->limiterAng->SetMinVelocity(minVel);
   }
   if (_sdf->HasElement("max_velocity"))
   {
     const double maxVel = _sdf->Get<double>("max_velocity");
     this->dataPtr->limiterLin->SetMaxVelocity(maxVel);
-    this->dataPtr->limiterAng->SetMaxVelocity(maxVel);
   }
   if (_sdf->HasElement("min_acceleration"))
   {
     const double minAccel = _sdf->Get<double>("min_acceleration");
     this->dataPtr->limiterLin->SetMinAcceleration(minAccel);
-    this->dataPtr->limiterAng->SetMinAcceleration(minAccel);
   }
   if (_sdf->HasElement("max_acceleration"))
   {
     const double maxAccel = _sdf->Get<double>("max_acceleration");
     this->dataPtr->limiterLin->SetMaxAcceleration(maxAccel);
-    this->dataPtr->limiterAng->SetMaxAcceleration(maxAccel);
   }
   if (_sdf->HasElement("min_jerk"))
   {
     const double minJerk = _sdf->Get<double>("min_jerk");
     this->dataPtr->limiterLin->SetMinJerk(minJerk);
-    this->dataPtr->limiterAng->SetMinJerk(minJerk);
   }
   if (_sdf->HasElement("max_jerk"))
   {
     const double maxJerk = _sdf->Get<double>("max_jerk");
     this->dataPtr->limiterLin->SetMaxJerk(maxJerk);
-    this->dataPtr->limiterAng->SetMaxJerk(maxJerk);
   }
+
+  //Instantiate the angular speed limiters.
+  this->dataPtr->limiterAng = std::make_unique<math::SpeedLimiter>();
+
+  // Parse angular speed limiter parameters.
+  if (_sdf->HasElement("min_angular_velocity"))
+  {
+    const double minAngVel = _sdf->Get<double>("min_angular_velocity");
+    this->dataPtr->limiterAng->SetMinVelocity(minAngVel);
+  }
+  if (_sdf->HasElement("max_angular_velocity"))
+  {
+    const double maxAngVel = _sdf->Get<double>("max_angular_velocity");
+    this->dataPtr->limiterAng->SetMaxVelocity(maxAngVel);
+  }
+  if (_sdf->HasElement("min_angular_acceleration"))
+  {
+    const double minAngAccel = _sdf->Get<double>("min_angular_acceleration");
+    this->dataPtr->limiterAng->SetMinAcceleration(minAngAccel);
+  }
+  if (_sdf->HasElement("max_angular_acceleration"))
+  {
+    const double maxAngAccel = _sdf->Get<double>("max_angular_acceleration");
+    this->dataPtr->limiterAng->SetMaxAcceleration(maxAngAccel);
+  }
+  if (_sdf->HasElement("min_angular_jerk"))
+  {
+    const double minAngJerk = _sdf->Get<double>("min_angular_jerk");
+    this->dataPtr->limiterAng->SetMinJerk(minAngJerk);
+  }
+  if (_sdf->HasElement("max_angular_jerk"))
+  {
+    const double maxAngJerk = _sdf->Get<double>("max_angular_jerk");
+    this->dataPtr->limiterAng->SetMaxJerk(maxAngJerk);
+  }
+
+
+
 
   if (!this->dataPtr->steeringOnly)
   {
@@ -920,11 +951,9 @@ void AckermannSteeringPrivate::UpdateVelocity(
   double leftDelta = leftSteeringJointAngle - leftSteeringPos->Data()[0];
   double rightDelta = rightSteeringJointAngle - rightSteeringPos->Data()[0];
 
-  // Simple proportional control with a gain of 1
-  // Adding programmable PID values might be a future feature.
-  // Works as is for tested cases
-  this->leftSteeringJointSpeed = leftDelta;
-  this->rightSteeringJointSpeed = rightDelta;
+  // simple proportional controller
+  this->leftSteeringJointSpeed = leftDelta * this->gainPAng;
+  this->rightSteeringJointSpeed = rightDelta * this->gainPAng;
 }
 
 //////////////////////////////////////////////////
