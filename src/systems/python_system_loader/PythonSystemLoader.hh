41917631d77f3d26c0ecff62317522a6cbc38851
Addisu Z. Taddese <addisu@openrobotics.org>
2023-07-20 19:09:33 -0500

    Implement other system interfaces
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- src/systems/python_system_loader/PythonSystemLoader.hh
+++ src/systems/python_system_loader/PythonSystemLoader.hh
@@ -38,30 +38,43 @@ namespace systems
 class GZ_SIM_PYTHON_SYSTEM_LOADER_SYSTEM_HIDDEN PythonSystemLoader
     : public System,
       public ISystemConfigure,
-      // public ISystemReset,
       public ISystemPreUpdate,
-      // public ISystemUpdate,
-      public ISystemPostUpdate
+      public ISystemUpdate,
+      public ISystemPostUpdate,
+      public ISystemReset
 {
   // Documentation inherited
   public: void Configure(const Entity &_entity,
                          const std::shared_ptr<const sdf::Element> &_sdf,
                          EntityComponentManager &_ecm,
-                         EventManager &_eventMgr) override;
+                         EventManager &_eventMgr) final;
 
   // Documentation inherited
   public: void PreUpdate(const UpdateInfo &_info,
-                         EntityComponentManager &_ecm) override;
+                         EntityComponentManager &_ecm) final;
+
+  // Documentation inherited
+  public: void Update(const UpdateInfo &_info,
+                      EntityComponentManager &_ecm) final;
 
   // Documentation inherited
   public: void PostUpdate(const UpdateInfo &_info,
-                          const EntityComponentManager &_ecm) override;
+                          const EntityComponentManager &_ecm) final;
+
+  // Documentation inherited
+  public: void Reset(const UpdateInfo &_info,
+                     EntityComponentManager &_ecm) final;
+
+  private: template <typename ...Args>
+  void CallPythonMethod(pybind11::object _method, Args&&...);
 
   private: bool validConfig{false};
   private: pybind11::module_ pythonModule;
   private: pybind11::object pythonSystem;
   private: pybind11::object preUpdateMethod;
+  private: pybind11::object updateMethod;
   private: pybind11::object postUpdateMethod;
+  private: pybind11::object resetMethod;
 };
 }  // namespace systems
 }  // namespace GZ_SIM_VERSION_NAMESPACE
