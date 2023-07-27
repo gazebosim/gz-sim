e68a009e16be520f62e40a6f1f934e7e2a9bfd9a
Voldivh <eloyabmfcv@gmail.com>
2023-07-27 07:23:13 -0500

    Adds test for the bindings
    Signed-off-by: Voldivh <eloyabmfcv@gmail.com>

---
+++ python/test/joint_TEST.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# Copyright (C) 2023 Open Source Robotics Foundation
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import unittest
+
+from gz.common import set_verbosity
+from gz.sim8 import TestFixture, Joint, Model, World, world_entity
+from gz.math7 import Pose3d
+from sdformat13 import JointAxis, JointType
+
+post_iterations = 0
+iterations = 0
+pre_iterations = 0
+
+class TestJoint(unittest.TestCase):
+
+    def test_model(self):
+        set_verbosity(4)
+
+        file_path = os.path.dirname(os.path.realpath(__file__))
+        fixture = TestFixture(os.path.join(file_path, 'joint_test.sdf'))
+
+        def on_post_udpate_cb(_info, _ecm):
+            global post_iterations
+            post_iterations += 1
+
+        def on_pre_udpate_cb(_info, _ecm):
+            global pre_iterations
+            pre_iterations += 1
+            world_e = world_entity(_ecm)
+            self.assertEqual(1, world_e)
+            w = World(world_e)
+            m = Model(w.model_by_name(_ecm, 'model_test'))
+            joint = Joint(m.joint_by_name(_ecm, 'joint_test'))
+            # Entity Test
+            self.assertEqual(7, joint.entity())
+            # Valid Test
+            self.assertTrue(joint.valid(_ecm))
+            # Name Test
+            self.assertEqual('joint_test', joint.name(_ecm))
+            # Parent Link Name Test
+            self.assertEqual('link_test_1', joint.parent_link_name(_ecm))
+            # Child Link Name Test
+            self.assertEqual('link_test_2', joint.child_link_name(_ecm))
+            # Pose Test
+            self.assertEqual(Pose3d(0, 0.5, 0, 0, 0, 0), joint.pose(_ecm))
+            # Thread Pitch Test
+            self.assertEqual(2, joint.thread_pitch(_ecm))
+            # Axis Test
+            self.assertEqual(JointAxis().xyz(), joint.axis(_ecm)[0].xyz())
+            # Type Test
+            self.assertEqual(JointType.REVOLUTE, joint.type(_ecm))
+            # Sensors Test
+            self.assertEqual(8, joint.sensor_by_name(_ecm, 'sensor_test'))
+            self.assertEqual(1, joint.sensor_count(_ecm))
+            # Velocity Test
+            self.assertEqual(None, joint.velocity(_ecm))
+            joint.enable_velocity_check(_ecm, True)
+            joint.set_velocity(_ecm, [10])
+            # This should be 'self.assertEqual([10], joint.velocity(_ecm))'
+            self.assertEqual([], joint.velocity(_ecm))
+            joint.enable_velocity_check(_ecm, False)
+            # Position Test
+            self.assertEqual(None, joint.position(_ecm))
+            joint.enable_position_check(_ecm, True)
+            self.assertEqual([], joint.position(_ecm))
+            joint.enable_position_check(_ecm, False)
+
+        def on_udpate_cb(_info, _ecm):
+            global iterations
+            iterations += 1
+
+        fixture.on_post_update(on_post_udpate_cb)
+        fixture.on_update(on_udpate_cb)
+        fixture.on_pre_update(on_pre_udpate_cb)
+        fixture.finalize()
+
+        server = fixture.server()
+        server.run(True, 1000, False)
+
+        self.assertEqual(1000, pre_iterations)
+        self.assertEqual(1000, iterations)
+        self.assertEqual(1000, post_iterations)
+
+if __name__ == '__main__':
+    unittest.main()
