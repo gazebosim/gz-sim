2e1d18861d470651677d376d3ca04025adae594e
Ian Chen <ichen@openrobotics.org>
2023-08-25 03:12:30 +0000

    clear rendering objects in system
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- src/systems/shader_param/ShaderParam.cc
+++ src/systems/shader_param/ShaderParam.cc
@@ -89,6 +89,9 @@ class gz::sim::systems::ShaderParamPrivate
   /// \brief Connection to pre-render event callback
   public: gz::common::ConnectionPtr connection{nullptr};
 
+  /// \brief Connection to render tear down event callback
+  public: gz::common::ConnectionPtr teardownConnection{nullptr};
+
   /// \brief Name of visual this plugin is attached to
   public: std::string visualName;
 
@@ -118,6 +121,9 @@ class gz::sim::systems::ShaderParamPrivate
 
   /// \brief All rendering operations must happen within this call
   public: void OnUpdate();
+
+  /// \brief Callback to rendering engine tear down
+  public: void OnRenderTeardown();
 };
 
 /////////////////////////////////////////////////
@@ -246,6 +252,10 @@ void ShaderParam::Configure(const Entity &_entity,
   this->dataPtr->connection =
       _eventMgr.Connect<gz::sim::events::SceneUpdate>(
       std::bind(&ShaderParamPrivate::OnUpdate, this->dataPtr.get()));
+
+  this->dataPtr->teardownConnection =
+      _eventMgr.Connect<gz::sim::events::RenderTeardown>(
+      std::bind(&ShaderParamPrivate::OnRenderTeardown, this->dataPtr.get()));
 }
 
 //////////////////////////////////////////////////
@@ -494,6 +504,15 @@ void ShaderParamPrivate::OnUpdate()
       params = this->material->VertexShaderParams();
     (*params)[spv.name] = floatValue;
   }
+}
+
+//////////////////////////////////////////////////
+void ShaderParamPrivate::OnRenderTeardown()
+{
+  std::cerr << "shader param render tear down " << std::endl;
+  this->visual.reset();
+  this->scene.reset();
+  this->material.reset();
 }
 
 GZ_ADD_PLUGIN(ShaderParam,
