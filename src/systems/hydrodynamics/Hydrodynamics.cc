c92a7e439baa38abd5f934d8d02bb235e564f8b4
Michael Carroll <mjcarroll@intrinsic.ai>
2023-08-28 21:15:57 +0000

    Lint and remove deprecation
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>

--- src/systems/hydrodynamics/Hydrodynamics.cc
+++ src/systems/hydrodynamics/Hydrodynamics.cc
@@ -293,16 +293,7 @@ void Hydrodynamics::Configure(
   gz::sim::EventManager &/*_eventMgr*/
 )
 {
-  if (_sdf->HasElement("waterDensity"))
-  {
-    gzwarn <<
-      "<waterDensity> parameter is deprecated and will be removed in Gazebo G.\n"
-      << "\tPlease update your SDF to use <water_density> instead.";
-  }
-
-  this->dataPtr->waterDensity = SdfParamDouble(_sdf, "waterDensity",
-                                  SdfParamDouble(_sdf, "water_density", 998)
-                                );
+  this->dataPtr->waterDensity = SdfParamDouble(_sdf, "water_density", 998);
   // Load stability derivatives
   // Use SNAME 1950 convention to load the coeffecients.
   const auto snameConventionVel = "UVWPQR";
