8eaf9dc4a1ec2a14ead8d5e4cdab1d614d2555c4
ashbabu <shyamashi@gmail.com>
2023-10-04 17:12:12 +0100

    wrap to 80 char
    Signed-off-by: ashbabu <shyamashi@gmail.com>

--- src/Conversions.cc
+++ src/Conversions.cc
@@ -1432,19 +1432,23 @@ sdf::Sensor gazebo::convert(const msgs::Sensor &_in)
     {
       if (_in.gps().position().has_horizontal_noise())
       {
-          sensor.SetHorizontalPositionNoise(gazebo::convert<sdf::Noise>(_in.gps().position().horizontal_noise()));
+        sensor.SetHorizontalPositionNoise(gazebo::convert<sdf::Noise>(
+              _in.gps().position().horizontal_noise()));
       }
       if (_in.gps().position().has_vertical_noise())
       {
-          sensor.SetVerticalPositionNoise(gazebo::convert<sdf::Noise>(_in.gps().position().vertical_noise()));
+        sensor.SetVerticalPositionNoise(gazebo::convert<sdf::Noise>(
+              _in.gps().position().vertical_noise()));
       }
       if (_in.gps().velocity().has_horizontal_noise())
       {
-          sensor.SetHorizontalVelocityNoise(gazebo::convert<sdf::Noise>(_in.gps().velocity().horizontal_noise()));
+        sensor.SetHorizontalVelocityNoise(gazebo::convert<sdf::Noise>(
+              _in.gps().velocity().horizontal_noise()));
       }
       if (_in.gps().velocity().has_vertical_noise())
       {
-          sensor.SetVerticalVelocityNoise(gazebo::convert<sdf::Noise>(_in.gps().velocity().vertical_noise()));
+        sensor.SetVerticalVelocityNoise(gazebo::convert<sdf::Noise>(
+              _in.gps().velocity().vertical_noise()));
       }
     }
     else
@@ -1452,6 +1456,7 @@ sdf::Sensor gazebo::convert(const msgs::Sensor &_in)
       ignerr << "Attempting to convert an navsat sensor message, but the "
              << "message does not have a navsat nested message.\n";
     }
+
     out.SetNavSatSensor(sensor);
   }
   else if (out.Type() == sdf::SensorType::ALTIMETER)
