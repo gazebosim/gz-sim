606efea48b03e2232e2b69ffb7092cfc636c83f5
Mabel Zhang <mabel@openrobotics.org>
2023-10-14 05:18:40 -0400

    add components tutorial
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

--- src/systems/apply_joint_force/ApplyJointForce.cc
+++ src/systems/apply_joint_force/ApplyJointForce.cc
@@ -42,20 +42,26 @@ class gz::sim::systems::ApplyJointForcePrivate
   /// \brief Gazebo communication node.
   public: transport::Node node;
 
+  //! [jointEntityDeclaration]
   /// \brief Joint Entity
   public: Entity jointEntity;
+  //! [jointEntityDeclaration]
 
   /// \brief Joint name
   public: std::string jointName;
 
+  //! [forceDeclaration]
   /// \brief Commanded joint force
   public: double jointForceCmd;
+  //! [forceDeclaration]
 
   /// \brief mutex to protect jointForceCmd
   public: std::mutex jointForceCmdMutex;
 
+  //! [modelDeclaration]
   /// \brief Model interface
   public: Model model{kNullEntity};
+  //! [modelDeclaration]
 };
 
 //////////////////////////////////////////////////
@@ -65,12 +71,14 @@ ApplyJointForce::ApplyJointForce()
 }
 
 //////////////////////////////////////////////////
+//! [Configure]
 void ApplyJointForce::Configure(const Entity &_entity,
     const std::shared_ptr<const sdf::Element> &_sdf,
     EntityComponentManager &_ecm,
     EventManager &/*_eventMgr*/)
 {
   this->dataPtr->model = Model(_entity);
+  //! [Configure]
 
   if (!this->dataPtr->model.Valid(_ecm))
   {
@@ -96,17 +104,21 @@ void ApplyJointForce::Configure(const Entity &_entity,
   }
 
   // Subscribe to commands
+  //! [cmdTopic]
   auto topic = transport::TopicUtils::AsValidTopic("/model/" +
       this->dataPtr->model.Name(_ecm) + "/joint/" + this->dataPtr->jointName +
       "/cmd_force");
+  //! [cmdTopic]
   if (topic.empty())
   {
     gzerr << "Failed to create valid topic for [" << this->dataPtr->jointName
            << "]" << std::endl;
     return;
   }
+  //! [cmdSub]
   this->dataPtr->node.Subscribe(topic, &ApplyJointForcePrivate::OnCmdForce,
                                 this->dataPtr.get());
+  //! [cmdSub]
 
   gzmsg << "ApplyJointForce subscribing to Double messages on [" << topic
          << "]" << std::endl;
@@ -126,12 +138,14 @@ void ApplyJointForce::PreUpdate(const UpdateInfo &_info,
         << "s]. System may not work properly." << std::endl;
   }
 
+  //! [findJoint]
   // If the joint hasn't been identified yet, look for it
   if (this->dataPtr->jointEntity == kNullEntity)
   {
     this->dataPtr->jointEntity =
         this->dataPtr->model.JointByName(_ecm, this->dataPtr->jointName);
   }
+  //! [findJoint]
 
   if (this->dataPtr->jointEntity == kNullEntity)
     return;
@@ -140,12 +154,15 @@ void ApplyJointForce::PreUpdate(const UpdateInfo &_info,
   if (_info.paused)
     return;
 
+  //! [jointForceComponent]
   // Update joint force
   auto force = _ecm.Component<components::JointForceCmd>(
       this->dataPtr->jointEntity);
+  //! [jointForceComponent]
 
   std::lock_guard<std::mutex> lock(this->dataPtr->jointForceCmdMutex);
 
+  //! [modifyComponent]
   if (force == nullptr)
   {
     _ecm.CreateComponent(
@@ -156,14 +173,17 @@ void ApplyJointForce::PreUpdate(const UpdateInfo &_info,
   {
     force->Data()[0] += this->dataPtr->jointForceCmd;
   }
+  //! [modifyComponent]
 }
 
 //////////////////////////////////////////////////
+//! [setForce]
 void ApplyJointForcePrivate::OnCmdForce(const msgs::Double &_msg)
 {
   std::lock_guard<std::mutex> lock(this->jointForceCmdMutex);
   this->jointForceCmd = _msg.data();
 }
+//! [setForce]
 
 GZ_ADD_PLUGIN(ApplyJointForce,
                     System,
