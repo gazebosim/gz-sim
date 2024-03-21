497d8e1253208c87213d01823d1c2764fa134c1b
Ian Chen <ichen@openrobotics.org>
2024-03-21 18:53:56 +0000

    use dt
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- src/SimulationRunner.cc
+++ src/SimulationRunner.cc
@@ -463,14 +463,7 @@ void SimulationRunner::PublishStats()
 
   msg.set_paused(this->currentInfo.paused);
 
-  msg.mutable_step_size();
-  if (!this->currentInfo.paused)
-  {
-    auto stepSizeSecNsec =
-      math::durationToSecNsec(this->stepSize);
-    msg.mutable_step_size()->set_sec(stepSizeSecNsec.first);
-    msg.mutable_step_size()->set_nsec(stepSizeSecNsec.second);
-  }
+  msgs::Set(msg.mutable_step_size(), this->currentInfo.dt);
 
   if (this->Stepping())
   {
