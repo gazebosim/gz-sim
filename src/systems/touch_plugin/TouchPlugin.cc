0cc0464e904bb0bc05026580b1fa945e7c4ce33b
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2024-06-06 03:24:46 -0700

    Fix warning message to show precise jump back in time duration (#2435)
    ---------
    
    Signed-off-by: AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>

--- src/systems/touch_plugin/TouchPlugin.cc
+++ src/systems/touch_plugin/TouchPlugin.cc
@@ -251,8 +251,8 @@ void TouchPluginPrivate::Update(const UpdateInfo &_info,
   if (_info.dt < std::chrono::steady_clock::duration::zero())
   {
     gzwarn << "Detected jump back in time ["
-        << std::chrono::duration_cast<std::chrono::seconds>(_info.dt).count()
-        << "s]. System may not work properly." << std::endl;
+           << std::chrono::duration<double>(_info.dt).count()
+           << "s]. System may not work properly." << std::endl;
   }
 
   {
