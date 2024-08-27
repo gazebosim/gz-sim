166d8fe478391b111d96c7137dd677169386e29b
Steve Peters <scpeters@openrobotics.org>
2024-08-27 00:55:47 +0000

    clang-tidy: include what you use
    Add missing, remove unused headers, alphabetize.
    
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- src/LevelManager.cc
+++ src/LevelManager.cc
@@ -18,18 +18,23 @@
 #include "LevelManager.hh"
 
 #include <algorithm>
+#include <optional>
 
 #include <sdf/Actor.hh>
 #include <sdf/Atmosphere.hh>
+#include <sdf/Element.hh>
 #include <sdf/Joint.hh>
 #include <sdf/Light.hh>
 #include <sdf/Model.hh>
+#include <sdf/Plugin.hh>
 #include <sdf/World.hh>
 
 #include <gz/math/SphericalCoordinates.hh>
+#include <gz/common/Console.hh>
 #include <gz/common/Profiler.hh>
 
 #include "gz/sim/Events.hh"
+#include "gz/sim/Entity.hh"
 #include "gz/sim/EntityComponentManager.hh"
 
 #include "gz/sim/components/Actor.hh"
