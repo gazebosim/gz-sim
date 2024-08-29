ad0465bfd6df77cc647512d891ff31bee747a2ca
Addisu Z. Taddese <addisu@openrobotics.org>
2024-08-28 23:35:15 -0500

    Set include_server_config_plugins for `TestWorldSansPhysics`
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- src/Server_TEST.cc
+++ src/Server_TEST.cc
@@ -511,7 +511,7 @@ TEST_P(ServerFixture, SdfStringServerConfig)
   EXPECT_TRUE(*server.Paused());
   EXPECT_EQ(0u, *server.IterationCount());
   EXPECT_EQ(3u, *server.EntityCount());
-  EXPECT_EQ(5u, *server.SystemCount());
+  EXPECT_EQ(2u, *server.SystemCount());
 }
 
 /////////////////////////////////////////////////
