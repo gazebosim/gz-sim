d7eaf89720413db4e10539e5d73b5f228870a935
Saurabh Kamat <kamatsaurabh01@gmail.com>
2025-01-21 00:56:08 +0800

    Separated arguments in model exe callback
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

--- src/cmd/model_main.cc
+++ src/cmd/model_main.cc
@@ -15,6 +15,7 @@
  *
 */
 
+#include <optional>
 #include <string>
 
 #include <gz/utils/cli/CLI.hpp>
@@ -23,6 +24,8 @@
 #include "gz/sim/config.hh"
 #include "gz.hh"
 
+//////////////////////////////////////////////////
+/// \brief Enumeration of available model commands
 enum class ModelCommand
 {
   kNone,
@@ -30,21 +33,34 @@ enum class ModelCommand
   kModelInfo
 };
 
+//////////////////////////////////////////////////
+/// \brief Structure to hold all available model options
 struct ModelOptions
 {
+  /// \brief Command to execute
   ModelCommand command{ModelCommand::kNone};
 
+  /// \brief Name of the model to show
   std::string modelName{""};
 
+  /// \brief Flag to print pose of the model
   int pose{0};
 
-  std::string linkName{""};
+  /// \brief Name of the link to show when set using argument
+  /// Prints all links if set to empty string
+  std::optional<std::string> linkName;
 
-  std::string jointName{""};
+  /// \brief Name of the joint to show when set using argument
+  /// Prints all joints if set to empty string
+  std::optional<std::string> jointName;
 
-  std::string sensorName{""};
+  /// \brief Name of the sensor to show when set using argument
+  /// Prints all sensors if set to empty string
+  std::optional<std::string> sensorName;
 };
 
+//////////////////////////////////////////////////
+/// \brief Callback fired when options are successfully parsed
 void runModelCommand(const ModelOptions &_opt)
 {
   switch(_opt.command)
@@ -53,8 +69,22 @@ void runModelCommand(const ModelOptions &_opt)
       cmdModelList();
       break;
     case ModelCommand::kModelInfo:
-      cmdModelInfo(_opt.modelName.c_str(), _opt.pose, _opt.linkName.c_str(),
-                   _opt.jointName.c_str(), _opt.sensorName.c_str());
+      {
+        const char *linkName{nullptr};
+        if(_opt.linkName.has_value())
+          linkName = _opt.linkName.value().c_str();
+
+        const char *jointName{nullptr};
+        if(_opt.jointName.has_value())
+          jointName = _opt.jointName.value().c_str();
+
+        const char *sensorName{nullptr};
+        if(_opt.sensorName.has_value())
+          sensorName = _opt.sensorName.value().c_str();
+
+        cmdModelInfo(_opt.modelName.c_str(), _opt.pose, linkName,
+                     jointName, sensorName);
+      }
       break;
     case ModelCommand::kNone:
     default:
@@ -62,21 +92,13 @@ void runModelCommand(const ModelOptions &_opt)
   }
 }
 
+//////////////////////////////////////////////////
 void addModelFlags(CLI::App &_app)
 {
   auto opt = std::make_shared<ModelOptions>();
 
   _app.add_flag("-p,--pose", opt->pose,
                 "Print the pose of the model.");
-  _app.add_option("-l,--link", opt->linkName,
-                  "Select a link to show its properties.\n"
-                  "If no arg is passed all links are printed.");
-  _app.add_option("-s,--sensor", opt->sensorName,
-                  "Select a sensor to show its properties.\n"
-                  "If no arg is passed all sensors are printed.");
-  _app.add_option("-j,--joint", opt->jointName,
-                  "Select a joint to show its properties.\n"
-                  "If no arg is passed all joints are printed.");
 
   auto command = _app.add_option_group("command", "Command to be executed.");
 
@@ -86,16 +108,48 @@ void addModelFlags(CLI::App &_app)
     },
     "Get a list of available models.");
 
-  command->add_option_function<std::string>("-m,--model",
+  auto modelCmd = command->add_option_function<std::string>("-m,--model",
     [opt](const std::string &_modelName){
       opt->command = ModelCommand::kModelInfo;
       opt->modelName = _modelName;
     },
     "Select the model to be shown.");
 
+  auto linkCmd = command->add_option_function<std::string>("-l,--link",
+    [opt](const std::string &_linkName){
+      opt->linkName = _linkName;
+    },
+    "Select a link to show its properties.\n"
+    "If no arg is passed all links are printed.")
+    ->needs(modelCmd)
+    ->expected(0, 1)
+    ->default_val("");
+
+  command->add_option_function<std::string>("-s,--sensor",
+    [opt](const std::string &_sensorName){
+      opt->sensorName = _sensorName;
+    },
+    "Select a sensor to show its properties.\n"
+    "If no arg is passed all sensors are printed.")
+    ->needs(modelCmd)
+    ->needs(linkCmd)
+    ->expected(0, 1)
+    ->default_val("");
+
+  command->add_option_function<std::string>("-j,--joint",
+    [opt](const std::string &_jointName){
+      opt->jointName = _jointName;
+    },
+    "Select a joint to show its properties.\n"
+    "If no arg is passed all joints are printed.")
+    ->needs(modelCmd)
+    ->expected(0, 1)
+    ->default_val("");
+
   _app.callback([opt](){ runModelCommand(*opt); });
 }
 
+//////////////////////////////////////////////////
 int main(int argc, char** argv)
 {
   CLI::App app{"Utilities for information about models."};
