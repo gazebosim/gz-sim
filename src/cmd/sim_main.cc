c0dcc9873517ea244c40097ddd1e1a74432ba955
Saurabh Kamat <kamatsaurabh01@gmail.com>
2025-03-11 19:09:03 +0800

    Updated executable with SDF parsing and added render options
    Signed-off-by: Saurabh Kamat <kamatsaurabh01@gmail.com>

--- src/cmd/sim_main.cc
+++ src/cmd/sim_main.cc
@@ -28,6 +28,7 @@
 #include "gz.hh"
 
 //////////////////////////////////////////////////
+/// \brief Enumeration of available sim commands
 enum class SimCommand
 {
   kNone,
@@ -37,102 +38,147 @@ enum class SimCommand
 };
 
 //////////////////////////////////////////////////
+/// \brief Structure to hold all available sim options
 struct SimOptions
 {
+  /// \brief Command to execute
   SimCommand command{SimCommand::kNone};
 
-  std::string filename{""};
+  /// \brief Name of the SDFormat file
+  std::string file{""};
 
-  int runGui{0};
-
+  /// \brief Update rate in hertz
   int rate{-1};
 
+  /// \brief Initial simulation time (in seconds)
   double initialSimTime{0.0};
 
+  /// \brief Number of iterations to execute
   int iterations{0};
 
+  /// \brief Levels system to display models
   int levels{0};
 
+  /// \brief Participant role in distributed simulation environment
   std::string networkRole{""};
 
+  /// \brief Number of expected secondary participants
   int networkSecondaries{0};
 
+  /// \brief Record states and console
   int record{0};
 
+  /// \brief Custom path to store recorded files
   std::string recordPath{""};
 
+  /// \brief Records meshes and material files
   int recordResources{0};
 
+  /// \brief List of topics to record
   std::vector<std::string> recordTopics{};
 
+  /// \brief Time period between state recording (in seconds)
   float recordPeriod{-1};
 
+  /// \brief Overwrite existing files when recording
   int logOverwrite{0};
 
+  /// \brief Compress final log when recording
   int logCompress{0};
 
+  /// \brief Path to recorded states for playback
   std::string playback{""};
 
+  /// \brief Run simulation on start
   int runOnStart{0};
 
-  int runServer{0};
-
+  /// \brief Path to GUI configuration file
   std::string guiConfig{""};
 
+  /// \brief Physics engine plugin
   std::string physicsEngine{""};
 
+  /// \brief Render engine GUI plugin
   std::string renderEngineGui{""};
 
+  /// \brief Render engine GUI API Backend
   std::string renderEngineGuiApiBackend{""};
 
+  /// \brief Render engine Server plugin
   std::string renderEngineServer{""};
 
+  /// \brief Render engine Server API Backend
   std::string renderEngineServerApiBackend{""};
 
+  /// \brief Enable headless rendering
   int headlessRendering{0};
 
+  /// \brief Show the world loading menu
   int waitGui{1};
 
+  /// \brief Custom seed to random value generator
   int seed{1};
 };
 
 //////////////////////////////////////////////////
-void runSimCommand(const SimOptions &_opt)
+/// \brief Callback fired when options are successfully parsed
+void runSimCommand(SimOptions &_opt)
 {
   switch(_opt.command)
   {
     case SimCommand::kSimServer:
-      runServer(nullptr, _opt.iterations, _opt.runOnStart, _opt.rate,
-          _opt.initialSimTime, _opt.levels, _opt.networkRole.c_str(),
-          _opt.networkSecondaries, _opt.record, _opt.recordPath.c_str(),
-          _opt.recordResources, _opt.logOverwrite, _opt.logCompress,
-          _opt.playback.c_str(), _opt.physicsEngine.c_str(),
-          _opt.renderEngineServer.c_str(),
-          _opt.renderEngineServerApiBackend.c_str(),
-          _opt.renderEngineGui.c_str(),
-          _opt.renderEngineGuiApiBackend.c_str(), _opt.filename.c_str(),
-          _opt.recordTopics, _opt.waitGui, _opt.headlessRendering,
-          _opt.recordPeriod, _opt.seed);
+      {
+        if(checkFile(_opt.file) < 0)
+          return;
+
+        std::string parsedSdfFile;
+        if(parseSdfFile(_opt.file, parsedSdfFile) < 0)
+          return;
+
+        runServer(parsedSdfFile.c_str(), _opt.iterations, _opt.runOnStart, _opt.rate,
+                  _opt.initialSimTime, _opt.levels, _opt.networkRole.c_str(),
+                  _opt.networkSecondaries, _opt.record, _opt.recordPath.c_str(),
+                  _opt.recordResources, _opt.logOverwrite, _opt.logCompress,
+                  _opt.playback.c_str(), _opt.physicsEngine.c_str(),
+                  _opt.renderEngineServer.c_str(),
+                  _opt.renderEngineServerApiBackend.c_str(),
+                  _opt.renderEngineGui.c_str(),
+                  _opt.renderEngineGuiApiBackend.c_str(), _opt.file.c_str(),
+                  _opt.recordTopics, _opt.waitGui, _opt.headlessRendering,
+                  _opt.recordPeriod, _opt.seed);
+      }
       break;
     case SimCommand::kSimGui:
-        runGui(_opt.guiConfig.c_str(), _opt.filename.c_str(), _opt.waitGui,
+      {
+        if(checkFile(_opt.file) < 0)
+          return;
+
+        runGui(_opt.guiConfig.c_str(), _opt.file.c_str(), _opt.waitGui,
                _opt.renderEngineGui.c_str(),
                _opt.renderEngineGuiApiBackend.c_str());
-        break;
+      }
+      break;
     case SimCommand::kSimComplete:
       {
-        std::thread serverThread([_opt]{
-          runServer(nullptr, _opt.iterations, _opt.runOnStart, _opt.rate,
-              _opt.initialSimTime, _opt.levels, _opt.networkRole.c_str(),
-              _opt.networkSecondaries, _opt.record, _opt.recordPath.c_str(),
-              _opt.recordResources, _opt.logOverwrite, _opt.logCompress,
-              _opt.playback.c_str(), _opt.physicsEngine.c_str(),
-              _opt.renderEngineServer.c_str(),
-              _opt.renderEngineServerApiBackend.c_str(),
-              _opt.renderEngineGui.c_str(),
-              _opt.renderEngineGuiApiBackend.c_str(), _opt.filename.c_str(),
-              _opt.recordTopics, _opt.waitGui, _opt.headlessRendering,
-              _opt.recordPeriod, _opt.seed);
+        if(checkFile(_opt.file) < 0)
+          return;
+
+        std::string parsedSdfFile;
+        if(parseSdfFile(_opt.file, parsedSdfFile) < 0)
+          return;
+
+        std::thread serverThread([_opt, parsedSdfFile]{
+          runServer(parsedSdfFile.c_str(), _opt.iterations, _opt.runOnStart, _opt.rate,
+                    _opt.initialSimTime, _opt.levels, _opt.networkRole.c_str(),
+                    _opt.networkSecondaries, _opt.record,
+                    _opt.recordPath.c_str(), _opt.recordResources,
+                    _opt.logOverwrite, _opt.logCompress, _opt.playback.c_str(),
+                    _opt.physicsEngine.c_str(), _opt.renderEngineServer.c_str(),
+                    _opt.renderEngineServerApiBackend.c_str(),
+                    _opt.renderEngineGui.c_str(),
+                    _opt.renderEngineGuiApiBackend.c_str(),
+                    _opt.file.c_str(), _opt.recordTopics, _opt.waitGui,
+                    _opt.headlessRendering, _opt.recordPeriod, _opt.seed);
 
           // Server and GUI handle SIGINT independently.
           // When the first SIGINT is received, only one of the two processes
@@ -144,7 +190,7 @@ void runSimCommand(const SimOptions &_opt)
         });
 
         std::thread guiThread([_opt]{
-          runGui(_opt.guiConfig.c_str(), _opt.filename.c_str(), _opt.waitGui,
+          runGui(_opt.guiConfig.c_str(), _opt.file.c_str(), _opt.waitGui,
                 _opt.renderEngineGui.c_str(),
                 _opt.renderEngineGuiApiBackend.c_str());
 
@@ -174,12 +220,11 @@ void addSimFlags(CLI::App &_app)
 
   opt->command = SimCommand::kSimComplete;
 
-  auto filenameOpt =
-    _app.add_option_function<std::string>("filename",
-      [opt](const std::string &_filename){
-        opt->filename = _filename;
-      },
-      "Name of the SDFormat file.");
+  _app.add_option_function<std::string>("file",
+    [opt](const std::string &_file){
+      opt->file = _file;
+    },
+    "Name of the SDFormat file.");
 
   _app.add_option<double>("--initial-sim-time", opt->initialSimTime,
                           "Initial simulation time, in seconds.");
@@ -213,7 +258,7 @@ void addSimFlags(CLI::App &_app)
     "to join a distributed simulation environment.")
     ->needs(networkRoleOpt);
 
-  auto recordCmd = _app.add_flag("--record", opt->record,
+  _app.add_flag("--record", opt->record,
                 "Use logging system to record states and console\n"
                 "to default location, ~/.gz/sim/log.");
 
@@ -222,7 +267,7 @@ void addSimFlags(CLI::App &_app)
       opt->record = 1;
       opt->recordPath = _recordPath;
     },
-    "Specifies a custom path to put recorded files.\n"
+    "Specifies a custom path to store recorded files.\n"
     "This will enable console logging to a console.log\n"
     "file in the specified path.\n"
     "Note: Implicitly invokes --record");
@@ -254,22 +299,20 @@ void addSimFlags(CLI::App &_app)
     "   2. Record only the /stats topic.\n"
     "       --record-topic /stats\n"
     "   3. Record the /stats and /clock topics.\n"
-    "       --record-topic /stats \\n"
-    "       --record-topic /clock\n"
-    "Note: Implicitly invokes--record.");
+    "       --record-topic /stats --record-topic /clock"
+    "Note: Implicitly invokes --record.");
 
   _app.add_option("--record-period", opt->recordPeriod,
                   "Specify the time period (seconds) between\n"
-                  "state recording.")
-                  ->needs(recordCmd);
+                  "state recording.");
 
   _app.add_flag("--log-overwrite", opt->logOverwrite,
-                "Overwrite existing files when recording.")
-                ->needs(recordCmd);
+                "Overwrite existing files when recording.\n"
+                "Only valid if recording is enabled.");
 
   _app.add_flag("--log-compress", opt->logCompress,
-                "Compress final log when recording.")
-                ->needs(recordCmd);
+                "Compress final log when recording.\n"
+                "Only valid if recording is enabled.");
 
   _app.add_option("--seed", opt->seed,
                   "Pass a custom seed value to the random\n"
@@ -292,26 +335,83 @@ void addSimFlags(CLI::App &_app)
     "Use the logging system to play back states.\n"
     "Argument is the path to recorded states.");
 
+  _app.add_option("--physics-engine", opt->physicsEngine,
+                  "Gazebo Physics engine plugin to load. Gazebo\n"
+                  "will use DART by default (gz-physics-dartsim-plugin)\n"
+                  "Make sure custom plugins are inside\n"
+                  "GZ_SIM_PHYSICS_ENGINE_PATH.");
+
+  _app.add_option("--render-engine-gui", opt->renderEngineGui,
+                  "Gazebo Rendering engine plugin to load for the GUI.\n"
+                  "Gazebo will use OGRE2 by default. Make sure custom\n"
+                  "plugins are in GZ_SIM_RENDER_ENGINE_PATH.")
+                  ->default_str("ogre2");
+
+  _app.add_option("--render-engine-gui-api-backend",
+                  opt->renderEngineGuiApiBackend,
+                  "Same as --render-engine-api-backend but only\n"
+                  "for the GUI.")
+                  ->check(CLI::IsMember({"opengl", "vulkan", "metal"}));
+
+  _app.add_option("--render-engine-server", opt->renderEngineServer,
+                  "Gazebo Rendering engine plugin to load for the Server.\n"
+                  "Gazebo will use OGRE2 by default. Make sure custom\n"
+                  "plugins are in GZ_SIM_RENDER_ENGINE_PATH.")
+                  ->default_str("ogre2");
+
+  _app.add_option("--render-engine-server-api-backend",
+                  opt->renderEngineServerApiBackend,
+                  "Same as --render-engine-api-backend but only\n"
+                  "for the Server.")
+                  ->check(CLI::IsMember({"opengl", "vulkan", "metal"}));
+
+  _app.add_option_function<std::string>("--render-engine",
+    [opt](const std::string &_renderEngine){
+      opt->renderEngineGui = _renderEngine;
+      opt->renderEngineServer = _renderEngine;
+    },
+    "Gazebo Rendering engine plugin to load for both the Server\n"
+    "and the GUI. Gazebo will use OGRE2 by default.\n"
+    "Make sure custom plugins are inside\n"
+    "GZ_SIM_RENDER_ENGINE_PATH.")
+    ->default_str("ogre2");
+
+  _app.add_option_function<std::string>("--render-engine-api-backend",
+    [opt](const std::string &_renderEngineApiBackend){
+      opt->renderEngineGuiApiBackend = _renderEngineApiBackend;
+      opt->renderEngineServerApiBackend = _renderEngineApiBackend;
+    },
+    "API to use for both Server and GUI.\n"
+    "Possible values for ogre2:\n"
+    "   - opengl (default)\n"
+    "   - vulkan (beta)\n"
+    "   - metal (Apple only. Default for Apple)\n"
+    "Note: If Vulkan is being in the GUI and gz-gui was\n"
+    "built against Qt < 5.15.2, it may be very slow")
+    ->check(CLI::IsMember({"opengl", "vulkan", "metal"}));
+
   _app.add_flag("--headless-rendering", opt->headlessRendering,
                 "Run rendering in headless mode.");
 
   _app.add_flag("-r", opt->runOnStart,
                 "Run simulation on start.");
 
-  _app.add_flag("-z", opt->rate,
+  _app.add_option("-z", opt->rate,
                 "Update rate in hertz.");
 
   auto command = _app.add_option_group("command", "Command to be executed");
 
-  auto runGuiCmd = command->add_flag_callback("-g",
+  command->add_flag_callback("-g",
     [opt]() {
       opt->command = SimCommand::kSimGui;
+      opt->waitGui = 0;
     },
     "Run only the GUI.");
 
-  auto runServerCmd = command->add_flag_callback("-s",
+  command->add_flag_callback("-s",
     [opt](){
       opt->command = SimCommand::kSimServer;
+      opt->waitGui = 0;
     },
     "Run only the server (headless mode).\n"
     "This overrides -g, if it is also present.");
@@ -343,6 +443,18 @@ int main(int argc, char** argv)
   // Dummy flags handled by gz-tools
   app.add_flag("--force-version", "Use a particular library version.");
   app.add_flag("--versions", "Show the available versions.");
+
+  app.footer(
+    "Environment variables:\n"
+    "   GZ_SIM_RESOURCE_PATH          Colon separated paths used to locate\n"
+    "                                 resources such as worlds and models.\n"
+    "   GZ_SIM_SYSTEM_PLUGIN_PATH     Colon separated paths used to locate\n"
+    "                                 system plugins.\n"
+    "   GZ_SIM_SERVER_CONFIG_PATH     Path to server configuration file.\n"
+    "   GZ_GUI_PLUGIN_PATH            Colon separated paths used to locate\n"
+    "                                 GUI plugins.\n"
+    "   GZ_GUI_RESOURCE_PATH          Colon separated paths used to locate\n"
+    "                                 resources such as configuration files.");
 
   addSimFlags(app);
   app.formatter(std::make_shared<GzFormatter>(&app));
