39573e702978105af0dcd450d0e3ff58be1a111a
Jose Luis Rivero <jrivero@honurobotics.com>
2025-04-10 15:34:31 +0200

    Standalone gz sim executable (Jetty)
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- src/cmd/gz.hh
+++ src/cmd/gz.hh
@@ -14,31 +14,42 @@
  * limitations under the License.
  *
 */
-#ifndef GZ_SIM_GZ_HH_
-#define GZ_SIM_GZ_HH_
 
-#include "gz/sim/gz/Export.hh"
+#include <string>
+#include <vector>
 
-/// \brief External hook to read the library version.
-/// \return C-string representing the version. Ex.: 0.1.2
-extern "C" GZ_SIM_GZ_VISIBLE char *gzSimVersion();
-
-/// \brief Get the Gazebo version header.
-/// \return C-string containing the Gazebo version information.
-extern "C" GZ_SIM_GZ_VISIBLE char *simVersionHeader();
+#include "gz/sim/ServerConfig.hh"
 
 /// \brief Set verbosity level
 /// \param[in] _verbosity 0 to 4
-extern "C" GZ_SIM_GZ_VISIBLE void cmdVerbosity(
-    const char *_verbosity);
+void cmdVerbosity(const int _verbosity);
 
-extern "C" GZ_SIM_GZ_VISIBLE const char *worldInstallDir();
+/// \brief Get the install directory of world SDFormat files
+/// \return String containing the relative path
+const std::string worldInstallDir();
 
-/// \brief External hook to run simulation server.
+/// \brief Check if the file exists
+/// \param[in] _file Name of the SDFormat file
+/// \return 0 if exists, -1 in case of error
+int checkFile(std::string &_file);
+
+/// \brief Parse the SDFormat file into a string
+/// \param[in] _file Name of the SDFormat file
+/// \param[in] _parsedSdfFile Assign the parsed string
+/// \return 0 if successful, -1 in case of error
+int parseSdfFile(const std::string &_file, std::string &_parsedSdfFile);
+
+/// \brief Find or download a fuel world provided a URL.
+/// \param[in] _pathToResource Path to the fuel world resource, ie,
+/// https://staging-fuel.gazebosim.org/1.0/gmas/worlds/ShapesClone
+/// \return String containing the path to the local world sdf file
+std::string findFuelResource(const std::string &_pathToResource);
+
+/// \brief Create Gazebo server configuration
+/// \param[in] _config Gazebo server config
 /// \param[in] _sdfString SDF file to run, as a string.
-/// \param[in] _iterations --iterations option
-/// \param[in] _run -r option
 /// \param[in] _hz -z option
+/// \param[in] _initialSimTime --initial_sim_time option
 /// \param[in] _levels --levels option
 /// \param[in] _networkRole --network-role option
 /// \param[in] _networkSecondaries --network-secondaries option
@@ -61,19 +72,24 @@ extern "C" GZ_SIM_GZ_VISIBLE const char *worldInstallDir();
 /// \param[in] _headless True if server rendering should run headless
 /// \param[in] _recordPeriod --record-period option
 /// \param[in] _seed --seed value to be used for random number generator.
-/// \return 0 if successful, 1 if not.
-extern "C" GZ_SIM_GZ_VISIBLE int runServer(const char *_sdfString,
-    int _iterations, int _run, float _hz, double _initialSimTime, int _levels,
-    const char *_networkRole, int _networkSecondaries, int _record,
-    const char *_recordPath, int _recordResources, int _logOverwrite,
-    int _logCompress, const char *_playback,
-    const char *_physicsEngine,
-    const char *_renderEngineServer, const char *_renderEngineServerApiBackend,
-    const char *_renderEngineGui, const char *_renderEngineGuiApiBackend,
-    const char *_file, const char *_recordTopics, int _waitGui, int _headless,
-    float _recordPeriod, int _seed);
+/// \return 0 if successful, -1 in case of failure
+void createServerConfig(gz::sim::ServerConfig &_config, const char *_sdfString,
+                        float _hz, double _initialSimTime, int _levels,
+                        const char *_networkRole, int _networkSecondaries,
+                        int _record, const char *_recordPath,
+                        int _recordResources, int _logOverwrite,
+                        int _logCompress, const char *_playback,
+                        const char *_physicsEngine,
+                        const char *_renderEngineServer,
+                        const char *_renderEngineServerApiBackend,
+                        const char *_renderEngineGui,
+                        const char *_renderEngineGuiApiBackend,
+                        const char *_file,
+                        std::vector<std::string> _recordTopics, int _waitGui,
+                        int _headless, float _recordPeriod, int _seed);
 
-/// \brief External hook to run simulation GUI.
+#ifdef WITH_GUI
+/// \brief Run simulation GUI.
 /// \param[in] _guiConfig Path to Gazebo GUI configuration file.
 /// \param[in] _file The world file path passed as a command line argument.
 /// If set, QuickStart Dialog will not be shown.
@@ -81,18 +97,8 @@ extern "C" GZ_SIM_GZ_VISIBLE int runServer(const char *_sdfString,
 /// it receives a world path from GUI.
 /// \param[in] _renderEngine --render-engine-gui option
 /// \param[in] _renderEngineGuiApiBackend --render-engine-gui-api-backend option
-/// \return 0 if successful, 1 if not.
-extern "C" GZ_SIM_GZ_VISIBLE
+/// \return 0 if successful, -1 in case of failure
 int runGui(const char *_guiConfig, const char *_file,
-           int _waitGui,
-           const char *_renderEngine,
+           int _waitGui, const char *_renderEngine,
            const char *_renderEngineGuiApiBackend);
-
-/// \brief External hook to find or download a fuel world provided a URL.
-/// \param[in] _pathToResource Path to the fuel world resource, ie,
-/// https://staging-fuel.gazebosim.org/1.0/gmas/worlds/ShapesClone
-/// \return C-string containing the path to the local world sdf file
-extern "C" GZ_SIM_GZ_VISIBLE const char *findFuelResource(
-    char *_pathToResource);
-
 #endif
