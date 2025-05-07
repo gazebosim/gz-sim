f937aaaa429586c0fbac8720c4507ce485c04123
Jose Luis Rivero <jrivero@honurobotics.com>
2025-05-07 22:18:21 +0200

    Reduce and fix testSystem plugin
    Signed-off-by: Jose Luis Rivero <jrivero@honurobotics.com>

--- test/plugins/TestSystem.cc
+++ test/plugins/TestSystem.cc
@@ -17,21 +17,10 @@
 
 #include "TestSystem.hh"
 
-#include <gz/plugin/RegisterMore.hh>
-
-using namespace gz;
-using namespace sim;
-
-/////////////////////////////////////////////////
-TestSystem::TestSystem()
-  : System()
-{
-}
-
-/////////////////////////////////////////////////
-TestSystem::~TestSystem() = default;
+#include <gz/plugin/Register.hh>
 
 // Register this plugin
-GZ_ADD_PLUGIN(TestSystem, System)
+GZ_ADD_PLUGIN(gz::sim::TestSystem,
+    gz::sim::System)
 
 GZ_ADD_PLUGIN_ALIAS(TestSystem, "gz::sim::TestSystem")
