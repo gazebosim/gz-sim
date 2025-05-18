5723fa58ddf5691a1521c9ff60703e11ac9b7ea6
Pasquale Siciliano <66377952+PasqualeSicilianoIM@users.noreply.github.com>
2025-05-18 21:06:04 +0200

    Add files via upload
    This folder contains a simple example on how to create a plugin which set a linear velocity on a link entity. The CMakeLists.txt file is set for the gz-sim8.
    
    Signed-off-by: Pasquale Siciliano <66377952+PasqualeSicilianoIM@users.noreply.github.com>

---
+++ examples/plugin/velocity_plugin/PluginLinearVelocity.cc
@@ -0,0 +1,102 @@
+#include <string>
+#include <gz/common/Console.hh>
+#include <iostream>
+
+#include <gz/sim/Link.hh>
+#include <gz/sim/Model.hh>
+#include <gz/sim/Util.hh>
+
+#include <gz/plugin/Register.hh>
+#include <gz/common/Console.hh>
+
+#include <gz/sim/Types.hh>
+
+// This header is required to register plugins.
+// The good practice suggests to recall it in the ".cc" file
+
+#include <gz/plugin/Register.hh>
+
+
+#include <gz/sim/components/Pose.hh>
+#include <gz/math/Vector3.hh>
+
+
+// The next row recalls the plugin's header.
+
+#include "PluginLinearVelocity.hh"
+
+using namespace gz;
+using namespace sim;
+using namespace systems;
+
+
+// This is required to register the plugin.
+// NOTE: The interfaces shall match what's in the plugin's header (see line 26).
+
+GZ_ADD_PLUGIN(
+    plugin_test::PluginLinearVelocity,
+    gz::sim::System,
+    plugin_test::PluginLinearVelocity::ISystemConfigure,
+    plugin_test::PluginLinearVelocity::ISystemPreUpdate)
+
+
+using namespace plugin_test;
+
+// In the next section, all the functions that we need are recalled.
+// Since the plugin's header (see PluginLinearVelocity.hh file) is used as "function declaration",
+// in the following section the core purpose of our plugin will be defined.
+
+// For this plugin I decided to use the "Configure function" to read the field in the .sdf file
+// as e.g. the link_name field. Let's dive into the details, analyzing step-by-step the following code.
+
+
+void plugin_test::PluginLinearVelocity::Configure(const gz::sim::Entity &_identity,
+                           const std::shared_ptr<const sdf::Element> &_sdf,
+                           gz::sim::EntityComponentManager &_ecm,
+                           gz::sim::EventManager &_eventMgr)
+  {
+    // STEP1) Read property from SDF.
+    // In this case, I call it "link_name" (see line 84 in the velocity_world_plugin.sdf file)
+
+    auto linkName = _sdf->Get<std::string>("link_name");
+
+    // STEP 2) Store the lName variable the value of the "link_name"
+
+    this -> lName = linkName; // Do not forget to declare the lName variable in the plugin's header file (see line 50 in the PluginLinearVelocity.hh file))
+
+    // Store the _identity value using the "this ->"
+    this -> entity=_identity;
+
+    // Create the model object to access all the function using as attibute the "entity" variable.
+    // The function "Model()" enables to get some important details 
+    
+    auto model = Model(entity);
+
+    // Look for link entity using "LinkByName" into "model" defined above 
+    
+    this-> linkEntity = model.LinkByName(_ecm, linkName);
+
+  }
+
+
+void PluginLinearVelocity::PreUpdate(const gz::sim::UpdateInfo &_info,
+    gz::sim::EntityComponentManager &_ecm)
+{
+
+    // Define the linear velocity vector using.
+
+    math::Vector3d lVel(1,0,0);
+
+    // Create an object using the "Link" class.
+
+    Link link_n(linkEntity);
+
+    // The next row is the main command wich applies the linear velocity vector "lVel" on the link entity.
+    // For this example we are applying the linear velocity on a simple box that we defined in the sdf file (see line 39 in the velocity_world_plugin.sdf file)
+
+    link_n.SetLinearVelocity(_ecm,lVel);
+    
+
+}
+
+// end
---
+++ examples/plugin/velocity_plugin/PluginLinearVelocity.hh
@@ -0,0 +1,59 @@
+#ifndef SYSTEM_PLUGIN_HELLOWORLD_HH_
+#define SYSTEM_PLUGIN_HELLOWORLD_HH_
+
+
+#include <gz/sim/System.hh>
+#include <gz/sim/Model.hh>
+#include <gz/sim/Util.hh>
+#include <gz/math/Vector3.hh>
+
+
+using namespace gz;
+using namespace sim;
+using namespace systems;
+
+// When a plugin's header file is coded, it is a good practice to define a customized namespace.
+// In this case we call it "plugin_test"
+
+namespace plugin_test
+{
+
+  // Under our customized namespace we define and recall all the functions and variables that we need
+
+  // Since our objective is to:
+  // - Read property from the .sdf file
+  // - Send the LinearVelovity command to a link entity
+
+  // Here we have to use "ISystemConfigure" and "ISystemPreUpdate"
+
+  class PluginLinearVelocity:
+    public gz::sim::System,
+    public gz::sim::ISystemConfigure,
+    public gz::sim::ISystemPreUpdate
+  {
+    // The PreUpdate callback must be imported by the plugins that are inheriting ISystemPreUpdate.
+    // Since its nature, the PreUpdate callback is called every
+    // simulation iteration before the physics updates the world.
+    // On the other hand the Configure callback is called only once.
+    
+    public: void Configure(const Entity &_entity,
+                         const std::shared_ptr<const sdf::Element> &_sdf,
+                         EntityComponentManager &_ecm,
+                         EventManager &/*_eventMgr*/) override;
+
+    
+    public: void PreUpdate(const gz::sim::UpdateInfo &_info,
+                gz::sim::EntityComponentManager &_ecm) override;
+                
+
+    private: 
+      gz::sim::Entity linkEntity;
+      gz::sim::Entity entity;
+      std::string lName;
+
+  };
+
+
+}
+
+#endif
