aacf1db30e616ad799fddc2f3366d70a1b69dddf
Steve Peters <scpeters@openrobotics.org>
2025-05-27 09:16:09 -0700

    Clean up version logic
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- src/cmd/model_main.cc
+++ src/cmd/model_main.cc
@@ -150,7 +150,7 @@ void addModelFlags(CLI::App &_app)
                         "Print the pose of the model (only useful with --model).");
   // Don't specify that it needs --model for cli11 >= 2.2.0, < 2.5
   // See https://github.com/gazebosim/gz-sim/issues/2918
-  if (!(CLI11_VERSION_MAJOR >= 2 &&
+  if (!(CLI11_VERSION_MAJOR == 2 &&
         CLI11_VERSION_MINOR >= 2 &&
         CLI11_VERSION_MINOR < 5))
   {
