6ae1c2572dfbd4e268701bdf48edb579affb924e
Shivang <shivangso23@iitk.ac.in>
2025-11-14 18:46:00 +0530

    added test for gravity update command
    Signed-off-by: Shivang <shivangso23@iitk.ac.in>

--- test/integration/user_commands.cc
+++ test/integration/user_commands.cc
@@ -16,6 +16,7 @@
  */
 
 #include <future>
+#include <gz/msgs/details/vector3d.pb.h>
 #include <string>
 
 #include <gtest/gtest.h>
@@ -43,6 +44,7 @@
 #include "gz/sim/components/Model.hh"
 #include "gz/sim/components/Name.hh"
 #include "gz/sim/components/Physics.hh"
+#include "gz/sim/components/Gravity.hh"
 #include "gz/sim/components/Pose.hh"
 #include "gz/sim/components/VisualCmd.hh"
 #include "gz/sim/components/WheelSlipCmd.hh"
@@ -1584,14 +1586,27 @@ TEST_F(UserCommandsTest, GZ_UTILS_TEST_DISABLED_ON_WIN32(Physics))
   auto worldEntity = ecm->EntityByComponents(components::World());
   EXPECT_NE(kNullEntity, worldEntity);
   auto physicsComp = ecm->Component<components::Physics>(worldEntity);
+  auto gravityComp = ecm->Component<components::Gravity>(worldEntity);
   ASSERT_NE(nullptr, physicsComp);
+  ASSERT_NE(nullptr, gravityComp);
   EXPECT_DOUBLE_EQ(0.001, physicsComp->Data().MaxStepSize());
   EXPECT_DOUBLE_EQ(0.0, physicsComp->Data().RealTimeFactor());
+
+  auto gravity = gravityComp->Data();
+  EXPECT_DOUBLE_EQ(0.0,gravity.X());
+  EXPECT_DOUBLE_EQ(0.0,gravity.Y());
+  EXPECT_DOUBLE_EQ(-9.8,gravity.Z());
 
   // Set physics properties
   msgs::Physics req;
   req.set_max_step_size(0.123);
   req.set_real_time_factor(4.567);
+
+  gz::msgs::Vector3d new_gravity;
+  new_gravity.set_x(1.0);
+  new_gravity.set_y(3.0);
+  new_gravity.set_z(5.0);
+  req.set_allocated_gravity(&new_gravity);
 
   msgs::Boolean res;
   bool result;
@@ -1606,6 +1621,13 @@ TEST_F(UserCommandsTest, GZ_UTILS_TEST_DISABLED_ON_WIN32(Physics))
   // Run two iterations, in the first one the PhysicsCmd component is created
   // in the second one it is processed
   server.Run(true, 2, false);
+
+  // Check updated gravity
+  gravityComp = ecm->Component<components::Gravity>(worldEntity);
+  gravity = gravityComp->Data();
+  EXPECT_DOUBLE_EQ(new_gravity.x(),gravity.X());
+  EXPECT_DOUBLE_EQ(new_gravity.y(),gravity.Y());
+  EXPECT_DOUBLE_EQ(new_gravity.z(),gravity.Z());
 
   // Check updated physics properties
   physicsComp = ecm->Component<components::Physics>(worldEntity);
