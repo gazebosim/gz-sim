8e85f00f193257e83b965efa630d916d6de708d0
Oscmoar07 <108773152+XINJIANGMO@users.noreply.github.com>
2026-02-11 01:19:33 +0800

    Fix thread issue by adding check for invalid CLI combo (#3135)
    * add check to avoid thread issue
    
    Signed-off-by: momo <2438833481@qq.com>
    
    * update error message
    
    Co-authored-by: Ian Chen <ichen@openrobotics.org>
    Signed-off-by: Oscmoar07 <108773152+XINJIANGMO@users.noreply.github.com>
    
    * fix minor style issue
    
    Signed-off-by: momo <2438833481@qq.com>
    
    ---------
    
    Signed-off-by: momo <2438833481@qq.com>
    Signed-off-by: Oscmoar07 <108773152+XINJIANGMO@users.noreply.github.com>
    Co-authored-by: Ian Chen <ichen@openrobotics.org>

--- src/cmd/sim_main.cc
+++ src/cmd/sim_main.cc
@@ -461,7 +461,15 @@ int main(int argc, char** argv)
 
   app.formatter(std::make_shared<GzFormatter>(&app));
   CLI11_PARSE(app, argc, argv);
-
+  // check for invalid combination to avoid thread issue
+  if (!opt->file.empty() && !opt->playback.empty())
+  {
+    gzerr << "Both an SDF file and the playback "
+          << "flag (--playback) are specified. "
+          << "Specify only one of these arguments."
+          << std::endl;
+    return -1;
+  }
   std::string parsedSdfFile;
   if(!opt->launchServer && !opt->launchGui)
   {
