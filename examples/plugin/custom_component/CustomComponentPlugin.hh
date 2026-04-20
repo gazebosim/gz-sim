1c694190c2c43ca237809c7524a074a2375d7208
Luca Della Vedova <lucadv@intrinsic.ai>
2026-04-20 16:22:04 +0800

    Example of component in different namespace
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>

--- examples/plugin/custom_component/CustomComponentPlugin.hh
+++ examples/plugin/custom_component/CustomComponentPlugin.hh
@@ -26,14 +26,22 @@
 #include <gz/sim/System.hh>
 #include <gz/sim/config.hh>
 
-// Note that all components must be in the gz::sim::components namespace
-// for registration to work.
-namespace gz::sim::components
+namespace examples
 {
   // The most convenient way to create a component is to create a template alias
   // of `components::Component` where the first argument is the type being wrapped
   // (i.e. int) and the second is a unique name tag.
-  using CustomComponent = Component<int, class CustomComponentTag>;
+  using CustomComponent = gz::sim::components::Component<int, class CustomComponentTag>;
+}
+
+// All components must be registered in the gz::sim::components namespace
+// for the registration macro to work.
+namespace gz::sim::components
+{
+  // We use a local alias here because the GZ_SIM_REGISTER_COMPONENT macro
+  // uses token pasting (##), which doesn't work with qualified names (containing ::).
+  using CustomComponent = ::examples::CustomComponent;
+
   // Use this macro to register a component. Give it a unique name across the
   // entire simulation.
   GZ_SIM_REGISTER_COMPONENT("examples::CustomComponent", CustomComponent)
@@ -41,8 +49,6 @@ namespace gz::sim::components
 
 namespace examples
 {
-using CustomComponent = gz::sim::components::CustomComponent;
-
 class CustomComponentPlugin :
   public gz::sim::System,
   public gz::sim::ISystemConfigure
