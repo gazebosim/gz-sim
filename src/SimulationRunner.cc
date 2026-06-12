36a95d34e62d22eeca92ce1159a686fd98334f73
Luca Della Vedova <lucadv@intrinsic.ai>
2026-06-13 03:26:48 +0800

    Skip Recreate component query if no component exists (#3650)
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>
    Co-authored-by: Ian Chen <iche@intrinsic.ai>
    (cherry picked from commit 189dd1705af2741cee63678bd5d7ba17a31d73c9)

--- src/SimulationRunner.cc
+++ src/SimulationRunner.cc
@@ -1270,6 +1270,10 @@ void SimulationRunner::ProcessRecreateEntitiesRemove()
 {
   IGN_PROFILE("SimulationRunner::ProcessRecreateEntitiesRemove");
 
+  if (!this->entityCompMgr.HasComponentType(components::Recreate::typeId))
+  {
+    return;
+  }
   // store the original entities to recreate and put in request to remove them
   this->entityCompMgr.EachNoCache<components::Model,
                            components::Recreate>(
