4420095c0f54a3aaf91d854c19a9d5658b50cbce
Luca Della Vedova <lucadv@intrinsic.ai>
2026-06-29 21:42:02 +0800

    Use components::ParentEntity for parent lookup (#3711)
    Signed-off-by: Luca Della Vedova <lucadv@intrinsic.ai>
    Co-authored-by: Carlos Agüero <caguero@honurobotics.com>
    (cherry picked from commit dba8747ea3332db1ffe429e6204ae1f18becf59b)

--- src/EntityComponentManager.cc
+++ src/EntityComponentManager.cc
@@ -73,6 +73,15 @@ class gz::sim::EntityComponentManagerPrivate
   /// `AddEntityToMessage`.
   public: void CalculateStateThreadLoad();
 
+  /// \brief Helper function to only update the entity graph for a new
+  /// parent - child relation. This does not do any book-keeping on the
+  /// components::ParentEntity component, for which the EntityComponentManager
+  /// SetParentEntity function should be used.
+  /// \param[in] _child the entity to update the parent for.
+  /// \param[in] _parent the new parent of the entity, or kNullEntity if the
+  /// entity should be left parentless.
+  public: bool SetParentEntityGraph(const Entity _child, const Entity _parent);
+
   /// \brief Copies the contents of `_from` into this object.
   /// \note This is a member function instead of a copy constructor so that
   /// it can have additional parameters if the need arises in the future.
@@ -507,7 +516,6 @@ Entity EntityComponentManager::CloneImpl(Entity _entity, Entity _parent,
   if (_parent != kNullEntity)
   {
     this->SetParentEntity(clonedEntity, _parent);
-    this->CreateComponent(clonedEntity, components::ParentEntity(_parent));
   }
 
   // make sure that the cloned entity has a unique name
@@ -889,6 +897,12 @@ bool EntityComponentManager::RemoveComponent(
     this->dataPtr->removedComponents[_entity].insert(_typeId);
   }
 
+  // If the component is a components::ParentEntity, leave the entity parentless
+  if (_typeId == components::ParentEntity::typeId)
+  {
+    this->dataPtr->SetParentEntityGraph(_entity, kNullEntity);
+  }
+
   return true;
 }
 
@@ -1045,24 +1059,43 @@ bool EntityComponentManager::HasEntity(const Entity _entity) const
 /////////////////////////////////////////////////
 Entity EntityComponentManager::ParentEntity(const Entity _entity) const
 {
-  auto parents = this->Entities().AdjacentsTo(_entity);
-  if (parents.empty())
+  const auto* parent = this->Component<components::ParentEntity>(_entity);
+  if (!parent)
     return kNullEntity;
-
-  // TODO(louise) Do we want to support multiple parents?
-  return parents.begin()->first;
+  return parent->Data();
 }
 
 /////////////////////////////////////////////////
 bool EntityComponentManager::SetParentEntity(const Entity _child,
     const Entity _parent)
 {
+  // Validate input, child must exist and parent must either be kNullEntity
+  // (in which case we remove the parent) or a valid entity to set.
+  if (!this->HasEntity(_child))
+    return false;
+  if (_parent != kNullEntity && !this->HasEntity(_parent))
+    return false;
+
+  // Component creation and deletion take care of updating the graph
+  this->RemoveComponent<components::ParentEntity>(_child);
+  if (_parent == kNullEntity)
+  {
+    return true;
+  }
+  this->CreateComponent(_child, components::ParentEntity(_parent));
+  return true;
+}
+
+/////////////////////////////////////////////////
+bool EntityComponentManagerPrivate::SetParentEntityGraph(const Entity _child,
+    const Entity _parent)
+{
   // Remove current parent(s)
-  auto parents = this->Entities().AdjacentsTo(_child);
+  auto parents = this->entities.AdjacentsTo(_child);
   for (const auto &parent : parents)
   {
-    auto edge = this->dataPtr->entities.EdgeFromVertices(parent.first, _child);
-    this->dataPtr->entities.RemoveEdge(edge);
+    auto edge = this->entities.EdgeFromVertices(parent.first, _child);
+    this->entities.RemoveEdge(edge);
   }
 
   // Leave parent-less
@@ -1072,7 +1105,7 @@ bool EntityComponentManager::SetParentEntity(const Entity _child,
   }
 
   // Add edge
-  auto edge = this->dataPtr->entities.AddEdge({_parent, _child}, true);
+  auto edge = this->entities.AddEdge({_parent, _child}, true);
   return (math::graph::kNullId != edge.Id());
 }
 
@@ -1197,8 +1230,14 @@ bool EntityComponentManager::CreateComponentImplementation(
   // update the entities graph.
   if (_componentTypeId == components::ParentEntity::typeId)
   {
-    auto parentComp = this->Component<components::ParentEntity>(_entity);
-    this->SetParentEntity(_entity, parentComp->Data());
+    if (!_data)
+    {
+      gzerr << "Internal error: Invalid parent component detected, "
+            << "this should not happen." << std::endl;
+      return updateData;
+    }
+    const auto *parent = static_cast<const components::ParentEntity *>(_data);
+    this->dataPtr->SetParentEntityGraph(_entity, parent->Data());
   }
 
   return updateData;
@@ -2290,7 +2329,6 @@ void EntityComponentManager::ApplyEntityDiff(
         this->dataPtr->entityCount = entity;
       }
       copyComponents(entity);
-      this->SetParentEntity(entity, _other.ParentEntity(entity));
     }
   }
 
@@ -2314,7 +2352,6 @@ void EntityComponentManager::ApplyEntityDiff(
         this->dataPtr->entityCount = entity;
       }
       copyComponents(entity);
-      this->SetParentEntity(entity, _other.ParentEntity(entity));
     }
 
     this->RequestRemoveEntity(entity, false);
