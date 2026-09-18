7c25e199a679367ed063789bdd80f0e71d113186
C88-YQ <1409947012@qq.com>
2026-09-18 15:59:55 +0800

    Refactor UnitTestUtil
    Signed-off-by: C88-YQ <1409947012@qq.com>

--- test/helpers/UnitTestUtil.hh
+++ test/helpers/UnitTestUtil.hh
@@ -33,19 +33,19 @@
 namespace gz::sim::test
 {
 /////////////////////////////////////////////////
-/// \brief Load a world from an SDF string and create entities
-/// \param[in] _sdfString The SDF string to load
+/// \brief Load a world from an SDF string, create entities, and copy the
+/// world's first plugin SDF.
+/// \param[in] _sdfString The SDF string to load.
 /// \param[in] _ecm Immutable reference to ECM.
 /// \param[in] _eventMgr Event manager used while creating entities.
-/// \param[out] _entity The entity corresponding to the world
-/// \param[out] _pluginSdf Optional copy of the world's first plugin SDF.
-/// If nullptr, no plugin will be stored
+/// \param[out] _entity The entity corresponding to the world.
+/// \param[out] _pluginSdf Copy of the world's first plugin SDF.
 inline void LoadWorldContext(
   const std::string &_sdfString,
   EntityComponentManager &_ecm,
   EventManager &_eventMgr,
   Entity &_entity,
-  sdf::Plugin *_pluginSdf = nullptr)
+  sdf::Plugin &_pluginSdf)
 {
   sdf::Root root;
   root.LoadSdfString(_sdfString);
@@ -54,11 +54,8 @@ inline void LoadWorldContext(
   const sdf::World *worldSdf = root.WorldByIndex(0);
   ASSERT_NE(nullptr, worldSdf);
 
-  if (_pluginSdf)
-  {
-    ASSERT_FALSE(worldSdf->Plugins().empty());
-    *_pluginSdf = worldSdf->Plugins()[0];
-  }
+  ASSERT_FALSE(worldSdf->Plugins().empty());
+  _pluginSdf = worldSdf->Plugins()[0];
 
   SdfEntityCreator entityCreator(_ecm, _eventMgr);
 
@@ -67,19 +64,36 @@ inline void LoadWorldContext(
 }
 
 /////////////////////////////////////////////////
-/// \brief Load a model from an SDF string and create entities
-/// \param[in] _sdfString The SDF string to load
+/// \brief Load a world from an SDF string and create entities without
+/// copying the plugin SDF.
+/// \param[in] _sdfString The SDF string to load.
 /// \param[in] _ecm Immutable reference to ECM.
 /// \param[in] _eventMgr Event manager used while creating entities.
-/// \param[out] _entity The entity corresponding to the model
-/// \param[out] _pluginSdf Optional copy of the model's first plugin SDF.
-/// If nullptr, no plugin will be stored
+/// \param[out] _entity The entity corresponding to the world.
+inline void LoadWorldContext(
+  const std::string &_sdfString,
+  EntityComponentManager &_ecm,
+  EventManager &_eventMgr,
+  Entity &_entity)
+{
+  sdf::Plugin pluginSdf;
+  LoadWorldContext(_sdfString, _ecm, _eventMgr, _entity, pluginSdf);
+}
+
+/////////////////////////////////////////////////
+/// \brief Load a model from an SDF string, create entities, and copy the
+/// model's first plugin SDF.
+/// \param[in] _sdfString The SDF string to load.
+/// \param[in] _ecm Immutable reference to ECM.
+/// \param[in] _eventMgr Event manager used while creating entities.
+/// \param[out] _entity The entity corresponding to the model.
+/// \param[out] _pluginSdf Copy of the Model's first plugin SDF.
 inline void LoadModelContext(
   const std::string &_sdfString,
   EntityComponentManager &_ecm,
   EventManager &_eventMgr,
   Entity &_entity,
-  sdf::Plugin *_pluginSdf = nullptr)
+  sdf::Plugin &_pluginSdf)
 {
   sdf::Root root;
   root.LoadSdfString(_sdfString);
@@ -92,15 +106,29 @@ inline void LoadModelContext(
   const sdf::Model *modelSdf = worldSdf->ModelByIndex(0);
   ASSERT_NE(nullptr, modelSdf);
 
-  if (_pluginSdf)
-  {
-    ASSERT_FALSE(modelSdf->Plugins().empty());
-    *_pluginSdf = modelSdf->Plugins()[0];
-  }
+  ASSERT_FALSE(modelSdf->Plugins().empty());
+  _pluginSdf = modelSdf->Plugins()[0];
 
   SdfEntityCreator entityCreator(_ecm, _eventMgr);
   _entity = entityCreator.CreateEntitiesWithoutLoadingPlugins(modelSdf);
   ASSERT_NE(kNullEntity, _entity);
+}
+
+/////////////////////////////////////////////////
+/// \brief Load a model from an SDF string and create entities without
+/// copying the plugin SDF.
+/// \param[in] _sdfString The SDF string to load.
+/// \param[in] _ecm Immutable reference to ECM.
+/// \param[in] _eventMgr Event manager used while creating entities.
+/// \param[out] _entity The entity corresponding to the model.
+inline void LoadModelContext(
+  const std::string &_sdfString,
+  EntityComponentManager &_ecm,
+  EventManager &_eventMgr,
+  Entity &_entity)
+{
+  sdf::Plugin pluginSdf;
+  LoadModelContext(_sdfString, _ecm, _eventMgr, _entity, pluginSdf);
 }
 } // namespace gz::sim::test
 
